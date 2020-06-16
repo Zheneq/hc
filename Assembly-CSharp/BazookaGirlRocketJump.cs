86787f2bebfd05f6fd4d19f594394c56d0e648b2
Zhenёq <Zheneq@users.noreply.github.com>
2020-06-16 09:07:32 +0300

    Update BazookaGirlRocketJump.cs

--- Assembly-CSharp/BazookaGirlRocketJump.cs
+++ Assembly-CSharp/BazookaGirlRocketJump.cs
@@ -19,29 +19,11 @@ public class BazookaGirlRocketJump : Ability
 	private void SetupTargeter()
 	{
 		StandardEffectInfo moddedEffectForAllies = GetModdedEffectForAllies();
-		int num;
-		if (moddedEffectForAllies != null)
-		{
-			num = (moddedEffectForAllies.m_applyEffect ? 1 : 0);
-		}
-		else
-		{
-			num = 0;
-		}
-		bool affectsAllies = (byte)num != 0;
+		bool affectsAllies = moddedEffectForAllies != null && moddedEffectForAllies.m_applyEffect;
 		StandardEffectInfo moddedEffectForSelf = GetModdedEffectForSelf();
-		int num2;
-		if (moddedEffectForSelf != null)
-		{
-			num2 = (moddedEffectForSelf.m_applyEffect ? 1 : 0);
-		}
-		else
-		{
-			num2 = 0;
-		}
-		bool affectsCaster = (byte)num2 != 0;
-		base.Targeter = new AbilityUtil_Targeter_RocketJump(this, m_shape, m_penetrateLineOfSight, 0f, affectsAllies);
-		base.Targeter.SetAffectedGroups(true, affectsAllies, affectsCaster);
+		bool affectsCaster = moddedEffectForSelf != null && moddedEffectForSelf.m_applyEffect;
+		Targeter = new AbilityUtil_Targeter_RocketJump(this, m_shape, m_penetrateLineOfSight, 0f, affectsAllies);
+		Targeter.SetAffectedGroups(true, affectsAllies, affectsCaster);
 	}
 
 	protected override List<AbilityTooltipNumber> CalculateAbilityTooltipNumbers()
@@ -54,26 +36,12 @@ public class BazookaGirlRocketJump : Ability
 
 	public override Dictionary<AbilityTooltipSymbol, int> GetCustomNameplateItemTooltipValues(ActorData targetActor, int currentTargeterIndex)
 	{
-		List<AbilityTooltipSubject> tooltipSubjectTypes = base.Targeter.GetTooltipSubjectTypes(targetActor);
-		if (tooltipSubjectTypes != null)
+		List<AbilityTooltipSubject> tooltipSubjectTypes = Targeter.GetTooltipSubjectTypes(targetActor);
+		if (tooltipSubjectTypes != null && tooltipSubjectTypes.Contains(AbilityTooltipSubject.Primary))
 		{
-			if (tooltipSubjectTypes.Contains(AbilityTooltipSubject.Primary))
-			{
-				while (true)
-				{
-					switch (5)
-					{
-					case 0:
-						break;
-					default:
-					{
-						Dictionary<AbilityTooltipSymbol, int> dictionary = new Dictionary<AbilityTooltipSymbol, int>();
-						dictionary[AbilityTooltipSymbol.Damage] = GetDamageAmount();
-						return dictionary;
-					}
-					}
-				}
-			}
+			Dictionary<AbilityTooltipSymbol, int> dictionary = new Dictionary<AbilityTooltipSymbol, int>();
+			dictionary[AbilityTooltipSymbol.Damage] = GetDamageAmount();
+			return dictionary;
 		}
 		return null;
 	}
@@ -81,17 +49,8 @@ public class BazookaGirlRocketJump : Ability
 	protected override void AddSpecificTooltipTokens(List<TooltipTokenEntry> tokens, AbilityMod modAsBase)
 	{
 		AbilityMod_BazookaGirlRocketJump abilityMod_BazookaGirlRocketJump = modAsBase as AbilityMod_BazookaGirlRocketJump;
-		string empty = string.Empty;
-		int val;
-		if ((bool)abilityMod_BazookaGirlRocketJump)
-		{
-			val = abilityMod_BazookaGirlRocketJump.m_damageMod.GetModifiedValue(m_damageAmount);
-		}
-		else
-		{
-			val = m_damageAmount;
-		}
-		AddTokenInt(tokens, "DamageAmount", empty, val);
+		int damage = abilityMod_BazookaGirlRocketJump?.m_damageMod.GetModifiedValue(m_damageAmount) ?? m_damageAmount;
+		AddTokenInt(tokens, "DamageAmount", string.Empty, damage);
 	}
 
 	internal override ActorData.MovementType GetMovementType()
@@ -101,22 +60,13 @@ public class BazookaGirlRocketJump : Ability
 
 	protected override void OnApplyAbilityMod(AbilityMod abilityMod)
 	{
-		if (abilityMod.GetType() == typeof(AbilityMod_BazookaGirlRocketJump))
+		if (abilityMod.GetType() != typeof(AbilityMod_BazookaGirlRocketJump))
 		{
-			while (true)
-			{
-				switch (1)
-				{
-				case 0:
-					break;
-				default:
-					m_abilityMod = (abilityMod as AbilityMod_BazookaGirlRocketJump);
-					SetupTargeter();
-					return;
-				}
-			}
+			Debug.LogError("Trying to apply wrong type of ability mod");
+			return;
 		}
-		Debug.LogError("Trying to apply wrong type of ability mod");
+		m_abilityMod = (abilityMod as AbilityMod_BazookaGirlRocketJump);
+		SetupTargeter();
 	}
 
 	protected override void OnRemoveAbilityMod()
@@ -127,20 +77,11 @@ public class BazookaGirlRocketJump : Ability
 
 	public int GetDamageAmount()
 	{
-		int result;
-		if (m_abilityMod == null)
-		{
-			result = m_damageAmount;
-		}
-		else
-		{
-			result = m_abilityMod.m_damageMod.GetModifiedValue(m_damageAmount);
-		}
-		return result;
+		return m_abilityMod?.m_damageMod.GetModifiedValue(m_damageAmount) ?? m_damageAmount;
 	}
 
 	public bool ResetCooldownOnKill()
 	{
-		return !(m_abilityMod == null) && m_abilityMod.m_resetCooldownOnKill;
+		return m_abilityMod != null && m_abilityMod.m_resetCooldownOnKill;
 	}
 }
