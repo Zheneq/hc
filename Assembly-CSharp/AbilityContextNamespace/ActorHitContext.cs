6ff57442d63c2dbc5d6765695335663423b08c9c
Zhenёq <Zheneq@users.noreply.github.com>
2022-01-14 01:53:22 +0300

    Update ActorHitContext.cs

--- Assembly-CSharp/AbilityContextNamespace/ActorHitContext.cs
+++ Assembly-CSharp/AbilityContextNamespace/ActorHitContext.cs
@@ -9,7 +9,7 @@ namespace AbilityContextNamespace
 		public bool m_inRangeForTargeter;
 		public ContextVars m_contextVars = new ContextVars();
 
-		public void Clear()
+		public void Reset()
 		{
 			m_hitOrigin = Vector3.zero;
 			m_contextVars.ClearData();
