b8326367ecd459fe21394c4bbd6d7c73a22edfcd
Zhenёq <Zheneq@users.noreply.github.com>
2022-01-27 00:43:37 +0300

    Update ControlPointManager.cs

--- Assembly-CSharp/ControlPointManager.cs
+++ Assembly-CSharp/ControlPointManager.cs
@@ -5,7 +5,6 @@ public class ControlPointManager : MonoBehaviour
 {
 	[Separator("For Randomly Activating Control Point On Start", true)]
 	public List<ControlPoint> m_randControlPointsToConsider;
-
 	public int m_numRandControlPointsToActivate;
 
 	[Header("-- Turns Locked Override, ignored if < 0")]
@@ -13,6 +12,5 @@ public class ControlPointManager : MonoBehaviour
 
 	[Separator("For Randomly Activating Control Point after X turns of no capturing", true)]
 	public RandomControlPointActivateData m_stalemateAlleviateActivations;
-
 	public int m_stalemateAlleviateTurns;
 }
