0bc8755f24fa6b04613aa721d322c7f9db72aa3f
Zhenёq <Zheneq@users.noreply.github.com>
2022-01-27 19:08:41 +0300

    Update StandardEffectInfo.cs

--- Assembly-CSharp/StandardEffectInfo.cs
+++ Assembly-CSharp/StandardEffectInfo.cs
@@ -5,23 +5,22 @@ using System.Collections.Generic;
 public class StandardEffectInfo
 {
 	public bool m_applyEffect;
-
 	public StandardActorEffectData m_effectData;
 
 	public virtual void ReportAbilityTooltipNumbers(ref List<AbilityTooltipNumber> numbers, AbilityTooltipSubject subject)
 	{
-		if (!m_applyEffect)
+		if (m_applyEffect)
 		{
-			return;
+			m_effectData.ReportAbilityTooltipNumbers(ref numbers, subject);
 		}
-		m_effectData.ReportAbilityTooltipNumbers(ref numbers, subject);
 	}
 
 	public StandardEffectInfo GetShallowCopy()
 	{
-		StandardEffectInfo standardEffectInfo = new StandardEffectInfo();
-		standardEffectInfo.m_applyEffect = m_applyEffect;
-		standardEffectInfo.m_effectData = m_effectData.GetShallowCopy();
-		return standardEffectInfo;
+		return new StandardEffectInfo
+		{
+			m_applyEffect = m_applyEffect,
+			m_effectData = m_effectData.GetShallowCopy()
+		};
 	}
 }
