f8f6e4c836d8df72f1cdca7b54c92ff4453cb0a7
Zhenёq <Zheneq@users.noreply.github.com>
2022-01-27 19:04:57 +0300

    Update PowerUp_Standard_Ability.cs

--- Assembly-CSharp/PowerUp_Standard_Ability.cs
+++ Assembly-CSharp/PowerUp_Standard_Ability.cs
@@ -14,32 +14,21 @@ public class PowerUp_Standard_Ability : Ability
 	public class ExtraEffectApplyData
 	{
 		public ExtraEffectApplyCondition m_condition;
-
 		public StandardEffectInfo m_extraEffect;
 	}
 
 	public int m_healAmount = 30;
-
 	public int m_techPointsAmount;
-
 	[Tooltip("Credits to give to the actor who ran over the powerup.")]
 	public int m_personalCredits;
-
 	[Tooltip("Credits to give to each actor on the team of the actor who ran over the powerup (including the picker-upper).")]
 	public int m_teamCredits;
-
 	public int m_objectivePointAdjust_casterTeam;
-
 	public int m_objectivePointAdjust_enemyTeam;
-
 	public bool m_applyEffect;
-
 	public StandardActorEffectData m_effect;
-
 	public AbilityStatMod[] m_permanentStatMods;
-
 	public StatusType[] m_permanentStatusChanges;
-
 	public bool m_awardCoins;
 
 	[Separator("Extra Effects, for one-off powerups", true)]
