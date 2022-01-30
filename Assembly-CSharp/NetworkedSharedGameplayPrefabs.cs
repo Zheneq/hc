bf746277f952a7eef1f4b5b9fc8f43a8efadb4e1
Zhenёq <Zheneq@users.noreply.github.com>
2022-01-30 23:27:29 +0300

    Update NetworkedSharedGameplayPrefabs.cs

--- Assembly-CSharp/NetworkedSharedGameplayPrefabs.cs
+++ Assembly-CSharp/NetworkedSharedGameplayPrefabs.cs
@@ -3,10 +3,7 @@ using UnityEngine;
 public class NetworkedSharedGameplayPrefabs : MonoBehaviour
 {
 	public GameObject m_sharedActionBuffer;
-
 	public GameObject m_sharedEffectBarrierManager;
-
 	public GameObject m_actorTeamSensitiveData_Friendly;
-
 	public GameObject m_actorTeamSensitiveData_Hostile;
 }
