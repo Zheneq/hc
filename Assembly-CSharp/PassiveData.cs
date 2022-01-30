8cee160882ddb72bc22135c586fc7789207cc7c4
Zhenёq <Zheneq@users.noreply.github.com>
2022-01-30 11:22:04 +0300

    Update PassiveData.cs

--- Assembly-CSharp/PassiveData.cs
+++ Assembly-CSharp/PassiveData.cs
@@ -4,48 +4,30 @@ using UnityEngine;
 public class PassiveData : MonoBehaviour
 {
 	public Passive[] m_passives;
-
 	public string m_toolTip;
-
 	public string m_toolTipTitle = "Passive";
 
 	public Passive GetPassiveOfType(Type passiveType)
 	{
-		Passive[] passives = m_passives;
-		foreach (Passive passive in passives)
+		foreach (Passive passive in m_passives)
 		{
-			if (passive != null)
+			if (passive != null && passive.GetType() == passiveType)
 			{
-				if (passive.GetType() == passiveType)
-				{
-					return passive;
-				}
+				return passive;
 			}
 		}
-		while (true)
-		{
-			return null;
-		}
+		return null;
 	}
 
 	public T GetPassiveOfType<T>() where T : Passive
 	{
-		Passive[] passives = m_passives;
-		foreach (Passive passive in passives)
+		foreach (Passive passive in m_passives)
 		{
-			if (!(passive != null))
-			{
-				continue;
-			}
-			if (passive.GetType() != typeof(T))
-			{
-				continue;
-			}
-			while (true)
+			if (passive != null && passive.GetType() == typeof(T))
 			{
 				return passive as T;
 			}
 		}
-		return (T)null;
+		return null;
 	}
 }
