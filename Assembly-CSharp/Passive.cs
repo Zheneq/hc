650f9b522dcb1aaf5898a3f2024f0781350fedcd
Zhenёq <Zheneq@users.noreply.github.com>
2022-01-30 23:22:44 +0300

    Update Passive.cs

--- Assembly-CSharp/Passive.cs
+++ Assembly-CSharp/Passive.cs
@@ -3,19 +3,12 @@ using UnityEngine;
 public class Passive : MonoBehaviour
 {
 	public string m_passiveName;
-
 	protected ActorData m_owner;
-
 	public GameObject m_sequencePrefab;
-
 	public AbilityStatMod[] m_passiveStatMods;
-
 	public StatusType[] m_passiveStatusChanges;
-
 	public EmptyTurnData[] m_onEmptyTurn;
-
 	public PassiveEventData[] m_eventResponses;
-
 	public StandardActorEffectData[] m_effectsOnEveryTurn;
 
 	public ActorData Owner
