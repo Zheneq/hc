c9ac00785c3900bfab204b515a993c27f2d600f3
Zhenёq <Zheneq@users.noreply.github.com>
2022-02-01 12:28:33 +0300

    Update GameplayResponseForActor.cs

--- Assembly-CSharp/GameplayResponseForActor.cs
+++ Assembly-CSharp/GameplayResponseForActor.cs
@@ -49,22 +49,22 @@ public class GameplayResponseForActor
 		bool addDiff = showDiff && other != null;
 		string otherSep = "\t        \t | in base  =";
 		string desc = "\n" + InEditorDescHelper.BoldedStirng(header) + "\n";
-		float creditsVal = addDiff ? other.m_credits : 0;		
-		desc += InEditorDescHelper.AssembleFieldWithDiff("[ Credits ] = ", indent, otherSep, m_credits, addDiff, creditsVal, ((float f) => f != 0f));
-		int healingVal = addDiff ? other.m_healing : 0;
-		desc += InEditorDescHelper.AssembleFieldWithDiff("[ Healing ] = ", indent, otherSep, m_healing, addDiff, healingVal);
-		int damageVal = addDiff ? other.m_damage : 0;
-		desc += InEditorDescHelper.AssembleFieldWithDiff("[ Damage ] = ", indent, otherSep, m_damage, addDiff, damageVal);
-		float techPointsVal = addDiff ? other.m_techPoints : 0;		
-		desc += InEditorDescHelper.AssembleFieldWithDiff("[ TechPoints ] = ", indent, otherSep, m_techPoints, addDiff, techPointsVal, ((float f) => f != 0f));
-		StandardEffectInfo effectVal = addDiff ? other.m_effect : null;
-		desc += AbilityModHelper.GetModEffectInfoDesc(m_effect, "{ Effect on Moved-Through }", indent, addDiff, effectVal);
-		AbilityStatMod[] permModsVal = addDiff ? other.m_permanentStatMods : null;
-		desc += InEditorDescHelper.GetListDiffString("Permanent Stat Mods:", indent, m_permanentStatMods, addDiff, permModsVal);
-		StatusType[] permStatusVal = addDiff ? other.m_permanentStatusChanges : null;
-		desc += InEditorDescHelper.GetListDiffString("Permanent Status Changes:", indent, m_permanentStatusChanges, addDiff, permStatusVal);
-		GameObject sequenceVal = addDiff ? other.m_sequenceToPlay : null;
-		desc += InEditorDescHelper.AssembleFieldWithDiff("Response Hit Sequence", indent, otherSep, m_sequenceToPlay, addDiff, sequenceVal);
+		float otherCredits = addDiff ? other.m_credits : 0;		
+		desc += InEditorDescHelper.AssembleFieldWithDiff("[ Credits ] = ", indent, otherSep, m_credits, addDiff, otherCredits, ((float f) => f != 0f));
+		int otherHealing = addDiff ? other.m_healing : 0;
+		desc += InEditorDescHelper.AssembleFieldWithDiff("[ Healing ] = ", indent, otherSep, m_healing, addDiff, otherHealing);
+		int otherDamage = addDiff ? other.m_damage : 0;
+		desc += InEditorDescHelper.AssembleFieldWithDiff("[ Damage ] = ", indent, otherSep, m_damage, addDiff, otherDamage);
+		float otherTechPoints = addDiff ? other.m_techPoints : 0;		
+		desc += InEditorDescHelper.AssembleFieldWithDiff("[ TechPoints ] = ", indent, otherSep, m_techPoints, addDiff, otherTechPoints, ((float f) => f != 0f));
+		StandardEffectInfo otherEffect = addDiff ? other.m_effect : null;
+		desc += AbilityModHelper.GetModEffectInfoDesc(m_effect, "{ Effect on Moved-Through }", indent, addDiff, otherEffect);
+		AbilityStatMod[] otherMods = addDiff ? other.m_permanentStatMods : null;
+		desc += InEditorDescHelper.GetListDiffString("Permanent Stat Mods:", indent, m_permanentStatMods, addDiff, otherMods);
+		StatusType[] otherStatuses = addDiff ? other.m_permanentStatusChanges : null;
+		desc += InEditorDescHelper.GetListDiffString("Permanent Status Changes:", indent, m_permanentStatusChanges, addDiff, otherStatuses);
+		GameObject otherSequence = addDiff ? other.m_sequenceToPlay : null;
+		desc += InEditorDescHelper.AssembleFieldWithDiff("Response Hit Sequence", indent, otherSep, m_sequenceToPlay, addDiff, otherSequence);
 		return desc + indent + "-- END of Move-Through Response Output --\n";
 	}
 
