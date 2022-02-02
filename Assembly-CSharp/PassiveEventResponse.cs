fd808fe5ba859659fec12387fe8ec34ae7c4f9f1
Zhenёq <Zheneq@users.noreply.github.com>
2022-02-02 21:47:31 +0300

    Update PassiveEventResponse.cs

--- Assembly-CSharp/PassiveEventResponse.cs
+++ Assembly-CSharp/PassiveEventResponse.cs
@@ -4,14 +4,9 @@ using System;
 public class PassiveEventResponse
 {
 	public int m_healthBonus;
-
 	public int m_techPointsBonus;
-
 	public int m_personalCreditsBonus;
-
 	public int m_mechanicPointAdjust;
-
 	public PassiveActionType[] m_actions;
-
 	public StandardEffectInfo m_effect;
 }
