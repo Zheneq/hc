47fd1b388e56d600681e0a1977680a22abdc144c
Zhenёq <Zheneq@users.noreply.github.com>
2022-02-06 18:12:33 +0300

    Update SparkBasicAttack.cs

--- Assembly-CSharp/SparkBasicAttack.cs
+++ Assembly-CSharp/SparkBasicAttack.cs
@@ -5,53 +5,33 @@ public class SparkBasicAttack : Ability
 {
 	[Space(10f)]
 	public int m_laserDamageAmount = 5;
-
 	public int m_healOnCasterOnTick;
-
 	public bool m_healCasterOnIniialAttach;
-
 	public LaserTargetingInfo m_laserInfo;
-
 	public StandardEffectInfo m_laserHitEffect;
-
 	[Header("-- Energy on Caster Per Turn")]
 	public int m_energyOnCasterPerTurn = 5;
-
 	[Separator("Tether", true)]
 	public float m_tetherDistance = 5f;
-
 	[Header("-- Tether Duration")]
 	public int m_tetherDuration;
-
 	public int m_additionalEnergizedDamage = 2;
-
 	[Header("-- Extra Energy Gain On Caster --")]
 	public int m_maxBonusEnergyFromGrowingGain;
-
 	public int m_bonusEnergyGrowthRate;
-
 	[Header("-- Animation on Pulse")]
 	public int m_pulseAnimIndex;
-
 	public int m_energizedPulseAnimIndex;
-
 	[Header("-- Sequences")]
 	public GameObject m_castSequence;
-
 	public GameObject m_pulseSequence;
-
 	public GameObject m_energizedPulseSequence;
-
 	public GameObject m_beamSequence;
-
 	public GameObject m_targetPersistentSequence;
 
 	private AbilityMod_SparkBasicAttack m_abilityMod;
-
 	private SparkEnergized m_energizedAbility;
-
 	private LaserTargetingInfo m_cachedLaserInfo;
-
 	private StandardActorEffectData m_cachedEffectData;
 
 	private void Start()
@@ -68,30 +48,21 @@ public class SparkBasicAttack : Ability
 		SetCachedFields();
 		if (m_energizedAbility == null)
 		{
-			AbilityData component = GetComponent<AbilityData>();
-			if (component != null)
+			AbilityData abilityData = GetComponent<AbilityData>();
+			if (abilityData != null)
 			{
-				m_energizedAbility = (component.GetAbilityOfType(typeof(SparkEnergized)) as SparkEnergized);
+				m_energizedAbility = abilityData.GetAbilityOfType(typeof(SparkEnergized)) as SparkEnergized;
 			}
 		}
-		if (base.Targeter != null)
+		if (Targeter != null)
 		{
-			base.Targeter.ResetTargeter(true);
+			Targeter.ResetTargeter(true);
 		}
-		AbilityUtil_Targeter_Laser abilityUtil_Targeter_Laser = new AbilityUtil_Targeter_Laser(this, GetLaserInfo());
-		int affectsCaster;
-		if (m_healCasterOnIniialAttach)
-		{
-			affectsCaster = ((GetHealOnCasterPerTurn() > 0) ? 1 : 0);
-		}
-		else
-		{
-			affectsCaster = 0;
-		}
-		abilityUtil_Targeter_Laser.SetAffectedGroups(true, false, (byte)affectsCaster != 0);
-		
-		abilityUtil_Targeter_Laser.m_affectCasterDelegate = ((ActorData caster, List<ActorData> actorsSoFar) => actorsSoFar.Count > 0);
-		base.Targeter = abilityUtil_Targeter_Laser;
+		AbilityUtil_Targeter_Laser targeter = new AbilityUtil_Targeter_Laser(this, GetLaserInfo());
+		bool affectsCaster = m_healCasterOnIniialAttach && GetHealOnCasterPerTurn() > 0;
+		targeter.SetAffectedGroups(true, false, affectsCaster);
+		targeter.m_affectCasterDelegate = ((ActorData caster, List<ActorData> actorsSoFar) => actorsSoFar.Count > 0);
+		Targeter = targeter;
 	}
 
 	public override bool CanShowTargetableRadiusPreview()
@@ -106,16 +77,9 @@ public class SparkBasicAttack : Ability
 
 	public int GetInitialDamage()
 	{
-		int result;
-		if ((bool)m_abilityMod)
-		{
-			result = m_abilityMod.m_initialDamageMod.GetModifiedValue(m_laserDamageAmount);
-		}
-		else
-		{
-			result = m_laserDamageAmount;
-		}
-		return result;
+		return m_abilityMod != null
+			? m_abilityMod.m_initialDamageMod.GetModifiedValue(m_laserDamageAmount)
+			: m_laserDamageAmount;
 	}
 
 	public int GetPerTurnDamage()
@@ -125,97 +89,62 @@ public class SparkBasicAttack : Ability
 
 	public int GetAdditionalDamageOnRadiated()
 	{
-		return (!m_abilityMod) ? m_additionalEnergizedDamage : m_abilityMod.m_additionalDamageOnRadiatedMod.GetModifiedValue(m_additionalEnergizedDamage);
+		return m_abilityMod != null
+			? m_abilityMod.m_additionalDamageOnRadiatedMod.GetModifiedValue(m_additionalEnergizedDamage)
+			: m_additionalEnergizedDamage;
 	}
 
 	public int GetEnergyOnCasterPerTurn()
 	{
-		int num;
-		if ((bool)m_abilityMod)
-		{
-			num = m_abilityMod.m_energyOnCasterPerTurnMod.GetModifiedValue(m_energyOnCasterPerTurn);
-		}
-		else
-		{
-			num = m_energyOnCasterPerTurn;
-		}
-		int num2 = num;
-		if (m_energizedAbility != null)
-		{
-			num2 = m_energizedAbility.CalcEnergyOnSelfPerTurn(num2);
-		}
-		return num2;
+		int energy = m_abilityMod != null
+			? m_abilityMod.m_energyOnCasterPerTurnMod.GetModifiedValue(m_energyOnCasterPerTurn)
+			: m_energyOnCasterPerTurn;
+		return m_energizedAbility != null
+			? m_energizedAbility.CalcEnergyOnSelfPerTurn(energy)
+			: energy;
 	}
 
 	public int GetHealOnCasterPerTurn()
 	{
-		int num = (!m_abilityMod) ? m_healOnCasterOnTick : m_abilityMod.m_healOnCasterOnTickMod.GetModifiedValue(m_healOnCasterOnTick);
-		if (m_energizedAbility != null)
-		{
-			num = m_energizedAbility.CalcHealOnSelfPerTurn(num);
-		}
-		return num;
+		int heal = m_abilityMod != null
+			? m_abilityMod.m_healOnCasterOnTickMod.GetModifiedValue(m_healOnCasterOnTick)
+			: m_healOnCasterOnTick;
+		return m_energizedAbility != null
+			? m_energizedAbility.CalcHealOnSelfPerTurn(heal)
+			: heal;
 	}
 
 	public float GetTetherDistance()
 	{
-		float result;
-		if ((bool)m_abilityMod)
-		{
-			result = m_abilityMod.m_tetherDistanceMod.GetModifiedValue(m_tetherDistance);
-		}
-		else
-		{
-			result = m_tetherDistance;
-		}
-		return result;
+		return m_abilityMod != null
+			? m_abilityMod.m_tetherDistanceMod.GetModifiedValue(m_tetherDistance)
+			: m_tetherDistance;
 	}
 
 	public int GetTetherDuration()
 	{
-		int result;
-		if (m_abilityMod != null)
-		{
-			result = m_abilityMod.m_tetherDurationMod.GetModifiedValue(m_tetherDuration);
-		}
-		else
-		{
-			result = m_tetherDuration;
-		}
-		return result;
+		return m_abilityMod != null
+			? m_abilityMod.m_tetherDurationMod.GetModifiedValue(m_tetherDuration)
+			: m_tetherDuration;
 	}
 
 	public bool UseBonusDamageOverTime()
 	{
-		int result;
-		if ((bool)m_abilityMod)
-		{
-			result = (m_abilityMod.m_useBonusDamageOverTime ? 1 : 0);
-		}
-		else
-		{
-			result = 0;
-		}
-		return (byte)result != 0;
+		return m_abilityMod != null && m_abilityMod.m_useBonusDamageOverTime;
 	}
 
 	public int GetBonusDamageGrowRate()
 	{
-		return m_abilityMod ? m_abilityMod.m_bonusDamageIncreaseRateMod.GetModifiedValue(0) : 0;
+		return m_abilityMod != null
+			? m_abilityMod.m_bonusDamageIncreaseRateMod.GetModifiedValue(0)
+			: 0;
 	}
 
 	public int GetMaxBonusDamage()
 	{
-		int result;
-		if ((bool)m_abilityMod)
-		{
-			result = m_abilityMod.m_maxBonusDamageAmountMod.GetModifiedValue(0);
-		}
-		else
-		{
-			result = 0;
-		}
-		return result;
+		return m_abilityMod != null
+			? m_abilityMod.m_maxBonusDamageAmountMod.GetModifiedValue(0)
+			: 0;
 	}
 
 	public int GetBonusDamageFromTetherAge(int age)
@@ -239,76 +168,40 @@ public class SparkBasicAttack : Ability
 
 	public int GetEnergyGainCyclePeriod()
 	{
-		int num;
-		if ((bool)m_abilityMod)
-		{
-			num = m_abilityMod.m_energyGainCyclePeriod.GetModifiedValue(0);
-		}
-		else
-		{
-			num = 1;
-		}
-		int b = num;
-		return Mathf.Max(1, b);
+		int num = m_abilityMod != null
+			? m_abilityMod.m_energyGainCyclePeriod.GetModifiedValue(0)
+			: 1;
+		return Mathf.Max(1, num);
 	}
 
 	public int GetEnergyGainPerCycle()
 	{
-		return m_abilityMod ? m_abilityMod.m_energyGainPerCycle.GetModifiedValue(0) : 0;
+		return m_abilityMod != null
+			? m_abilityMod.m_energyGainPerCycle.GetModifiedValue(0) 
+			: 0;
 	}
 
 	public int GetMaxBonusEnergyFromGrowingGain()
 	{
-		int result;
-		if ((bool)m_abilityMod)
-		{
-			result = m_abilityMod.m_maxBonusEnergyFromGrowingGainMod.GetModifiedValue(m_maxBonusEnergyFromGrowingGain);
-		}
-		else
-		{
-			result = m_maxBonusEnergyFromGrowingGain;
-		}
-		return result;
+		return m_abilityMod != null
+			? m_abilityMod.m_maxBonusEnergyFromGrowingGainMod.GetModifiedValue(m_maxBonusEnergyFromGrowingGain)
+			: m_maxBonusEnergyFromGrowingGain;
 	}
 
 	public int GetBonusEnergyGrowthRate()
 	{
-		int result;
-		if ((bool)m_abilityMod)
-		{
-			result = m_abilityMod.m_bonusEnergyGrowthRateMod.GetModifiedValue(m_bonusEnergyGrowthRate);
-		}
-		else
-		{
-			result = m_bonusEnergyGrowthRate;
-		}
-		return result;
+		return m_abilityMod != null
+			? m_abilityMod.m_bonusEnergyGrowthRateMod.GetModifiedValue(m_bonusEnergyGrowthRate)
+			: m_bonusEnergyGrowthRate;
 	}
 
 	private void SetCachedFields()
 	{
-		LaserTargetingInfo laserInfo = m_laserInfo;
-		object mod;
-		if ((bool)m_abilityMod)
-		{
-			mod = m_abilityMod.m_laserInfoMod;
-		}
-		else
-		{
-			mod = null;
-		}
-		m_cachedLaserInfo = laserInfo.GetModifiedCopy((AbilityModPropertyLaserInfo)mod);
-		StandardEffectInfo standardEffectInfo;
-		if ((bool)m_abilityMod)
-		{
-			standardEffectInfo = m_abilityMod.m_tetherBaseEffectOverride.GetModifiedValue(m_laserHitEffect);
-		}
-		else
-		{
-			standardEffectInfo = m_laserHitEffect.GetShallowCopy();
-		}
-		StandardEffectInfo standardEffectInfo2 = standardEffectInfo;
-		m_cachedEffectData = standardEffectInfo2.m_effectData;
+		m_cachedLaserInfo = m_laserInfo.GetModifiedCopy(m_abilityMod?.m_laserInfoMod);
+		StandardEffectInfo effectInfo = m_abilityMod != null
+			? m_abilityMod.m_tetherBaseEffectOverride.GetModifiedValue(m_laserHitEffect)
+			: m_laserHitEffect.GetShallowCopy();
+		m_cachedEffectData = effectInfo.m_effectData;
 		m_cachedEffectData.m_sequencePrefabs = new GameObject[2]
 		{
 			m_targetPersistentSequence,
@@ -318,30 +211,12 @@ public class SparkBasicAttack : Ability
 
 	public StandardActorEffectData GetEnemyTetherEffectData()
 	{
-		StandardActorEffectData result;
-		if (m_cachedEffectData != null)
-		{
-			result = m_cachedEffectData;
-		}
-		else
-		{
-			result = m_laserHitEffect.m_effectData;
-		}
-		return result;
+		return m_cachedEffectData ?? m_laserHitEffect.m_effectData;
 	}
 
 	public LaserTargetingInfo GetLaserInfo()
 	{
-		LaserTargetingInfo result;
-		if (m_cachedLaserInfo != null)
-		{
-			result = m_cachedLaserInfo;
-		}
-		else
-		{
-			result = m_laserInfo;
-		}
-		return result;
+		return m_cachedLaserInfo ?? m_laserInfo;
 	}
 
 	protected override void AddSpecificTooltipTokens(List<TooltipTokenEntry> tokens, AbilityMod modAsBase)
@@ -350,10 +225,10 @@ public class SparkBasicAttack : Ability
 		AddTokenInt(tokens, "Damage_PerTurnAfterFirst", "damage on subsequent turns", m_laserHitEffect.m_effectData.m_damagePerTurn);
 		AddTokenInt(tokens, "Damage_AdditionalOnRadiated", "additional damage on radiated", m_additionalEnergizedDamage);
 		AddTokenInt(tokens, "Heal_OnCasterPerTurn", "heal on caster per turn", m_healOnCasterOnTick);
-		AddTokenInt(tokens, "EnergyOnCasterPerTurn", string.Empty, m_energyOnCasterPerTurn);
-		AddTokenInt(tokens, "MaxBonusEnergyFromGrowingGain", string.Empty, m_maxBonusEnergyFromGrowingGain);
-		AddTokenInt(tokens, "BonusEnergyGrowthRate", string.Empty, m_bonusEnergyGrowthRate);
-		AddTokenInt(tokens, "TetherDuration", string.Empty, m_tetherDuration);
+		AddTokenInt(tokens, "EnergyOnCasterPerTurn", "", m_energyOnCasterPerTurn);
+		AddTokenInt(tokens, "MaxBonusEnergyFromGrowingGain", "", m_maxBonusEnergyFromGrowingGain);
+		AddTokenInt(tokens, "BonusEnergyGrowthRate", "", m_bonusEnergyGrowthRate);
+		AddTokenInt(tokens, "TetherDuration", "", m_tetherDuration);
 	}
 
 	protected override List<AbilityTooltipNumber> CalculateAbilityTooltipNumbers()
@@ -364,29 +239,20 @@ public class SparkBasicAttack : Ability
 			AbilityTooltipHelper.ReportDamage(ref number, AbilityTooltipSubject.Primary, GetInitialDamage());
 		}
 		GetEnemyTetherEffectData().ReportAbilityTooltipNumbers(ref number, AbilityTooltipSubject.Primary);
-		if (m_healCasterOnIniialAttach)
+		if (m_healCasterOnIniialAttach && GetHealOnCasterPerTurn() > 0)
 		{
-			if (GetHealOnCasterPerTurn() > 0)
-			{
-				AbilityTooltipHelper.ReportHealing(ref number, AbilityTooltipSubject.Self, GetHealOnCasterPerTurn());
-			}
+			AbilityTooltipHelper.ReportHealing(ref number, AbilityTooltipSubject.Self, GetHealOnCasterPerTurn());
 		}
 		return number;
 	}
 
 	public override int GetAdditionalTechPointGainForNameplateItem(ActorData caster, int currentTargeterIndex)
 	{
-		int visibleActorsCountByTooltipSubject = base.Targeter.GetVisibleActorsCountByTooltipSubject(AbilityTooltipSubject.Enemy);
-		int result;
-		if (visibleActorsCountByTooltipSubject > 0)
+		if (Targeter.GetVisibleActorsCountByTooltipSubject(AbilityTooltipSubject.Enemy) > 0)
 		{
-			result = GetEnergyOnCasterPerTurn();
+			return GetEnergyOnCasterPerTurn();
 		}
-		else
-		{
-			result = 0;
-		}
-		return result;
+		return 0;
 	}
 
 	public override List<int> Debug_GetExpectedNumbersInTooltip()
@@ -398,15 +264,10 @@ public class SparkBasicAttack : Ability
 
 	protected override void OnApplyAbilityMod(AbilityMod abilityMod)
 	{
-		if (abilityMod.GetType() != typeof(AbilityMod_SparkBasicAttack))
-		{
-			return;
-		}
-		while (true)
+		if (abilityMod.GetType() == typeof(AbilityMod_SparkBasicAttack))
 		{
 			m_abilityMod = (abilityMod as AbilityMod_SparkBasicAttack);
 			Setup();
-			return;
 		}
 	}
 
