5b40d3cac0fa2fd60e001cc9f03c689c8babcbb9
Zhenёq <Zheneq@users.noreply.github.com>
2022-02-12 14:32:35 +0300

    Update SparkTetherDistanceMonitor.cs

--- Assembly-CSharp/SparkTetherDistanceMonitor.cs
+++ Assembly-CSharp/SparkTetherDistanceMonitor.cs
@@ -3,47 +3,24 @@ using UnityEngine;
 public class SparkTetherDistanceMonitor : MonoBehaviour, IGameEventListener
 {
 	private ProjectileWithTetherSequence m_lineFromCasterSequence;
-
 	private LineSequence m_lineFromTargetSequence;
-
 	private SparkBeamTrackerComponent m_syncComp;
-
 	private ActorData m_targetActor;
 
 	public void OnGameEvent(GameEventManager.EventType eventType, GameEventManager.GameEventArgs args)
 	{
-		if (eventType != GameEventManager.EventType.TheatricsEvasionMoveStart || !(m_syncComp != null))
+		if (eventType == GameEventManager.EventType.TheatricsEvasionMoveStart
+			&& m_syncComp != null
+			&& m_targetActor != null
+			&& m_syncComp.IsActorOutOfRangeForEvade(m_targetActor))
 		{
-			return;
-		}
-		while (true)
-		{
-			if (!(m_targetActor != null))
+			if (m_lineFromCasterSequence != null)
 			{
-				return;
+				m_lineFromCasterSequence.ForceHideLine();
 			}
-			while (true)
+			if (m_lineFromTargetSequence != null)
 			{
-				if (!m_syncComp.IsActorOutOfRangeForEvade(m_targetActor))
-				{
-					return;
-				}
-				while (true)
-				{
-					if (m_lineFromCasterSequence != null)
-					{
-						m_lineFromCasterSequence.ForceHideLine();
-					}
-					if (m_lineFromTargetSequence != null)
-					{
-						while (true)
-						{
-							m_lineFromTargetSequence.ForceHideLine();
-							return;
-						}
-					}
-					return;
-				}
+				m_lineFromTargetSequence.ForceHideLine();
 			}
 		}
 	}
@@ -52,13 +29,10 @@ public class SparkTetherDistanceMonitor : MonoBehaviour, IGameEventListener
 	{
 		m_lineFromCasterSequence = GetComponent<ProjectileWithTetherSequence>();
 		m_lineFromTargetSequence = GetComponent<LineSequence>();
-		if (m_lineFromCasterSequence != null)
+		if (m_lineFromCasterSequence != null && m_lineFromCasterSequence.Caster != null)
 		{
-			if (m_lineFromCasterSequence.Caster != null)
-			{
-				m_syncComp = m_lineFromCasterSequence.Caster.GetComponent<SparkBeamTrackerComponent>();
-				m_targetActor = m_lineFromCasterSequence.Target;
-			}
+			m_syncComp = m_lineFromCasterSequence.Caster.GetComponent<SparkBeamTrackerComponent>();
+			m_targetActor = m_lineFromCasterSequence.Target;
 		}
 		GameEventManager.Get().AddListener(this, GameEventManager.EventType.TheatricsEvasionMoveStart);
 	}
