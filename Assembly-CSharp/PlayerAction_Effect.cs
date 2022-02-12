8f3839c122c6041dc943f073c32522dd82277dfb
Zhenёq <Zheneq@users.noreply.github.com>
2022-02-12 18:38:31 +0300

    Rogues PlayerAction

---
+++ Assembly-CSharp/PlayerAction_Effect.cs
@@ -0,0 +1,113 @@
+﻿// ROGUES
+// SERVER
+using System.Collections.Generic;
+using System.Linq;
+using Theatrics;
+using UnityEngine;
+using UnityEngine.Networking;
+
+// rogues-only, missing in reactor
+#if SERVER
+public class PlayerAction_Effect : PlayerAction
+{
+	private List<EffectResults> m_requests;
+
+	private AbilityPriority m_phase;
+
+	public PlayerAction_Effect(List<global::Effect> requests, AbilityPriority phase)
+	{
+		this.m_requests = requests.SelectMany(delegate (global::Effect e)
+		{
+			// rogues
+			//EffectSystem.Effect effect;
+			//if ((effect = (e as EffectSystem.Effect)) != null)
+			//{
+			//    return effect.pendingEffectResults;
+			//}
+			return new List<EffectResults>
+				{
+					e.GetResultsForPhase(phase, true)
+				};
+		}).ToList<EffectResults>();
+		this.m_phase = phase;
+	}
+
+	public AbilityPriority GetRelevantPhase()
+	{
+		return this.m_phase;
+	}
+
+	public override bool ExecuteAction()
+	{
+		if (this.m_requests != null)
+		{
+			base.ExecuteAction();
+			AbilityPriority phase = this.m_phase;
+			bool flag = phase == AbilityPriority.Evasion;
+			bool flag2 = phase == AbilityPriority.Combat_Knockback;
+			List<ActorAnimation> list = new List<ActorAnimation>();
+			this.m_requests = (from effectResults in this.m_requests
+							   where effectResults.m_actorToHitResults.Any<KeyValuePair<ActorData, ActorHitResults>>() || effectResults.m_positionToHitResults.Any<KeyValuePair<Vector3, PositionHitResults>>() || effectResults.m_sequenceStartData.Any<ServerClientUtils.SequenceStartData>()
+							   select effectResults).ToList<EffectResults>();
+			sbyte b = 0;
+			foreach (EffectResults effectResults2 in this.m_requests)
+			{
+				if (effectResults2.Effect.AddActorAnimEntryIfHasHits(phase) || effectResults2.Effect.GetCasterAnimationIndex(phase) > 0)
+				{
+					ActorAnimation actorAnimation = new ActorAnimation(null, null, effectResults2);
+					actorAnimation.m_playOrderGroupIndex = 0;
+					if (flag || flag2)
+					{
+						actorAnimation.m_playOrderIndex = 0;
+					}
+					else
+					{
+						actorAnimation.m_playOrderIndex = b;
+					}
+					list.Add(actorAnimation);
+				}
+				b += 1;
+			}
+			ServerActionBuffer.Get().SynchronizePositionsOfActorsParticipatingInPhase(phase);
+			if (!NetworkClient.active)
+			{
+				PlayerAction_Ability.InitializeTheatricsForPhaseActions(phase, list);
+			}
+			if (flag2)
+			{
+				List<ActorData> actorsThatWillBeSeenButArentMoving;
+				ServerActionBuffer.Get().GetKnockbackManager().ProcessKnockbacks(new List<AbilityRequest>(), out actorsThatWillBeSeenButArentMoving);
+				ServerActionBuffer.Get().SynchronizePositionsOfActorsThatWillBeSeen(actorsThatWillBeSeenButArentMoving);
+			}
+			ServerResolutionManager.Get().SendEffectActionsToClients_FCFS(this.m_requests, list, phase);
+			return true;
+		}
+		return false;
+	}
+
+	public override void OnExecutionComplete(bool isLastAction)
+	{
+		foreach (EffectResults effectResults in this.m_requests)
+		{
+			// rogues
+			//if (isLastAction && !PlayerActionStateMachine.ConstructPlayerActionOnExecution())
+			//{
+			//	effectResults.Caster.GetActorTurnSM().OnMessage(TurnMessage.EXECUTE_ACTION_DONE, true);
+			//}
+			effectResults.OnExecutionComplete();
+		}
+		if (this.m_phase == AbilityPriority.Combat_Knockback)
+		{
+			ServerActionBuffer.Get().GetKnockbackManager().ClearStoredData();
+		}
+		if (ServerCombatManager.Get().HasUnresolvedHealthEntries())
+		{
+			ServerCombatManager.Get().ResolveHitPoints();
+		}
+		if (ServerCombatManager.Get().HasUnresolvedTechPointsEntries())
+		{
+			ServerCombatManager.Get().ResolveTechPoints();
+		}
+	}
+}
+#endif
---
+++ Assembly-CSharp/PlayerAction_Movement.cs
@@ -0,0 +1,122 @@
+﻿// ROGUES
+// SERVER
+using System.Collections.Generic;
+
+// rogues-only, missing in reactor
+#if SERVER
+public class PlayerAction_Movement : PlayerAction
+{
+	private List<MovementRequest> m_moveRequests;
+
+	public PlayerAction_Movement(List<MovementRequest> moveRequests)
+	{
+		m_moveRequests = moveRequests;
+	}
+
+	public override bool ExecuteAction()
+	{
+		if (m_moveRequests == null)
+		{
+			return false;
+		}
+		base.ExecuteAction();
+		for (int i = m_moveRequests.Count - 1; i >= 0; i--)
+		{
+			MovementRequest movementRequest = m_moveRequests[i];
+			if (movementRequest.m_actor.IsDead())
+			{
+				//ServerActionBuffer.Get().CancelMovementRequests(movementRequest.m_actor, false);
+				m_moveRequests.RemoveAt(i);
+			}
+		}
+		if (m_moveRequests.Count == 0)
+		{
+			return false;
+		}
+		List<MovementRequest> validRequests = new List<MovementRequest>();
+		foreach (MovementRequest movementRequest in m_moveRequests)
+		{
+			BoardSquare targetSquare = movementRequest.m_targetSquare;
+			if (movementRequest.m_path != null && movementRequest.m_path.next != null && targetSquare != null)
+			{
+				// rogues
+				//movementRequest.m_actor.GetActorTurnSM().OnMessage(TurnMessage.EXECUTE_ACTION_START, true);
+				validRequests.Add(movementRequest);
+			}
+		}
+		ServerActionBuffer.Get().GetMoveStabilizer().StabilizeMovement(validRequests, false);
+		for (int j = validRequests.Count - 1; j >= 0; j--)
+		{
+			MovementRequest movementRequest = validRequests[j];
+			if (movementRequest.m_path == null || movementRequest.m_path.next == null)
+			{
+				ServerActionBuffer.Get().CancelMovementRequests(movementRequest.m_actor, false);
+				movementRequest.m_actor.GetActorMovement().UpdateSquaresCanMoveTo();
+				validRequests.RemoveAt(j);
+			}
+		}
+		ServerActionBuffer.Get().ClearNormalMovementResults();
+		ServerGameplayUtils.GatherGameplayResultsForNormalMovement(validRequests, false);
+		MovementCollection movementCollection = new MovementCollection(validRequests);
+		foreach (ActorData actorData in GameFlowData.Get().GetActors())
+		{
+			if (actorData.GetPassiveData() != null)
+			{
+				actorData.GetPassiveData().OnMovementResultsGathered(movementCollection);
+			}
+			actorData.GetActorMovement().ClearPath();
+			actorData.UpdateServerLastVisibleTurn();
+		}
+		ServerGameplayUtils.SetServerLastKnownPositionsForMovement(movementCollection, out _, out _);
+		ServerResolutionManager.Get().OnNormalMovementStart();
+		ServerMovementManager.Get().ServerMovementManager_OnMovementStart(movementCollection, ServerMovementManager.MovementType.NormalMovement_NonChase);
+		foreach (MovementRequest movementRequest in validRequests)
+		{
+			ServerActionBuffer.Get().RunMovementOnRequest(movementRequest);
+			ActorStatus actorStatus = movementRequest.m_actor.GetActorStatus();
+			if (actorStatus != null && actorStatus.HasStatus(StatusType.KnockedBack, true))
+			{
+				actorStatus.RemoveStatus(StatusType.KnockedBack);
+			}
+			//float increment = movementRequest.m_path.FindMoveCostToEnd();
+			float num = movementRequest.m_actor.GetActorMovement().CalculateMaxHorizontalMovement(true, false);
+			BoardSquarePathInfo boardSquarePathInfo = movementRequest.m_path;
+			bool isRoundingDown = GameplayData.Get() != null && GameplayData.Get().m_movementMaximumType == GameplayData.MovementMaximumType.CannotExceedMax;
+			bool canContinue = true;
+			while (boardSquarePathInfo != null && canContinue)
+			{
+				if (isRoundingDown)
+				{
+					canContinue = boardSquarePathInfo.moveCost <= num;
+				}
+				else if (boardSquarePathInfo.next != null)
+				{
+					canContinue = boardSquarePathInfo.moveCost < num;
+				}
+				boardSquarePathInfo = boardSquarePathInfo.next;
+			}
+			//bool flag3 = !flag2;
+			// rogues
+			//movementRequest.m_actor.GetActorTurnSM().IncrementPveMoveCostUsed(increment);
+			//movementRequest.m_actor.GetActorTurnSM().IncrementPveNumMoveActions(flag3 ? 2 : 1);
+		}
+		return validRequests.Count > 0;
+	}
+
+	public override void OnExecutionComplete(bool isLastAction)
+	{
+		foreach (MovementRequest movementRequest in m_moveRequests)
+		{
+			ServerActionBuffer.Get().CancelMovementRequests(movementRequest.m_actor, false);
+		}
+		if (ServerCombatManager.Get().HasUnresolvedHealthEntries())
+		{
+			ServerCombatManager.Get().ResolveHitPoints();
+		}
+		if (ServerCombatManager.Get().HasUnresolvedTechPointsEntries())
+		{
+			ServerCombatManager.Get().ResolveTechPoints();
+		}
+	}
+}
+#endif
