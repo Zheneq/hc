5b0ee40cf8b606796b6b305adcece2457d42a677
Zhenёq <Zheneq@users.noreply.github.com>
2022-02-18 02:03:03 +0300

    Update VisionProviderInfo.cs

--- Assembly-CSharp/VisionProviderInfo.cs
+++ Assembly-CSharp/VisionProviderInfo.cs
@@ -12,23 +12,14 @@ public struct VisionProviderInfo
 	}
 
 	public int m_actorIndex;
-
 	public int m_satelliteIndex;
-
 	public int m_boardX;
-
 	public int m_boardY;
-
 	public float m_radius;
-
 	public bool m_radiusAsStraightLineDist;
-
 	public BoardSquare.VisibilityFlags m_flag;
-
 	public BrushRevealType m_brushRevealType;
-
 	public bool m_ignoreLos;
-
 	public bool m_canFunctionInGlobalBlind;
 
 	public VisionProviderInfo(GridPos gridPos, float r, bool radiusAsStraightLineDist, BrushRevealType brushRevealType, bool ignoreLos, bool canFunctionInGlobalBlind, BoardSquare.VisibilityFlags f = BoardSquare.VisibilityFlags.Team)
@@ -75,53 +66,32 @@ public struct VisionProviderInfo
 
 	public BoardSquare GetBoardSquare()
 	{
-		if (m_actorIndex != ActorData.s_invalidActorIndex)
+		if (m_actorIndex == ActorData.s_invalidActorIndex)
 		{
-			while (true)
+			return Board.Get().GetSquareFromIndex(m_boardX, m_boardY);
+		}
+		ActorData actorData = GameFlowData.Get().FindActorByActorIndex(m_actorIndex);
+		if (actorData == null)
+		{
+			return null;
+		}
+		BoardSquare result = null;
+		if (m_satelliteIndex == -1)
+		{
+			if (!actorData.IsDead())
 			{
-				switch (5)
-				{
-				case 0:
-					break;
-				default:
-				{
-					ActorData actorData = GameFlowData.Get().FindActorByActorIndex(m_actorIndex);
-					if (actorData == null)
-					{
-						return null;
-					}
-					BoardSquare result = null;
-					if (m_satelliteIndex == -1)
-					{
-						if (!actorData.IsDead())
-						{
-							result = actorData.GetCurrentBoardSquare();
-						}
-					}
-					else
-					{
-						SatelliteController component = actorData.GetComponent<SatelliteController>();
-						object obj;
-						if (component == null)
-						{
-							obj = null;
-						}
-						else
-						{
-							obj = component.GetSatellite(m_satelliteIndex);
-						}
-						PersistentSatellite persistentSatellite = (PersistentSatellite)obj;
-						if (persistentSatellite != null && persistentSatellite.IsVisible())
-						{
-							result = Board.Get().GetSquareFromVec3(persistentSatellite.transform.position);
-						}
-					}
-					return result;
-				}
-				}
+				result = actorData.GetCurrentBoardSquare();
 			}
 		}
-		return Board.Get().GetSquareFromIndex(m_boardX, m_boardY);
+		else
+		{
+			PersistentSatellite persistentSatellite = actorData.GetComponent<SatelliteController>()?.GetSatellite(m_satelliteIndex);
+			if (persistentSatellite != null && persistentSatellite.IsVisible())
+			{
+				result = Board.Get().GetSquareFromVec3(persistentSatellite.transform.position);
+			}
+		}
+		return result;
 	}
 
 	public bool IsEqual(VisionProviderInfo other)
@@ -131,75 +101,33 @@ public struct VisionProviderInfo
 
 	public bool IsEqual(GridPos gridPos, float r, bool useSraightLineDist, BrushRevealType brushRevealType, bool ignoreLos, BoardSquare.VisibilityFlags f, bool canFunctionInGlobalBlind)
 	{
-		int result;
-		if (m_actorIndex == ActorData.s_invalidActorIndex)
-		{
-			if (m_boardX == gridPos.x && m_boardY == gridPos.y)
-			{
-				result = (HasSameProperties(r, useSraightLineDist, f, brushRevealType, ignoreLos, canFunctionInGlobalBlind) ? 1 : 0);
-				goto IL_0053;
-			}
-		}
-		result = 0;
-		goto IL_0053;
-		IL_0053:
-		return (byte)result != 0;
+		return m_actorIndex == ActorData.s_invalidActorIndex
+			&& m_boardX == gridPos.x
+			&& m_boardY == gridPos.y
+			&& HasSameProperties(r, useSraightLineDist, f, brushRevealType, ignoreLos, canFunctionInGlobalBlind);
 	}
 
 	public bool IsEqual(int actorIdx, float r, bool useSraightLineDist, BrushRevealType brushRevealType, bool ignoreLos, BoardSquare.VisibilityFlags f, bool canFunctionInGlobalBlind)
 	{
-		int result;
-		if (m_actorIndex == actorIdx)
-		{
-			if (m_satelliteIndex == -1)
-			{
-				result = (HasSameProperties(r, useSraightLineDist, f, brushRevealType, ignoreLos, canFunctionInGlobalBlind) ? 1 : 0);
-				goto IL_0044;
-			}
-		}
-		result = 0;
-		goto IL_0044;
-		IL_0044:
-		return (byte)result != 0;
+		return m_actorIndex == actorIdx
+			&& m_satelliteIndex == -1
+			&& HasSameProperties(r, useSraightLineDist, f, brushRevealType, ignoreLos, canFunctionInGlobalBlind);
 	}
 
 	public bool IsEqual(int actorIdx, int satelliteIdx, float r, bool useSraightLineDist, BrushRevealType brushRevealType, bool ignoreLos, BoardSquare.VisibilityFlags f, bool canFunctionInGlobalBlind)
 	{
-		int result;
-		if (m_actorIndex == actorIdx)
-		{
-			if (m_satelliteIndex == satelliteIdx)
-			{
-				result = (HasSameProperties(r, useSraightLineDist, f, brushRevealType, ignoreLos, canFunctionInGlobalBlind) ? 1 : 0);
-				goto IL_0045;
-			}
-		}
-		result = 0;
-		goto IL_0045;
-		IL_0045:
-		return (byte)result != 0;
+		return m_actorIndex == actorIdx
+			&& m_satelliteIndex == satelliteIdx
+			&& HasSameProperties(r, useSraightLineDist, f, brushRevealType, ignoreLos, canFunctionInGlobalBlind);
 	}
 
 	private bool HasSameProperties(float r, bool useSraightLineDist, BoardSquare.VisibilityFlags f, BrushRevealType brushRevealType, bool ignoreLos, bool canFunctionInGlobalBlind)
 	{
-		int result;
-		if (m_flag == f)
-		{
-			if (m_brushRevealType == brushRevealType)
-			{
-				if (m_ignoreLos == ignoreLos)
-				{
-					if (m_canFunctionInGlobalBlind == canFunctionInGlobalBlind && m_radiusAsStraightLineDist == useSraightLineDist)
-					{
-						result = ((Mathf.Abs(m_radius - r) < 0.05f) ? 1 : 0);
-						goto IL_007d;
-					}
-				}
-			}
-		}
-		result = 0;
-		goto IL_007d;
-		IL_007d:
-		return (byte)result != 0;
+		return m_flag == f
+			&& m_brushRevealType == brushRevealType
+			&& m_ignoreLos == ignoreLos
+			&& m_canFunctionInGlobalBlind == canFunctionInGlobalBlind
+			&& m_radiusAsStraightLineDist == useSraightLineDist
+			&& Mathf.Abs(m_radius - r) < 0.05f;
 	}
 }
