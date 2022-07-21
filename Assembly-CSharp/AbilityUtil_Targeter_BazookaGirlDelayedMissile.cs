02bbb497f247325e68910d95c023ea783a5daf42
Zhenёq <Zheneq@users.noreply.github.com>
2022-07-22 02:12:09 +0300

    Update Targeters

--- Assembly-CSharp/AbilityUtil_Targeter_BazookaGirlDelayedMissile.cs
+++ Assembly-CSharp/AbilityUtil_Targeter_BazookaGirlDelayedMissile.cs
@@ -6,19 +6,38 @@ public class AbilityUtil_Targeter_BazookaGirlDelayedMissile : AbilityUtil_Target
 	private bool m_useInnerShape;
 	private AbilityAreaShape m_innerShape;
 
-	public AbilityUtil_Targeter_BazookaGirlDelayedMissile(Ability ability, AbilityAreaShape outerShape, bool penetrateLoS, bool useInnerShape, AbilityAreaShape innerShape, DamageOriginType damageOriginType = DamageOriginType.CenterOfShape, bool affectsEnemies = true, bool affectsAllies = false)
+	public AbilityUtil_Targeter_BazookaGirlDelayedMissile(
+		Ability ability,
+		AbilityAreaShape outerShape,
+		bool penetrateLoS,
+		bool useInnerShape,
+		AbilityAreaShape innerShape,
+		DamageOriginType damageOriginType = DamageOriginType.CenterOfShape,
+		bool affectsEnemies = true,
+		bool affectsAllies = false)
 		: base(ability, outerShape, penetrateLoS, damageOriginType, affectsEnemies, affectsAllies)
 	{
 		m_useInnerShape = useInnerShape;
 		m_innerShape = innerShape;
 	}
 
-	protected override bool HandleAddActorInShape(ActorData potentialTarget, ActorData targetingActor, AbilityTarget currentTarget, Vector3 damageOrigin, ActorData bestTarget)
+	protected override bool HandleAddActorInShape(
+		ActorData potentialTarget,
+		ActorData targetingActor,
+		AbilityTarget currentTarget,
+		Vector3 damageOrigin,
+		ActorData bestTarget)
 	{
 		base.HandleAddActorInShape(potentialTarget, targetingActor, currentTarget, damageOrigin, bestTarget);
 		if (m_useInnerShape)
 		{
-			List<ActorData> actors = AreaEffectUtils.GetActorsInShape(m_innerShape, currentTarget, m_penetrateLoS, targetingActor, GetAffectedTeams(), null);
+			List<ActorData> actors = AreaEffectUtils.GetActorsInShape(
+				m_innerShape,
+				currentTarget,
+				m_penetrateLoS,
+				targetingActor,
+				GetAffectedTeams(),
+				null);
 			TargeterUtils.RemoveActorsInvisibleToClient(ref actors);
 			if (actors.Contains(potentialTarget))
 			{
