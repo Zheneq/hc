fb88cd0bec1dedc5099531cdb63651e4bbe51b5a
Zhenёq <Zheneq@users.noreply.github.com>
2022-08-13 15:40:23 +0300

    Rogues MatchObjective

--- Assembly-CSharp/MatchObjective.cs
+++ Assembly-CSharp/MatchObjective.cs
@@ -1,12 +1,41 @@
+﻿// ROGUES
+// SERVER
 using UnityEngine;
 
-public class MatchObjective : MonoBehaviour
+public class MatchObjective : MonoBehaviour //, IMissionTagged in rogues
 {
+	// rogues
+	// public string RequiredMissionTag;
+
 	public virtual void Client_OnActorDeath(ActorData actor)
 	{
 	}
 
+	// rogues
+	// public virtual void Server_OnActorRevived(ActorData actor)
+	// {
+	// }
+
 	public virtual void Server_OnActorDeath(ActorData actor)
 	{
 	}
+
+	// rogues
+	// public List<string> GetRelevantTags()
+	// {
+	// 	if (!RequiredMissionTag.IsNullOrEmpty())
+	// 	{
+	// 		return new List<string>
+	// 		{
+	// 			RequiredMissionTag
+	// 		};
+	// 	}
+	// 	return new List<string>();
+	// }
+
+	// rogues
+	// public bool MatchesTag(string tag)
+	// {
+	// 	return !RequiredMissionTag.IsNullOrEmpty() && RequiredMissionTag.EqualsIgnoreCase(tag);
+	// }
 }
