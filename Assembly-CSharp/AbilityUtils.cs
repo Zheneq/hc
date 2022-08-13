4bb042a437a64f0e4c54596d4546483ab1a25d89
Zhenёq <Zheneq@users.noreply.github.com>
2022-08-13 10:50:03 +0300

    Update AbilityUtils.cs

--- Assembly-CSharp/AbilityUtils.cs
+++ Assembly-CSharp/AbilityUtils.cs
@@ -6,13 +6,9 @@ public static class AbilityUtils
 	public class CustomTargetDamageableResult
 	{
 		public ActorData m_caster;
-
 		public DamageSource m_src;
-
 		public bool m_targetInCover;
-
 		public bool m_damageable;
-
 		public string m_failReason;
 
 		public CustomTargetDamageableResult(ActorData caster, DamageSource src, bool targetInCover)
@@ -29,53 +25,38 @@ public static class AbilityUtils
 
 	public static bool AbilityHasTag(Ability ability, AbilityTags tag)
 	{
-		bool result = false;
-		if (ability != null)
+		if (ability == null)
 		{
-			bool flag = ability.CurrentAbilityMod != null && ability.CurrentAbilityMod.m_tagsModType == AbilityMod.TagOverrideType.Override;
-			bool flag2 = ability.CurrentAbilityMod != null && ability.CurrentAbilityMod.m_tagsModType == AbilityMod.TagOverrideType.Append;
-			if (flag)
-			{
-				result = HasTagInList(tag, ability.CurrentAbilityMod.m_abilityTagsInMod);
-			}
-			else
-			{
-				int num;
-				if (!HasTagInList(tag, ability.m_tags))
-				{
-					if (flag2)
-					{
-						num = (HasTagInList(tag, ability.CurrentAbilityMod.m_abilityTagsInMod) ? 1 : 0);
-					}
-					else
-					{
-						num = 0;
-					}
-				}
-				else
-				{
-					num = 1;
-				}
-				result = ((byte)num != 0);
-			}
+			return false;
+		}
+		bool isOverride = ability.CurrentAbilityMod != null
+		                  && ability.CurrentAbilityMod.m_tagsModType == AbilityMod.TagOverrideType.Override;
+		bool isAppend = ability.CurrentAbilityMod != null
+		                && ability.CurrentAbilityMod.m_tagsModType == AbilityMod.TagOverrideType.Append;
+		bool result;
+		if (isOverride)
+		{
+			result = HasTagInList(tag, ability.CurrentAbilityMod.m_abilityTagsInMod);
+		}
+		else
+		{
+			result = HasTagInList(tag, ability.m_tags)
+			         || isAppend && HasTagInList(tag, ability.CurrentAbilityMod.m_abilityTagsInMod);
 		}
 		return result;
 	}
 
 	private static bool HasTagInList(AbilityTags tagToFind, List<AbilityTags> listToCheck)
 	{
-		if (listToCheck != null)
+		if (listToCheck == null)
 		{
-			for (int i = 0; i < listToCheck.Count; i++)
+			return false;
+		}
+		foreach (AbilityTags t in listToCheck)
+		{
+			if (t == tagToFind)
 			{
-				if (listToCheck[i] != tagToFind)
-				{
-					continue;
-				}
-				while (true)
-				{
-					return true;
-				}
+				return true;
 			}
 		}
 		return false;
@@ -83,40 +64,21 @@ public static class AbilityUtils
 
 	public static bool AbilityIgnoreCover(Ability ability, ActorData targetActor)
 	{
-		bool result = false;
-		if (ability != null)
+		if (ability == null)
 		{
-			int num;
-			if (!AbilityHasTag(ability, AbilityTags.IgnoreCover))
-			{
-				num = (ability.ForceIgnoreCover(targetActor) ? 1 : 0);
-			}
-			else
-			{
-				num = 1;
-			}
-			result = ((byte)num != 0);
+			return false;
 		}
-		return result;
+		return AbilityHasTag(ability, AbilityTags.IgnoreCover) || ability.ForceIgnoreCover(targetActor);
 	}
 
 	public static bool AbilityReduceCoverEffectiveness(Ability ability, ActorData targetActor)
 	{
-		bool result = false;
-		if (ability != null)
+		if (ability == null)
 		{
-			int num;
-			if (!AbilityHasTag(ability, AbilityTags.ReduceCoverByHalf))
-			{
-				num = (ability.ForceReduceCoverEffectiveness(targetActor) ? 1 : 0);
-			}
-			else
-			{
-				num = 1;
-			}
-			result = ((byte)num != 0);
+			return false;
 		}
-		return result;
+		return AbilityHasTag(ability, AbilityTags.ReduceCoverByHalf)
+		       || ability.ForceReduceCoverEffectiveness(targetActor);
 	}
 
 	public static AbilityPriority GetHighestAbilityPriority()
@@ -131,39 +93,31 @@ public static class AbilityUtils
 
 	public static AbilityPriority GetNextAbilityPriority(AbilityPriority previousPriority)
 	{
-		AbilityPriority result = GetLowestAbilityPriority();
 		switch (previousPriority)
 		{
 		case AbilityPriority.Prep_Defense:
-			result = AbilityPriority.Prep_Offense;
-			break;
+			return AbilityPriority.Prep_Offense;
 		case AbilityPriority.Prep_Offense:
-			result = AbilityPriority.Evasion;
-			break;
+			return AbilityPriority.Evasion;
 		case AbilityPriority.Evasion:
-			result = AbilityPriority.Combat_Damage;
-			break;
+			return AbilityPriority.Combat_Damage;
 		case AbilityPriority.Combat_Damage:
-			result = AbilityPriority.DEPRICATED_Combat_Charge;
-			break;
+			return AbilityPriority.DEPRICATED_Combat_Charge;
 		case AbilityPriority.DEPRICATED_Combat_Charge:
-			result = AbilityPriority.Combat_Knockback;
-			break;
+			return AbilityPriority.Combat_Knockback;
 		case AbilityPriority.Combat_Knockback:
-			result = AbilityPriority.Combat_Final;
-			break;
+			return AbilityPriority.Combat_Final;
 		case AbilityPriority.Combat_Final:
-			result = AbilityPriority.Combat_Final;
-			break;
+			return AbilityPriority.Combat_Final;
 		}
-		return result;
+		return GetLowestAbilityPriority();
 	}
 
 	public static float GetCurrentRangeInSquares(Ability ability, ActorData caster, int targetIndex)
 	{
 		float rangeInSquares = ability.GetRangeInSquares(targetIndex);
-		float num;
-		float result;
+		float num = 0f;
+		float result = 0f;
 		if (caster != null)
 		{
 			ActorStats actorStats = caster.GetActorStats();
@@ -175,14 +129,6 @@ public static class AbilityUtils
 				num = Mathf.Max(modifiedStatFloat - statBaseValueFloat, 0f);
 				result = actorMovement.CalculateMaxHorizontalMovement();
 			}
-			else
-			{
-				result = (num = 0f);
-			}
-		}
-		else
-		{
-			result = (num = 0f);
 		}
 		if (AbilityHasTag(ability, AbilityTags.SetRangeToCurrentMovement))
 		{
@@ -190,16 +136,7 @@ public static class AbilityUtils
 		}
 		if (AbilityHasTag(ability, AbilityTags.AddBonusMovementToRange))
 		{
-			while (true)
-			{
-				switch (3)
-				{
-				case 0:
-					break;
-				default:
-					return rangeInSquares + num;
-				}
-			}
+			return rangeInSquares + num;
 		}
 		return rangeInSquares;
 	}
@@ -215,71 +152,83 @@ public static class AbilityUtils
 		Board board = Board.Get();
 		float currentRangeInSquares = GetCurrentRangeInSquares(ability, caster, targetIndex);
 		Ability.TargetingParadigm targetingParadigm = ability.GetTargetingParadigm(targetIndex);
-		if (currentRangeInSquares >= 0f)
+		if (currentRangeInSquares < 0f)
 		{
-			if (targetingParadigm != Ability.TargetingParadigm.BoardSquare)
+			return hashSet;
+		}
+		if (targetingParadigm != Ability.TargetingParadigm.BoardSquare
+		    && targetingParadigm != Ability.TargetingParadigm.Position)
+		{
+			return hashSet;
+		}
+		int x1 = Mathf.Max(0, Mathf.FloorToInt(caster.GetGridPos().x - currentRangeInSquares));
+		int x2 = Mathf.Min(board.GetMaxX(), Mathf.CeilToInt(caster.GetGridPos().x + currentRangeInSquares) + 1);
+		int y1 = Mathf.Max(0, Mathf.FloorToInt(caster.GetGridPos().y - currentRangeInSquares));
+		int y2 = Mathf.Min(board.GetMaxY(), Mathf.CeilToInt(caster.GetGridPos().y + currentRangeInSquares) + 1);
+		float currentMinRangeInSquares = GetCurrentMinRangeInSquares(ability, caster, targetIndex);
+		float currentMaxRangeInSquares = GetCurrentRangeInSquares(ability, caster, targetIndex);
+		AbilityTarget abilityTarget = AbilityTarget.CreateSimpleAbilityTarget(caster);
+		for (int i = x1; i < x2; i++)
+		{
+			for (int j = y1; j < y2; j++)
 			{
-				if (targetingParadigm != Ability.TargetingParadigm.Position)
+				BoardSquare boardSquare = board.GetSquareFromIndex(i, j);
+				abilityTarget.SetValuesFromBoardSquare(boardSquare, caster.GetFreePos());
+				if (abilityData.ValidateAbilityOnTarget(
+					    ability,
+					    abilityTarget,
+					    targetIndex,
+					    currentMinRangeInSquares,
+					    currentMaxRangeInSquares))
 				{
-					goto IL_0186;
-				}
-			}
-			int num = Mathf.Max(0, Mathf.FloorToInt((float)caster.GetGridPos().x - currentRangeInSquares));
-			int num2 = Mathf.Min(board.GetMaxX(), Mathf.CeilToInt((float)caster.GetGridPos().x + currentRangeInSquares) + 1);
-			int num3 = Mathf.Max(0, Mathf.FloorToInt((float)caster.GetGridPos().y - currentRangeInSquares));
-			int num4 = Mathf.Min(board.GetMaxY(), Mathf.CeilToInt((float)caster.GetGridPos().y + currentRangeInSquares) + 1);
-			float currentMinRangeInSquares = GetCurrentMinRangeInSquares(ability, caster, targetIndex);
-			float currentRangeInSquares2 = GetCurrentRangeInSquares(ability, caster, targetIndex);
-			AbilityTarget abilityTarget = AbilityTarget.CreateSimpleAbilityTarget(caster);
-			for (int i = num; i < num2; i++)
-			{
-				for (int j = num3; j < num4; j++)
-				{
-					BoardSquare boardSquare = board.GetSquareFromIndex(i, j);
-					abilityTarget.SetValuesFromBoardSquare(boardSquare, caster.GetFreePos());
-					if (abilityData.ValidateAbilityOnTarget(ability, abilityTarget, targetIndex, currentMinRangeInSquares, currentRangeInSquares2))
-					{
-						hashSet.Add(boardSquare);
-					}
+					hashSet.Add(boardSquare);
 				}
 			}
 		}
-		goto IL_0186;
-		IL_0186:
 		return hashSet;
 	}
 
-	public static int GetTechPointRewardForInteraction(Ability ability, AbilityInteractionType interaction, bool firstTime, bool hitOnAlly = false, bool hitOnEnemy = false)
+	public static int GetTechPointRewardForInteraction(
+		Ability ability,
+		AbilityInteractionType interaction,
+		bool firstTime,
+		bool hitOnAlly = false,
+		bool hitOnEnemy = false)
 	{
 		int reward = 0;
 		HashSet<TechPointInteractionType> hashSet = new HashSet<TechPointInteractionType>();
 		TechPointInteraction[] baseTechPointInteractions = ability.GetBaseTechPointInteractions();
 		foreach (TechPointInteraction techPointInteraction in baseTechPointInteractions)
 		{
-			if (hashSet.Contains(techPointInteraction.m_type))
+			if (!hashSet.Contains(techPointInteraction.m_type))
 			{
-				continue;
+				hashSet.Add(techPointInteraction.m_type);
+				int techPoints = techPointInteraction.m_amount;
+				if (ability.CurrentAbilityMod != null)
+				{
+					techPoints = ability.CurrentAbilityMod.GetModdedTechPointForInteraction(techPointInteraction.m_type, techPoints);
+				}
+				AddToRewardAmountForInteraction(ref reward, techPointInteraction.m_type, interaction, techPoints, firstTime, hitOnAlly, hitOnEnemy);
 			}
-			hashSet.Add(techPointInteraction.m_type);
-			int num = techPointInteraction.m_amount;
-			if (ability.CurrentAbilityMod != null)
-			{
-				num = ability.CurrentAbilityMod.GetModdedTechPointForInteraction(techPointInteraction.m_type, num);
-			}
-			AddToRewardAmountForInteraction(ref reward, techPointInteraction.m_type, interaction, num, firstTime, hitOnAlly, hitOnEnemy);
 		}
 		if (ability.CurrentAbilityMod != null)
 		{
-			TechPointInteractionMod[] techPointInteractionMods = ability.CurrentAbilityMod.m_techPointInteractionMods;
-			foreach (TechPointInteractionMod techPointInteractionMod in techPointInteractionMods)
+			foreach (TechPointInteractionMod techPointInteractionMod in ability.CurrentAbilityMod.m_techPointInteractionMods)
 			{
 				if (!hashSet.Contains(techPointInteractionMod.interactionType))
 				{
 					hashSet.Add(techPointInteractionMod.interactionType);
-					int moddedTechPointForInteraction = ability.CurrentAbilityMod.GetModdedTechPointForInteraction(techPointInteractionMod.interactionType, 0);
-					if (moddedTechPointForInteraction > 0)
+					int techPoints = ability.CurrentAbilityMod.GetModdedTechPointForInteraction(techPointInteractionMod.interactionType, 0);
+					if (techPoints > 0)
 					{
-						AddToRewardAmountForInteraction(ref reward, techPointInteractionMod.interactionType, interaction, moddedTechPointForInteraction, firstTime, hitOnAlly, hitOnEnemy);
+						AddToRewardAmountForInteraction(
+							ref reward,
+							techPointInteractionMod.interactionType,
+							interaction,
+							techPoints,
+							firstTime,
+							hitOnAlly,
+							hitOnEnemy);
 					}
 				}
 			}
@@ -287,7 +236,14 @@ public static class AbilityUtils
 		return reward;
 	}
 
-	private static void AddToRewardAmountForInteraction(ref int reward, TechPointInteractionType techInteractionType, AbilityInteractionType interaction, int addAmount, bool firstTime, bool hitOnAlly, bool hitOnEnemy)
+	private static void AddToRewardAmountForInteraction(
+		ref int reward,
+		TechPointInteractionType techInteractionType,
+		AbilityInteractionType interaction,
+		int addAmount,
+		bool firstTime,
+		bool hitOnAlly,
+		bool hitOnEnemy)
 	{
 		switch (techInteractionType)
 		{
@@ -347,12 +303,12 @@ public static class AbilityUtils
 	{
 		bool isInCover = targetInCover && !AbilityIgnoreCover(ability, target);
 		bool isUnpreventable = ability.IsDamageUnpreventable();
-		ActorStatus tartgetStatus = target.GetActorStatus();
-		if (tartgetStatus.HasStatus(StatusType.DamageImmune) && !isUnpreventable)
+		ActorStatus targetStatus = target.GetActorStatus();
+		if (targetStatus.HasStatus(StatusType.DamageImmune) && !isUnpreventable)
 		{
 			return 0;
 		}
-		if (tartgetStatus.HasStatus(StatusType.ImmuneToPlayerDamage) && caster.IsHumanControlled() && !isUnpreventable)
+		if (targetStatus.HasStatus(StatusType.ImmuneToPlayerDamage) && caster.IsHumanControlled() && !isUnpreventable)
 		{
 			return 0;
 		}
@@ -360,7 +316,7 @@ public static class AbilityUtils
 		ActorStats targetStats = target.GetActorStats();
 		if (GameplayMutators.Get() != null)
 		{
-			baseDamage = Mathf.RoundToInt((float)baseDamage * GameplayMutators.GetDamageMultiplier());
+			baseDamage = Mathf.RoundToInt(baseDamage * GameplayMutators.GetDamageMultiplier());
 		}
 		if (!AbilityHasTag(ability, AbilityTags.IgnoreOutgoingDamageHealAbsorbBuffsAndDebuffs))
 		{
@@ -377,18 +333,19 @@ public static class AbilityUtils
 
 	public static int CalculateHealingForTargeter(ActorData caster, ActorData target, Ability ability, int baseHeal)
 	{
-		int num = baseHeal;
 		if (target.GetActorStatus().HasStatus(StatusType.HealImmune))
 		{
 			return 0;
 		}
-		if (caster != target && caster.GetTeam() == target.GetTeam() && target.GetActorStatus().HasStatus(StatusType.CantBeHelpedByTeam))
+		if (caster != target
+		    && caster.GetTeam() == target.GetTeam()
+		    && target.GetActorStatus().HasStatus(StatusType.CantBeHelpedByTeam))
 		{
 			return 0;
 		}
 		if (GameplayMutators.Get() != null)
 		{
-			baseHeal = Mathf.RoundToInt((float)baseHeal * GameplayMutators.GetHealingMultiplier());
+			baseHeal = Mathf.RoundToInt(baseHeal * GameplayMutators.GetHealingMultiplier());
 		}
 		if (AbilityHasTag(ability, AbilityTags.IgnoreOutgoingDamageHealAbsorbBuffsAndDebuffs))
 		{
@@ -406,13 +363,15 @@ public static class AbilityUtils
 		{
 			return 0;
 		}
-		if (caster != target && caster.GetTeam() == target.GetTeam() && target.GetActorStatus().HasStatus(StatusType.CantBeHelpedByTeam))
+		if (caster != target
+		    && caster.GetTeam() == target.GetTeam()
+		    && target.GetActorStatus().HasStatus(StatusType.CantBeHelpedByTeam))
 		{
 			return 0;
 		}
 		if (GameplayMutators.Get() != null)
 		{
-			baseAbsorb = Mathf.RoundToInt((float)baseAbsorb * GameplayMutators.GetAbsorbMultiplier());
+			baseAbsorb = Mathf.RoundToInt(baseAbsorb * GameplayMutators.GetAbsorbMultiplier());
 		}
 		if (AbilityHasTag(ability, AbilityTags.IgnoreOutgoingDamageHealAbsorbBuffsAndDebuffs))
 		{
@@ -428,55 +387,29 @@ public static class AbilityUtils
 	{
 		if (GameplayMutators.Get() != null)
 		{
-			baseGain = MathUtil.RoundToIntPadded((float)baseGain * GameplayMutators.GetEnergyGainMultiplier());
+			baseGain = MathUtil.RoundToIntPadded(baseGain * GameplayMutators.GetEnergyGainMultiplier());
 		}
-		int a = baseGain;
+		int finalGain = baseGain;
 		ActorStatus actorStatus = target.GetActorStatus();
-		bool flag = actorStatus.IsEnergized();
-		bool flag2 = actorStatus.HasStatus(StatusType.SlowEnergyGain);
-		AbilityModPropertyInt energizedEnergyGainMod;
-		if (flag)
+		bool isEnergized = actorStatus.IsEnergized();
+		bool isSlowEnergyGain = actorStatus.HasStatus(StatusType.SlowEnergyGain);
+		if (isEnergized && !isSlowEnergyGain)
 		{
-			if (!flag2)
-			{
-				if (!(GameplayMutators.Get() == null))
-				{
-					if (GameplayMutators.Get().m_useEnergizedOverride)
-					{
-						energizedEnergyGainMod = GameplayMutators.Get().m_energizedEnergyGainMod;
-						goto IL_00b1;
-					}
-				}
-				energizedEnergyGainMod = GameWideData.Get().m_energizedEnergyGainMod;
-				goto IL_00b1;
-			}
+			AbilityModPropertyInt energizedEnergyGainMod =
+				GameplayMutators.Get() != null && GameplayMutators.Get().m_useEnergizedOverride
+				? GameplayMutators.Get().m_energizedEnergyGainMod
+				: GameWideData.Get().m_energizedEnergyGainMod;
+			finalGain = energizedEnergyGainMod.GetModifiedValue(baseGain);
 		}
-		AbilityModPropertyInt slowEnergyGainEnergyGainMod;
-		if (!flag)
+		else if (!isEnergized && isSlowEnergyGain)
 		{
-			if (flag2)
-			{
-				if (!(GameplayMutators.Get() == null))
-				{
-					if (GameplayMutators.Get().m_useSlowEnergyGainOverride)
-					{
-						slowEnergyGainEnergyGainMod = GameplayMutators.Get().m_slowEnergyGainEnergyGainMod;
-						goto IL_012c;
-					}
-				}
-				slowEnergyGainEnergyGainMod = GameWideData.Get().m_slowEnergyGainEnergyGainMod;
-				goto IL_012c;
-			}
+			AbilityModPropertyInt slowEnergyGainEnergyGainMod =
+				GameplayMutators.Get() != null && GameplayMutators.Get().m_useSlowEnergyGainOverride
+					? GameplayMutators.Get().m_slowEnergyGainEnergyGainMod
+					: GameWideData.Get().m_slowEnergyGainEnergyGainMod;
+			finalGain = slowEnergyGainEnergyGainMod.GetModifiedValue(baseGain);
 		}
-		goto IL_0135;
-		IL_0135:
-		return Mathf.Max(a, 0);
-		IL_00b1:
-		a = energizedEnergyGainMod.GetModifiedValue(baseGain);
-		goto IL_0135;
-		IL_012c:
-		a = slowEnergyGainEnergyGainMod.GetModifiedValue(baseGain);
-		goto IL_0135;
+		return Mathf.Max(finalGain, 0);
 	}
 
 	public static int ApplyCoverDamageReduction(ActorStats targetStats, int initialDamage, bool reducedCoverEffectiveness)
@@ -486,21 +419,21 @@ public static class AbilityUtils
 		{
 			num = Mathf.Min(num * 1.5f, 1f);
 		}
-		return Mathf.RoundToInt((float)initialDamage * num);
+		return Mathf.RoundToInt(initialDamage * num);
 	}
 
 	public static int GetEnemyCount(List<ActorData> actorsToConsider, ActorData observingActor)
 	{
-		int num = 0;
-		if (actorsToConsider != null)
+		if (actorsToConsider == null)
 		{
-			for (int i = 0; i < actorsToConsider.Count; i++)
+			return 0;
+		}
+		int num = 0;
+		foreach (ActorData actorData in actorsToConsider)
+		{
+			if (actorData.GetTeam() != observingActor.GetTeam())
 			{
-				ActorData actorData = actorsToConsider[i];
-				if (actorData.GetTeam() != observingActor.GetTeam())
-				{
-					num++;
-				}
+				num++;
 			}
 		}
 		return num;
@@ -508,23 +441,15 @@ public static class AbilityUtils
 
 	public static int GetAllyCount(List<ActorData> actorsToConsider, ActorData observingActor, bool includeSelf)
 	{
-		int num = 0;
-		if (actorsToConsider != null)
+		if (actorsToConsider == null)
 		{
-			for (int i = 0; i < actorsToConsider.Count; i++)
+			return 0;
+		}
+		int num = 0;
+		foreach (ActorData actorData in actorsToConsider)
+		{
+			if (actorData.GetTeam() == observingActor.GetTeam() && (includeSelf || observingActor != actorData))
 			{
-				ActorData actorData = actorsToConsider[i];
-				if (actorData.GetTeam() != observingActor.GetTeam())
-				{
-					continue;
-				}
-				if (!includeSelf)
-				{
-					if (!(observingActor != actorData))
-					{
-						continue;
-					}
-				}
 				num++;
 			}
 		}
