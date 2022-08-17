1d63657d0d0a18b6b68c8f36182190c3fb6ed37b
Zhenёq <Zheneq@users.noreply.github.com>
2022-08-17 03:29:26 +0300

    Update MatchResultsNotification.cs

--- Assembly-CSharp/LobbyGameClientMessages/MatchResultsNotification.cs
+++ Assembly-CSharp/LobbyGameClientMessages/MatchResultsNotification.cs
@@ -10,66 +10,37 @@ namespace LobbyGameClientMessages
 		public class CurrencyReward
 		{
 			public CurrencyType Type;
-
 			public int BaseGained;
-
 			public int EventGained;
-
 			public int WinGained;
-
 			public int QuestGained;
-
 			public int GGGained;
-
 			public int LevelUpGained;
 		}
 
 		public List<CurrencyReward> CurrencyRewards;
-
 		public int BaseXpGained;
-
 		public int WinXpGained;
-
 		public int GGXpGained;
-
 		public int ConsumableXpGained;
-
 		public int PlayWithFriendXpGained;
-
 		public int QuestXpGained;
-
 		public int EventBonusXpGained;
-
 		public int FirstWinXpGained;
-
 		public int QueueTimeXpGained;
-
 		public int FreelancerOwnedXPGained;
-
 		public int AccountLevelAtStart = 1;
-
 		public int CharacterLevelAtStart = 1;
-
 		public int AccountXpAtStart;
-
 		public int CharacterXpAtStart;
-
 		public int SeasonLevelAtStart;
-
 		public int SeasonXpAtStart;
-
 		public int FactionCompetitionId;
-
 		public int FactionId;
-
 		public Dictionary<string, int> FactionContributionAmounts;
-
 		public List<BadgeAndParticipantInfo> BadgeAndParticipantsInfo;
-
 		public bool FirstWinOccured;
-
 		public float TotalBadgePoints;
-
 		public int NumCharactersPlayed;
 	}
 }
