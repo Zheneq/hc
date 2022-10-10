f3ae276335db0b47ca58de464934a7a5a4d5c601
Zhenёq <Zheneq@users.noreply.github.com>
2022-10-10 22:03:09 +0300

    Update TeamSensitiveDataMatchmaker.cs

--- Assembly-CSharp/TeamSensitiveDataMatchmaker.cs
+++ Assembly-CSharp/TeamSensitiveDataMatchmaker.cs
@@ -6,7 +6,6 @@ public class TeamSensitiveDataMatchmaker
 	private static TeamSensitiveDataMatchmaker s_instance;
 
 	private Dictionary<int, ActorTeamSensitiveData> m_actorIndexToFriendlyData;
-
 	private Dictionary<int, ActorTeamSensitiveData> m_actorIndexToHostileData;
 
 	private TeamSensitiveDataMatchmaker()
@@ -28,31 +27,18 @@ public class TeamSensitiveDataMatchmaker
 	{
 		if (NetworkServer.active)
 		{
-			while (true)
-			{
-				switch (5)
-				{
-				case 0:
-					break;
-				default:
-					return;
-				}
-			}
+			return;
 		}
+		
 		if (m_actorIndexToFriendlyData.ContainsKey(actor.ActorIndex))
 		{
 			actor.SetClientFriendlyTeamSensitiveData(m_actorIndexToFriendlyData[actor.ActorIndex]);
 			m_actorIndexToFriendlyData.Remove(actor.ActorIndex);
 		}
-		if (!m_actorIndexToHostileData.ContainsKey(actor.ActorIndex))
-		{
-			return;
-		}
-		while (true)
+		if (m_actorIndexToHostileData.ContainsKey(actor.ActorIndex))
 		{
 			actor.SetClientHostileTeamSensitiveData(m_actorIndexToHostileData[actor.ActorIndex]);
 			m_actorIndexToHostileData.Remove(actor.ActorIndex);
-			return;
 		}
 	}
 
@@ -62,57 +48,16 @@ public class TeamSensitiveDataMatchmaker
 		{
 			return;
 		}
-		while (true)
+		if (GameFlowData.Get() == null)
 		{
-			int num;
-			if (m_actorIndexToFriendlyData != null)
+			return;
+		}
+		if (m_actorIndexToFriendlyData != null && m_actorIndexToFriendlyData.Count > 0
+		    || m_actorIndexToHostileData != null && m_actorIndexToHostileData.Count > 0)
+		{
+			foreach (ActorData actor in GameFlowData.Get().GetActors())
 			{
-				if (m_actorIndexToFriendlyData.Count > 0)
-				{
-					num = 1;
-					goto IL_006f;
-				}
-			}
-			if (m_actorIndexToHostileData != null)
-			{
-				num = ((m_actorIndexToHostileData.Count > 0) ? 1 : 0);
-			}
-			else
-			{
-				num = 0;
-			}
-			goto IL_006f;
-			IL_006f:
-			bool flag = (byte)num != 0;
-			if (!(GameFlowData.Get() != null))
-			{
-				return;
-			}
-			while (true)
-			{
-				if (flag)
-				{
-					List<ActorData> actors = GameFlowData.Get().GetActors();
-					using (List<ActorData>.Enumerator enumerator = actors.GetEnumerator())
-					{
-						while (enumerator.MoveNext())
-						{
-							ActorData current = enumerator.Current;
-							SetTeamSensitiveDataForActor(current);
-						}
-						while (true)
-						{
-							switch (4)
-							{
-							default:
-								return;
-							case 0:
-								break;
-							}
-						}
-					}
-				}
-				return;
+				SetTeamSensitiveDataForActor(actor);
 			}
 		}
 	}
@@ -121,38 +66,25 @@ public class TeamSensitiveDataMatchmaker
 	{
 		if (NetworkServer.active)
 		{
-			while (true)
-			{
-				switch (1)
-				{
-				case 0:
-					break;
-				default:
-					return;
-				}
-			}
-		}
-		Dictionary<int, ActorTeamSensitiveData> dictionary;
-		if (teamSensitiveData.m_typeObservingMe == ActorTeamSensitiveData.ObservedBy.Friendlies)
-		{
-			dictionary = m_actorIndexToFriendlyData;
-		}
-		else if (teamSensitiveData.m_typeObservingMe == ActorTeamSensitiveData.ObservedBy.Hostiles)
-		{
-			dictionary = m_actorIndexToHostileData;
-		}
-		else
-		{
-			dictionary = null;
-		}
-		if (dictionary == null || dictionary.ContainsKey(teamSensitiveData.ActorIndex))
-		{
 			return;
 		}
-		while (true)
+		
+		Dictionary<int, ActorTeamSensitiveData> dictionary;
+		switch (teamSensitiveData.m_typeObservingMe)
+		{
+			case ActorTeamSensitiveData.ObservedBy.Friendlies:
+				dictionary = m_actorIndexToFriendlyData;
+				break;
+			case ActorTeamSensitiveData.ObservedBy.Hostiles:
+				dictionary = m_actorIndexToHostileData;
+				break;
+			default:
+				dictionary = null;
+				break;
+		}
+		if (dictionary != null && !dictionary.ContainsKey(teamSensitiveData.ActorIndex))
 		{
 			dictionary.Add(teamSensitiveData.ActorIndex, teamSensitiveData);
-			return;
 		}
 	}
 }
