7eea6fb10cacbf1a1ce5b34572f92c964b7ad3bd
Zhenёq <Zheneq@users.noreply.github.com>
2022-10-22 09:16:32 +0300

    Update PowerUps

--- Assembly-CSharp/PowerUpLocation.cs
+++ Assembly-CSharp/PowerUpLocation.cs
@@ -2,34 +2,21 @@ using UnityEngine;
 
 public class PowerUpLocation : MonoBehaviour
 {
-	private BoardSquare m_boardSquare;
+    private BoardSquare m_boardSquare;
 
-	public BoardSquare boardSquare => m_boardSquare;
+    public BoardSquare boardSquare => m_boardSquare;
 
-	public void Initialize()
-	{
-		Board board = Board.Get();
-		Vector3 position = base.transform.position;
-		float x = position.x;
-		Vector3 position2 = base.transform.position;
-		m_boardSquare = board.GetSquareFromPos(x, position2.z);
-	}
+    public void Initialize()
+    {
+        Vector3 position = transform.position;
+        m_boardSquare = Board.Get().GetSquareFromPos(position.x, position.z);
+    }
 
-	private void OnDrawGizmos()
-	{
-		if (!CameraManager.ShouldDrawGizmosForCurrentCamera())
-		{
-			while (true)
-			{
-				switch (4)
-				{
-				case 0:
-					break;
-				default:
-					return;
-				}
-			}
-		}
-		Gizmos.DrawIcon(base.transform.position, "icon_PowerUp.png");
-	}
+    private void OnDrawGizmos()
+    {
+        if (CameraManager.ShouldDrawGizmosForCurrentCamera())
+        {
+            Gizmos.DrawIcon(transform.position, "icon_PowerUp.png");
+        }
+    }
 }
--- Assembly-CSharp/PowerUpSpawnerPlacement.cs
+++ Assembly-CSharp/PowerUpSpawnerPlacement.cs
@@ -3,34 +3,19 @@ using UnityEngine;
 public class PowerUpSpawnerPlacement : MonoBehaviour, IGameEventListener
 {
 	public PowerUpSpawner m_powerUpSpawnerPrefab;
-
 	private PowerUpSpawner m_powerUpSpawnerInstance;
-
 	public int m_spawnInterval = 2;
-
 	public int m_initialSpawnDelay = 1;
-
 	[Tooltip("Objects=everyone can get it")]
 	public Team m_teamRestriction = Team.Objects;
-
 	public string[] m_tagsToApplyToPowerup;
 
 	private void OnDrawGizmos()
 	{
-		if (!CameraManager.ShouldDrawGizmosForCurrentCamera())
+		if (CameraManager.ShouldDrawGizmosForCurrentCamera())
 		{
-			while (true)
-			{
-				switch (3)
-				{
-				case 0:
-					break;
-				default:
-					return;
-				}
-			}
+			Gizmos.DrawIcon(base.transform.position, "icon_PowerUp.png");
 		}
-		Gizmos.DrawIcon(base.transform.position, "icon_PowerUp.png");
 	}
 
 	void IGameEventListener.OnGameEvent(GameEventManager.EventType eventType, GameEventManager.GameEventArgs args)
