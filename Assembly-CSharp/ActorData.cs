88d2583f6afa8d4e6d6311371a9cca8532a51903
Zhenёq <Zheneq@users.noreply.github.com>
2023-01-07 11:09:44 +0300

    Revert "Server: Enemy position updating fix"
    This reverts commit 470b32832451fc25b5a80d70d9aae7a71a4f96e0.

--- Assembly-CSharp/ActorData.cs
+++ Assembly-CSharp/ActorData.cs
@@ -5181,7 +5181,7 @@ public class ActorData : NetworkBehaviour, IGameEventListener
 		// custom
 		BoardSquare prevLastKnownSquare = ServerLastKnownPosSquare;
 		ServerLastKnownPosSquare = GetSquareAtPhaseStart();
-		if (ServerLastKnownPosSquare != null)
+		if (ServerLastKnownPosSquare != null && prevLastKnownSquare != ServerLastKnownPosSquare)
 		{
 			TeamSensitiveData_hostile.BroadcastMovement(
 				GameEventManager.EventType.ClientResolutionStarted,
