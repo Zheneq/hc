42f00b9a3f28a1c89dc992022a26040085f19ba5
Zhenёq <Zheneq@users.noreply.github.com>
2023-01-15 17:53:53 +0300

    Update RageBeast

--- Assembly-CSharp/AbilityMod_RageBeastKnockback.cs
+++ Assembly-CSharp/AbilityMod_RageBeastKnockback.cs
@@ -6,27 +6,18 @@ public class AbilityMod_RageBeastKnockback : AbilityMod
 {
 	[Header("-- Targeting Mod")]
 	public AbilityModPropertyInt m_maxTargetMod;
-
 	public AbilityModPropertyFloat m_targeterWidthMod;
-
 	public AbilityModPropertyFloat m_targeterLengthMod;
-
 	[Header("-- Damage Mod")]
 	public AbilityModPropertyInt m_onHitDamageMod;
-
 	[Header("-- UNUSED: Damage Mods from Knockback Move")]
 	public AbilityModPropertyInt m_collisionDamageToSelfMod;
-
 	public AbilityModPropertyInt m_collisionDamageToOtherMod;
-
 	public AbilityModPropertyInt m_collisionDamageFromGeoMod;
-
 	[Header("-- UNUSED Effect on Geo Collision")]
 	public StandardEffectInfo m_effectOnGeoCollision;
-
 	[Header("-- Knockback Mod")]
 	public AbilityModPropertyFloat m_knockbackDistanceMinMod;
-
 	public AbilityModPropertyFloat m_knockbackDistanceMaxMod;
 
 	public override Type GetTargetAbilityType()
@@ -37,91 +28,31 @@ public class AbilityMod_RageBeastKnockback : AbilityMod
 	protected override void AddModSpecificTooltipTokens(List<TooltipTokenEntry> tokens, Ability targetAbility)
 	{
 		RageBeastKnockback rageBeastKnockback = targetAbility as RageBeastKnockback;
-		if (!(rageBeastKnockback != null))
+		if (rageBeastKnockback != null)
 		{
-			return;
-		}
-		while (true)
-		{
-			AbilityMod.AddToken(tokens, m_targeterWidthMod, "LaserWidth", string.Empty, rageBeastKnockback.m_laserWidth);
-			AbilityMod.AddToken(tokens, m_targeterLengthMod, "LaserDistance", string.Empty, rageBeastKnockback.m_laserDistance);
-			AbilityMod.AddToken(tokens, m_maxTargetMod, "MaxTargets", string.Empty, rageBeastKnockback.m_maxTargets);
-			AbilityMod.AddToken(tokens, m_knockbackDistanceMinMod, "KnockbackMinDistance", string.Empty, rageBeastKnockback.m_knockbackDistanceMin);
-			AbilityMod.AddToken(tokens, m_knockbackDistanceMaxMod, "KnockbackMaxDistance", string.Empty, rageBeastKnockback.m_knockbackDistanceMax);
-			AbilityMod.AddToken(tokens, m_onHitDamageMod, "DamageAmount", string.Empty, rageBeastKnockback.m_damageAmount);
-			return;
+			AddToken(tokens, m_targeterWidthMod, "LaserWidth", string.Empty, rageBeastKnockback.m_laserWidth);
+			AddToken(tokens, m_targeterLengthMod, "LaserDistance", string.Empty, rageBeastKnockback.m_laserDistance);
+			AddToken(tokens, m_maxTargetMod, "MaxTargets", string.Empty, rageBeastKnockback.m_maxTargets);
+			AddToken(tokens, m_knockbackDistanceMinMod, "KnockbackMinDistance", string.Empty, rageBeastKnockback.m_knockbackDistanceMin);
+			AddToken(tokens, m_knockbackDistanceMaxMod, "KnockbackMaxDistance", string.Empty, rageBeastKnockback.m_knockbackDistanceMax);
+			AddToken(tokens, m_onHitDamageMod, "DamageAmount", string.Empty, rageBeastKnockback.m_damageAmount);
 		}
 	}
 
 	protected override string ModSpecificAutogenDesc(AbilityData abilityData)
 	{
 		RageBeastKnockback rageBeastKnockback = GetTargetAbilityOnAbilityData(abilityData) as RageBeastKnockback;
-		bool flag = rageBeastKnockback != null;
-		string empty = string.Empty;
-		string str = empty;
-		AbilityModPropertyInt maxTargetMod = m_maxTargetMod;
-		int baseVal;
-		if (flag)
-		{
-			baseVal = rageBeastKnockback.m_maxTargets;
-		}
-		else
-		{
-			baseVal = 0;
-		}
-		empty = str + AbilityModHelper.GetModPropertyDesc(maxTargetMod, "[Max Targets]", flag, baseVal);
-		string str2 = empty;
-		AbilityModPropertyFloat targeterWidthMod = m_targeterWidthMod;
-		float baseVal2;
-		if (flag)
-		{
-			baseVal2 = rageBeastKnockback.m_laserWidth;
-		}
-		else
-		{
-			baseVal2 = 0f;
-		}
-		empty = str2 + AbilityModHelper.GetModPropertyDesc(targeterWidthMod, "[Targeter Width]", flag, baseVal2);
-		string str3 = empty;
-		AbilityModPropertyFloat targeterLengthMod = m_targeterLengthMod;
-		float baseVal3;
-		if (flag)
-		{
-			baseVal3 = rageBeastKnockback.m_laserDistance;
-		}
-		else
-		{
-			baseVal3 = 0f;
-		}
-		empty = str3 + AbilityModHelper.GetModPropertyDesc(targeterLengthMod, "[Targeter Length]", flag, baseVal3);
-		empty += AbilityModHelper.GetModPropertyDesc(m_onHitDamageMod, "[On Hit Damage]", flag, flag ? rageBeastKnockback.m_damageAmount : 0);
-		empty += AbilityModHelper.GetModPropertyDesc(m_collisionDamageToSelfMod, "[ !!!UNUSED: Collision Damage to Mover]", flag, flag ? rageBeastKnockback.m_damageToMoverOnCollision : 0);
-		string str4 = empty;
-		AbilityModPropertyInt collisionDamageToOtherMod = m_collisionDamageToOtherMod;
-		int baseVal4;
-		if (flag)
-		{
-			baseVal4 = rageBeastKnockback.m_damageToOtherOnCollision;
-		}
-		else
-		{
-			baseVal4 = 0;
-		}
-		empty = str4 + AbilityModHelper.GetModPropertyDesc(collisionDamageToOtherMod, "[ !!!UNUSED: Collision Damage to Other]", flag, baseVal4);
-		empty += AbilityModHelper.GetModPropertyDesc(m_collisionDamageFromGeoMod, "[ !!!UNUSED: Collision Damage from Geo]", flag, flag ? rageBeastKnockback.m_damageCollisionWithGeo : 0);
-		empty += AbilityModHelper.GetModEffectInfoDesc(m_effectOnGeoCollision, "[Effect on Geo Collision]", string.Empty, flag);
-		string str5 = empty;
-		AbilityModPropertyFloat knockbackDistanceMinMod = m_knockbackDistanceMinMod;
-		float baseVal5;
-		if (flag)
-		{
-			baseVal5 = rageBeastKnockback.m_knockbackDistanceMin;
-		}
-		else
-		{
-			baseVal5 = 0f;
-		}
-		empty = str5 + AbilityModHelper.GetModPropertyDesc(knockbackDistanceMinMod, "[Knockback Min Distance]", flag, baseVal5);
-		return empty + AbilityModHelper.GetModPropertyDesc(m_knockbackDistanceMaxMod, "[Knockback Max Distance]", flag, (!flag) ? 0f : rageBeastKnockback.m_knockbackDistanceMax);
+		bool isValid = rageBeastKnockback != null;
+		string desc = string.Empty;
+		desc += AbilityModHelper.GetModPropertyDesc(m_maxTargetMod, "[Max Targets]", isValid, isValid ? rageBeastKnockback.m_maxTargets : 0);
+		desc += AbilityModHelper.GetModPropertyDesc(m_targeterWidthMod, "[Targeter Width]", isValid, isValid ? rageBeastKnockback.m_laserWidth : 0f);
+		desc += AbilityModHelper.GetModPropertyDesc(m_targeterLengthMod, "[Targeter Length]", isValid, isValid ? rageBeastKnockback.m_laserDistance : 0f);
+		desc += AbilityModHelper.GetModPropertyDesc(m_onHitDamageMod, "[On Hit Damage]", isValid, isValid ? rageBeastKnockback.m_damageAmount : 0);
+		desc += AbilityModHelper.GetModPropertyDesc(m_collisionDamageToSelfMod, "[ !!!UNUSED: Collision Damage to Mover]", isValid, isValid ? rageBeastKnockback.m_damageToMoverOnCollision : 0);
+		desc += AbilityModHelper.GetModPropertyDesc(m_collisionDamageToOtherMod, "[ !!!UNUSED: Collision Damage to Other]", isValid, isValid ? rageBeastKnockback.m_damageToOtherOnCollision : 0);
+		desc += AbilityModHelper.GetModPropertyDesc(m_collisionDamageFromGeoMod, "[ !!!UNUSED: Collision Damage from Geo]", isValid, isValid ? rageBeastKnockback.m_damageCollisionWithGeo : 0);
+		desc += AbilityModHelper.GetModEffectInfoDesc(m_effectOnGeoCollision, "[Effect on Geo Collision]", string.Empty, isValid);
+		desc += AbilityModHelper.GetModPropertyDesc(m_knockbackDistanceMinMod, "[Knockback Min Distance]", isValid, isValid ? rageBeastKnockback.m_knockbackDistanceMin : 0f);
+		return desc + AbilityModHelper.GetModPropertyDesc(m_knockbackDistanceMaxMod, "[Knockback Max Distance]", isValid, isValid ? rageBeastKnockback.m_knockbackDistanceMax : 0f);
 	}
 }
--- Assembly-CSharp/AbilityMod_RageBeastUltimate.cs
+++ Assembly-CSharp/AbilityMod_RageBeastUltimate.cs
@@ -6,20 +6,14 @@ public class AbilityMod_RageBeastUltimate : AbilityMod
 {
 	[Header("-- Plasma Damage/Duration Mod")]
 	public AbilityModPropertyInt m_plasmaDamageMod;
-
 	public AbilityModPropertyInt m_plasmaDurationMod;
-
 	[Header("-- Passive TechPoint Regen (while Mod is applied)")]
 	public int m_passiveTechPointRegen;
-
 	[Header("-- Ally Boosts While In Plasma")]
 	public AbilityModPropertyInt m_plasmaHealingMod;
-
 	public AbilityModPropertyInt m_plasmaTechPointGainMod;
-
 	[Header("-- Extra effect on Self")]
 	public AbilityModPropertyInt m_selfHealOnCastMod;
-
 	public AbilityModPropertyEffectInfo m_extraEffectOnSelfMod;
 
 	public override Type GetTargetAbilityType()
@@ -30,68 +24,29 @@ public class AbilityMod_RageBeastUltimate : AbilityMod
 	protected override void AddModSpecificTooltipTokens(List<TooltipTokenEntry> tokens, Ability targetAbility)
 	{
 		RageBeastUltimate rageBeastUltimate = targetAbility as RageBeastUltimate;
-		if (!(rageBeastUltimate != null))
+		if (rageBeastUltimate != null)
 		{
-			return;
-		}
-		while (true)
-		{
-			AbilityMod.AddToken(tokens, m_plasmaDamageMod, "PlasmaDamage", string.Empty, rageBeastUltimate.m_plasmaDamage);
-			AbilityMod.AddToken(tokens, m_plasmaDurationMod, "PlasmaDuration", string.Empty, rageBeastUltimate.m_plasmaDuration);
-			AbilityMod.AddToken(tokens, m_selfHealOnCastMod, "SelfHealOnCast", string.Empty, rageBeastUltimate.m_selfHealOnCast);
-			AbilityMod.AddToken_EffectMod(tokens, m_extraEffectOnSelfMod, "ExtraEffectOnSelf", rageBeastUltimate.m_extraEffectOnSelf);
-			return;
+			AddToken(tokens, m_plasmaDamageMod, "PlasmaDamage", string.Empty, rageBeastUltimate.m_plasmaDamage);
+			AddToken(tokens, m_plasmaDurationMod, "PlasmaDuration", string.Empty, rageBeastUltimate.m_plasmaDuration);
+			AddToken(tokens, m_selfHealOnCastMod, "SelfHealOnCast", string.Empty, rageBeastUltimate.m_selfHealOnCast);
+			AddToken_EffectMod(tokens, m_extraEffectOnSelfMod, "ExtraEffectOnSelf", rageBeastUltimate.m_extraEffectOnSelf);
 		}
 	}
 
 	protected override string ModSpecificAutogenDesc(AbilityData abilityData)
 	{
 		RageBeastUltimate rageBeastUltimate = GetTargetAbilityOnAbilityData(abilityData) as RageBeastUltimate;
-		bool flag = rageBeastUltimate != null;
-		string empty = string.Empty;
-		string str = empty;
-		AbilityModPropertyInt plasmaDamageMod = m_plasmaDamageMod;
-		int baseVal;
-		if (flag)
-		{
-			baseVal = rageBeastUltimate.m_plasmaDamage;
-		}
-		else
-		{
-			baseVal = 0;
-		}
-		empty = str + PropDesc(plasmaDamageMod, "[Plasma Damage]", flag, baseVal);
-		empty += PropDesc(m_plasmaDurationMod, "[Plasma Duration]", flag, flag ? rageBeastUltimate.m_plasmaDuration : 0);
+		bool isValid = rageBeastUltimate != null;
+		string desc = string.Empty;
+		desc += PropDesc(m_plasmaDamageMod, "[Plasma Damage]", isValid, isValid ? rageBeastUltimate.m_plasmaDamage : 0);
+		desc += PropDesc(m_plasmaDurationMod, "[Plasma Duration]", isValid, isValid ? rageBeastUltimate.m_plasmaDuration : 0);
 		if (m_passiveTechPointRegen > 0)
 		{
-			string text = empty;
-			empty = text + "Passive TechPoint Regen while has Mod: " + m_passiveTechPointRegen + "\n";
+			desc += "Passive TechPoint Regen while has Mod: " + m_passiveTechPointRegen + "\n";
 		}
-		empty += PropDesc(m_plasmaHealingMod, "[Plasma Ally Healing]", flag);
-		empty += PropDesc(m_plasmaTechPointGainMod, "[Plasma Ally Tech Point Gain]", flag);
-		string str2 = empty;
-		AbilityModPropertyInt selfHealOnCastMod = m_selfHealOnCastMod;
-		int baseVal2;
-		if (flag)
-		{
-			baseVal2 = rageBeastUltimate.m_selfHealOnCast;
-		}
-		else
-		{
-			baseVal2 = 0;
-		}
-		empty = str2 + PropDesc(selfHealOnCastMod, "[SelfHealOnCast]", flag, baseVal2);
-		string str3 = empty;
-		AbilityModPropertyEffectInfo extraEffectOnSelfMod = m_extraEffectOnSelfMod;
-		object baseVal3;
-		if (flag)
-		{
-			baseVal3 = rageBeastUltimate.m_extraEffectOnSelf;
-		}
-		else
-		{
-			baseVal3 = null;
-		}
-		return str3 + PropDesc(extraEffectOnSelfMod, "[ExtraEffectOnSelf]", flag, (StandardEffectInfo)baseVal3);
+		desc += PropDesc(m_plasmaHealingMod, "[Plasma Ally Healing]", isValid);
+		desc += PropDesc(m_plasmaTechPointGainMod, "[Plasma Ally Tech Point Gain]", isValid);
+		desc += PropDesc(m_selfHealOnCastMod, "[SelfHealOnCast]", isValid, isValid ? rageBeastUltimate.m_selfHealOnCast : 0);
+		return desc + PropDesc(m_extraEffectOnSelfMod, "[ExtraEffectOnSelf]", isValid, isValid ? rageBeastUltimate.m_extraEffectOnSelf : null);
 	}
 }
