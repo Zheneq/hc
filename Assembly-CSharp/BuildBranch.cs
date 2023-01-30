28bff88acb7b74ee0edb2cf0dd3385f5538c473f
Zhenёq <Zheneq@users.noreply.github.com>
2023-01-30 20:37:47 +0300

    Update Build info

--- Assembly-CSharp/BuildBranch.cs
+++ Assembly-CSharp/BuildBranch.cs
@@ -1,6 +1,5 @@
 public static class BuildBranch
 {
 	private const string s_branchName = "stable";
-
 	public static string BranchName => "stable".ToUpper();
 }
--- Assembly-CSharp/BuildVersion.cs
+++ Assembly-CSharp/BuildVersion.cs
@@ -5,57 +5,44 @@ public static class BuildVersion
 	public const string BuildDescriptionPrefixString = "Hydrogen Version: ";
 
 	private const int s_releaseNumber = 122;
-
 	private const int s_buildNumber = 100;
-
 	private const string s_buildLetter = "k";
-
 	private const string s_changelistNumber = "265834";
 
 	public static int ReleaseNumber => 122;
-
 	public static int BuildNumber => 100;
-
 	public static string BuildLetter => "k".ToUpper();
-
 	public static string BranchName => BuildBranch.BranchName;
-
 	public static string ChangelistNumber => "265834";
-
 	public static string MiniVersionString => $"{BranchName}-{ReleaseNumber}";
-
 	public static string ShortVersionString => $"{BranchName}-{ReleaseNumber}-{BuildNumber}";
-
 	public static string FullVersionString => $"{BranchName}-{ReleaseNumber}-{BuildNumber}-{BuildLetter}-{ChangelistNumber}";
 
 	public static string GetBuildDescriptionString(DateTime buildDate = default(DateTime), string buildHostName = null)
 	{
-		string text = string.Format("{0}{1}", "Hydrogen Version: ", FullVersionString);
+		string text = $"Hydrogen Version: {FullVersionString}";
 		if (buildDate != DateTime.MinValue)
 		{
 			TimeSpan timeSpan = DateTime.UtcNow - buildDate;
-			string arg;
+			string timespanString;
 			if (timeSpan.TotalHours < 1.0)
 			{
-				arg = $"{timeSpan.TotalMinutes:0} minutes ago";
+				timespanString = $"{timeSpan.TotalMinutes:0} minutes ago";
 			}
 			else if (timeSpan.TotalDays < 1.0)
 			{
-				arg = $"{timeSpan.TotalHours:0} hours ago";
+				timespanString = $"{timeSpan.TotalHours:0} hours ago";
 			}
 			else
 			{
-				arg = $"{timeSpan.TotalDays:0} days ago";
+				timespanString = $"{timeSpan.TotalDays:0} days ago";
 			}
-			string arg2 = ((DateTimeOffset)buildDate).ToLocalTime().ToString();
-			text += $", built {arg} at {arg2}";
+			string dateString = ((DateTimeOffset)buildDate).ToLocalTime().ToString();
+			text += $", built {timespanString} at {dateString}";
 		}
-		if (!buildHostName.IsNullOrEmpty())
+		if (!buildHostName.IsNullOrEmpty() && buildHostName != "unknown")
 		{
-			if (buildHostName != "unknown")
-			{
-				text += $" on {buildHostName}";
-			}
+			text += $" on {buildHostName}";
 		}
 		return text;
 	}
