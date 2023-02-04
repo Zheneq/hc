36d388b59a17901a60c9b1fa23561cf82ecc2816
Zhenёq <Zheneq@users.noreply.github.com>
2023-02-05 01:19:07 +0300

    Server: ScoundrelEvasionRoll cooldown reduction on damage

--- Assembly-CSharp/Passive_Scoundrel.cs
+++ Assembly-CSharp/Passive_Scoundrel.cs
@@ -1,20 +1,30 @@
 ﻿// ROGUES
 // SERVER
+using UnityEngine;
+
 public class Passive_Scoundrel : Passive
 {
 	public int m_trapwireLastCastTurn = -1;
 	public bool m_trapwireDidDamage;
 #if SERVER
+	// custom
+	public bool m_damagedThisTurn;
+	
 	// added in rogues
 	private ScoundrelTrapWire m_trapwireAbility;
+	// custom
+	private AbilityData m_abilityData;
+	// custom
+	private static readonly AbilityData.ActionType s_evasionAbilityType = AbilityData.ActionType.ABILITY_3;
 
 	// added in rogues
 	protected override void OnStartup()
 	{
 		AbilityData abilityData = Owner.GetAbilityData();
+		m_abilityData = abilityData; // custom
 		if (abilityData != null)
 		{
-			m_trapwireAbility = (abilityData.GetAbilityOfType(typeof(ScoundrelTrapWire)) as ScoundrelTrapWire);
+			m_trapwireAbility = abilityData.GetAbilityOfType(typeof(ScoundrelTrapWire)) as ScoundrelTrapWire;
 		}
 	}
 
@@ -28,6 +38,13 @@ public class Passive_Scoundrel : Passive
 		}
 	}
 
+	// custom
+	public override void OnDamaged(ActorData damageCaster, DamageSource damageSource, int damageAmount)
+	{
+		base.OnDamaged(damageCaster, damageSource, damageAmount);
+		m_damagedThisTurn = true;
+	}
+
 	// added in rogues
 	public override void OnTurnStart()
 	{
@@ -39,6 +56,19 @@ public class Passive_Scoundrel : Passive
 		{
 			m_trapwireAbility.TrapwireExpiredWithoutHitting();
 		}
+		
+		// custom 
+		if (m_damagedThisTurn)
+		{
+			int cd = m_abilityData.GetCooldownRemaining(s_evasionAbilityType);
+			if (cd > 0)
+			{
+				cd = Mathf.Max(0, cd - 1);
+				m_abilityData.OverrideCooldown(s_evasionAbilityType, cd);
+			}
+		}
+		m_damagedThisTurn = false;
+		// end custom
 	}
 #endif
 }
