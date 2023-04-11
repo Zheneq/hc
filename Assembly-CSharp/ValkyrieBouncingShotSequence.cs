bf25d1778ef62975b2e965a18abb66f02d2bfce2
Zhenёq <Zheneq@users.noreply.github.com>
2023-04-12 00:23:56 +0200

    Update Valkyrie Sequences

--- Assembly-CSharp/ValkyrieBouncingShotSequence.cs
+++ Assembly-CSharp/ValkyrieBouncingShotSequence.cs
@@ -5,12 +5,9 @@ public class ValkyrieBouncingShotSequence : BouncingShotSequence
 {
 	[Header("-- Valkyrie specific stuff --")]
 	public GameObject m_returnProjectileVfxPrefab;
-
 	public float m_endingHeightOffset;
-
 	[AudioEvent(false)]
 	public string m_beginReturnAudioEvent;
-
 	[AudioEvent(false)]
 	public string m_endReturnAudioEvent;
 
@@ -20,68 +17,45 @@ public class ValkyrieBouncingShotSequence : BouncingShotSequence
 	{
 		int curSegment = m_curSegment;
 		base.UpdateProjectileFX();
-		if (!(m_fx != null) || m_segmentPts.IsNullOrEmpty())
+		if (m_fx == null || m_segmentPts.IsNullOrEmpty())
 		{
 			return;
 		}
-		while (true)
+		Animator modelAnimator = Caster.GetModelAnimator();
+		modelAnimator.SetFloat(animDistToGoal, m_totalTravelDistance - m_distanceTraveled);
+		if (m_curSegment != curSegment && m_curSegment == m_segmentPts.Count - 2)
 		{
-			Animator modelAnimator = base.Caster.GetModelAnimator();
-			modelAnimator.SetFloat(animDistToGoal, m_totalTravelDistance - m_distanceTraveled);
-			if (m_curSegment != curSegment && m_curSegment == m_segmentPts.Count - 2)
+			if (m_returnProjectileVfxPrefab != null)
 			{
-				while (true)
+				bool sequenceVisibility = m_fx != null && m_fx.activeInHierarchy;
+				Destroy(m_fx);
+				Quaternion rotation = default(Quaternion);
+				rotation.SetLookRotation((m_segmentPts[m_curSegment + 1] - m_segmentPts[m_curSegment]).normalized);
+				m_fx = InstantiateFX(m_returnProjectileVfxPrefab, m_segmentPts[m_curSegment], rotation);
+				SetSequenceVisibility(sequenceVisibility);
+				if (m_fx != null && m_projectileFxAttributes != null)
 				{
-					switch (5)
+					foreach (KeyValuePair<string, float> projectileFxAttribute in m_projectileFxAttributes)
 					{
-					case 0:
-						break;
-					default:
-					{
-						if (m_returnProjectileVfxPrefab != null)
-						{
-							bool sequenceVisibility = m_fx != null && m_fx.activeInHierarchy;
-							Object.Destroy(m_fx);
-							Quaternion rotation = default(Quaternion);
-							rotation.SetLookRotation((m_segmentPts[m_curSegment + 1] - m_segmentPts[m_curSegment]).normalized);
-							m_fx = InstantiateFX(m_returnProjectileVfxPrefab, m_segmentPts[m_curSegment], rotation);
-							SetSequenceVisibility(sequenceVisibility);
-							if (m_fx != null)
-							{
-								if (m_projectileFxAttributes != null)
-								{
-									foreach (KeyValuePair<string, float> projectileFxAttribute in m_projectileFxAttributes)
-									{
-										Sequence.SetAttribute(m_fx, projectileFxAttribute.Key, projectileFxAttribute.Value);
-									}
-								}
-							}
-						}
-						else
-						{
-							Sequence.SetAttribute(m_fx, "projectileReturning", 1);
-						}
-						if (!string.IsNullOrEmpty(m_beginReturnAudioEvent))
-						{
-							AudioManager.PostEvent(m_beginReturnAudioEvent, m_fx.gameObject);
-						}
-						Vector3 value = m_segmentPts[m_segmentPts.Count - 1];
-						value.y += m_endingHeightOffset;
-						m_segmentPts[m_segmentPts.Count - 1] = value;
-						return;
-					}
+						SetAttribute(m_fx, projectileFxAttribute.Key, projectileFxAttribute.Value);
 					}
 				}
 			}
-			if (m_reachedDestination && !string.IsNullOrEmpty(m_endReturnAudioEvent))
+			else
 			{
-				while (true)
-				{
-					AudioManager.PostEvent(m_endReturnAudioEvent, base.Caster.gameObject);
-					return;
-				}
+				SetAttribute(m_fx, "projectileReturning", 1);
 			}
-			return;
+			if (!string.IsNullOrEmpty(m_beginReturnAudioEvent))
+			{
+				AudioManager.PostEvent(m_beginReturnAudioEvent, m_fx.gameObject);
+			}
+			Vector3 segmentPoint = m_segmentPts[m_segmentPts.Count - 1];
+			segmentPoint.y += m_endingHeightOffset;
+			m_segmentPts[m_segmentPts.Count - 1] = segmentPoint;
+		}
+		else if (m_reachedDestination && !string.IsNullOrEmpty(m_endReturnAudioEvent))
+		{
+			AudioManager.PostEvent(m_endReturnAudioEvent, Caster.gameObject);
 		}
 	}
 }
--- Assembly-CSharp/ValkyrieDirectionalShieldSequence.cs
+++ Assembly-CSharp/ValkyrieDirectionalShieldSequence.cs
@@ -20,27 +20,15 @@ public class ValkyrieDirectionalShieldSequence : SimpleAttachedVFXSequence
 	public bool m_useTempShieldIndicator = true;
 
 	private Vector3 m_aimDirection;
-
 	private GameObject m_placeholderShieldIndicator;
 
 	internal override void Initialize(IExtraSequenceParams[] extraParams)
 	{
 		foreach (IExtraSequenceParams extraSequenceParams in extraParams)
 		{
-			ExtraParams extraParams2 = extraSequenceParams as ExtraParams;
-			if (extraParams2 != null)
+			if (extraSequenceParams is ExtraParams ep)
 			{
-				m_aimDirection = base.Caster.GetActorCover().GetCoverOffset((ActorCover.CoverDirections)extraParams2.m_aimDirection);
-			}
-		}
-		while (true)
-		{
-			switch (6)
-			{
-			default:
-				return;
-			case 0:
-				break;
+				m_aimDirection = Caster.GetActorCover().GetCoverOffset((ActorCover.CoverDirections)ep.m_aimDirection);
 			}
 		}
 	}
@@ -52,19 +40,15 @@ public class ValkyrieDirectionalShieldSequence : SimpleAttachedVFXSequence
 		{
 			return;
 		}
-		while (true)
-		{
-			InitializeFXStorage();
-			m_placeholderShieldIndicator = new GameObject("Valkyrie_ShieldPlaceholder");
-			GameObject fxParentObject = GetFxParentObject();
-			m_placeholderShieldIndicator.transform.parent = fxParentObject.transform;
-			m_placeholderShieldIndicator.transform.localRotation = Quaternion.identity;
-			GameObject gameObject = HighlightUtils.Get().CreateConeCursor(0.7f, 360f);
-			gameObject.transform.parent = m_placeholderShieldIndicator.transform;
-			gameObject.transform.localPosition = 0.45f * Board.Get().squareSize * Vector3.forward;
-			gameObject.transform.localRotation = Quaternion.LookRotation(Vector3.up);
-			return;
-		}
+		InitializeFXStorage();
+		m_placeholderShieldIndicator = new GameObject("Valkyrie_ShieldPlaceholder");
+		GameObject fxParentObject = GetFxParentObject();
+		m_placeholderShieldIndicator.transform.parent = fxParentObject.transform;
+		m_placeholderShieldIndicator.transform.localRotation = Quaternion.identity;
+		GameObject cursor = HighlightUtils.Get().CreateConeCursor(0.7f, 360f);
+		cursor.transform.parent = m_placeholderShieldIndicator.transform;
+		cursor.transform.localPosition = 0.45f * Board.Get().squareSize * Vector3.forward;
+		cursor.transform.localRotation = Quaternion.LookRotation(Vector3.up);
 	}
 
 	private void OnDisable()
@@ -85,15 +69,10 @@ public class ValkyrieDirectionalShieldSequence : SimpleAttachedVFXSequence
 		{
 			m_fx.transform.rotation = Quaternion.LookRotation(m_aimDirection);
 		}
-		if (!(m_placeholderShieldIndicator != null))
+		if (m_placeholderShieldIndicator != null)
 		{
-			return;
-		}
-		while (true)
-		{
-			m_placeholderShieldIndicator.transform.position = base.Caster.transform.position + 2f * Vector3.up;
+			m_placeholderShieldIndicator.transform.position = Caster.transform.position + 2f * Vector3.up;
 			m_placeholderShieldIndicator.transform.rotation = Quaternion.LookRotation(m_aimDirection);
-			return;
 		}
 	}
 }
