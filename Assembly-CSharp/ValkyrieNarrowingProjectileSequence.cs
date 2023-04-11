61438fb5a41b01ef7708cc386b85284e658cd991
Zhenёq <Zheneq@users.noreply.github.com>
2023-04-12 00:23:56 +0200

    Update Valkyrie Sequences

--- Assembly-CSharp/ValkyrieNarrowingProjectileSequence.cs
+++ Assembly-CSharp/ValkyrieNarrowingProjectileSequence.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 public class ValkyrieNarrowingProjectileSequence : ArcingProjectileSequence
 {
 	private float m_totalLengthInSquares = -1f;
-
 	private float m_totalWidthInSquares = -1f;
 
 	internal override void Initialize(IExtraSequenceParams[] extraParams)
@@ -11,16 +10,9 @@ public class ValkyrieNarrowingProjectileSequence : ArcingProjectileSequence
 		base.Initialize(extraParams);
 		foreach (IExtraSequenceParams extraSequenceParams in extraParams)
 		{
-			if (!(extraSequenceParams is FxAttributeParam))
-			{
-				continue;
-			}
-			FxAttributeParam fxAttributeParam = extraSequenceParams as FxAttributeParam;
-			if (fxAttributeParam == null)
-			{
-				continue;
-			}
-			if (fxAttributeParam.m_paramNameCode == FxAttributeParam.ParamNameCode.None || fxAttributeParam.m_paramTarget != FxAttributeParam.ParamTarget.MainVfx)
+			if (!(extraSequenceParams is FxAttributeParam fxAttributeParam)
+			    || fxAttributeParam.m_paramNameCode == FxAttributeParam.ParamNameCode.None
+			    || fxAttributeParam.m_paramTarget != FxAttributeParam.ParamTarget.MainVfx)
 			{
 				continue;
 			}
@@ -33,45 +25,21 @@ public class ValkyrieNarrowingProjectileSequence : ArcingProjectileSequence
 				m_totalWidthInSquares = fxAttributeParam.m_paramValue;
 			}
 		}
-		while (true)
-		{
-			switch (1)
-			{
-			default:
-				return;
-			case 0:
-				break;
-			}
-		}
 	}
 
 	protected override void OnUpdate()
 	{
 		base.OnUpdate();
-		if (!(m_fx != null) || !m_fx.activeSelf)
+		if (m_fx == null
+		    || !m_fx.activeSelf
+		    || m_totalTravelDist2D <= 0f
+		    || m_totalLengthInSquares <= 0f
+		    || m_totalWidthInSquares <= 0f)
 		{
 			return;
 		}
-		while (true)
-		{
-			if (!(m_totalTravelDist2D > 0f))
-			{
-				return;
-			}
-			while (true)
-			{
-				if (m_totalLengthInSquares > 0f && m_totalWidthInSquares > 0f)
-				{
-					while (true)
-					{
-						float num = Mathf.Min(1f, VectorUtils.HorizontalPlaneDistInWorld(m_fxSpawnPos, m_fx.transform.position) / m_totalTravelDist2D);
-						float value = (1f - num) * m_totalLengthInSquares * (m_totalWidthInSquares / m_totalLengthInSquares);
-						Sequence.SetAttribute(m_fx, "widthInSquares", value);
-						return;
-					}
-				}
-				return;
-			}
-		}
+		float num = Mathf.Min(1f, VectorUtils.HorizontalPlaneDistInWorld(m_fxSpawnPos, m_fx.transform.position) / m_totalTravelDist2D);
+		float width = (1f - num) * m_totalLengthInSquares * (m_totalWidthInSquares / m_totalLengthInSquares);
+		SetAttribute(m_fx, "widthInSquares", width);
 	}
 }
