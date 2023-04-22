3c52910600732d6e6043871d0203a4bff41b9d55
zheneq <zheneq@users.noreply.github.com>
2023-04-22 02:21:23 +0200

    Update GameResultBadgeData.cs

--- Assembly-CSharp/GameResultBadgeData.cs
+++ Assembly-CSharp/GameResultBadgeData.cs
@@ -16,43 +16,28 @@ public class GameResultBadgeData : MonoBehaviour
 		}
 
 		public string DisplayName;
-
 		public string Description;
-
 		public StatDisplaySettings.StatType m_StatType;
-
 		public float LowWatermark = float.MinValue;
-
 		public StatUnitType StatUnit;
-
 		public bool LowerIsBetter;
 
 		public static string GetStatUnit(StatDescription description)
 		{
-			if (description != null)
+			if (description == null)
 			{
-				if (description.StatUnit == StatUnitType.PerTurn)
-				{
+				return string.Empty;
+			}
+			switch (description.StatUnit)
+			{
+				case StatUnitType.PerTurn:
 					return StringUtil.TR("PerTurn", "Global");
-				}
-				if (description.StatUnit == StatUnitType.PerLife)
-				{
-					while (true)
-					{
-						return StringUtil.TR("PerLife", "Global");
-					}
-				}
-				if (description.StatUnit == StatUnitType.PerTwentyLives)
-				{
-					while (true)
-					{
-						return StringUtil.TR("PerTwentyLives", "Global");
-					}
-				}
-				if (description.StatUnit == StatUnitType.Percentage)
-				{
+				case StatUnitType.PerLife:
+					return StringUtil.TR("PerLife", "Global");
+				case StatUnitType.PerTwentyLives:
+					return StringUtil.TR("PerTwentyLives", "Global");
+				case StatUnitType.Percentage:
 					return "%";
-				}
 			}
 			return string.Empty;
 		}
@@ -62,42 +47,32 @@ public class GameResultBadgeData : MonoBehaviour
 	public class ConsolidatedBadgeGroup
 	{
 		public string BadgeGroupDisplayName;
-
 		[TextArea(2, 5)]
 		public string BadgeGroupDescription;
-
 		public GameBalanceVars.GameResultBadge.BadgeRole DisplayCategory;
-
 		public int[] BadgeIDs;
 	}
 
 	public Color BadgeGroupGoldRequirementColorHex = Color.yellow;
-
 	public Color BadgeGroupSilverRequirementColorHex = Color.gray;
-
 	public Color BadgeGroupBronzeRequirementColorHex = Color.magenta;
-
 	public StatDescription[] StatDescriptions;
-
 	public ConsolidatedBadgeGroup[] BadgeGroups;
-
 	public GameBalanceVars.GameResultBadge[] GameResultBadges;
 
 	private static GameResultBadgeData s_instance;
 
 	public bool IsStatLowerBetter(StatDisplaySettings.StatType StatType)
 	{
-		for (int i = 0; i < StatDescriptions.Length; i++)
+		foreach (StatDescription statDesc in StatDescriptions)
 		{
-			if (StatDescriptions[i].m_StatType == StatType)
+			if (statDesc.m_StatType == StatType)
 			{
-				return StatDescriptions[i].LowerIsBetter;
+				return statDesc.LowerIsBetter;
 			}
 		}
-		while (true)
-		{
-			return false;
-		}
+
+		return false;
 	}
 
 	public static string ReplaceBadgeStringTokens(string input, GameBalanceVars.GameResultBadge BadgeData, CharacterType characterType)
@@ -130,21 +105,15 @@ public class GameResultBadgeData : MonoBehaviour
 
 	public StatDescription GetStatDescription(StatDisplaySettings.StatType TypeOfStat)
 	{
-		for (int i = 0; i < StatDescriptions.Length; i++)
+		foreach (StatDescription statDesc in StatDescriptions)
 		{
-			if (StatDescriptions[i].m_StatType != TypeOfStat)
+			if (statDesc.m_StatType == TypeOfStat)
 			{
-				continue;
-			}
-			while (true)
-			{
-				return StatDescriptions[i];
+				return statDesc;
 			}
 		}
-		while (true)
-		{
-			return null;
-		}
+
+		return null;
 	}
 
 	public GameBalanceVars.LobbyStatSettings[] GetStatSettings()
@@ -152,10 +121,12 @@ public class GameResultBadgeData : MonoBehaviour
 		GameBalanceVars.LobbyStatSettings[] array = new GameBalanceVars.LobbyStatSettings[StatDescriptions.Length];
 		for (int i = 0; i < StatDescriptions.Length; i++)
 		{
-			array[i] = new GameBalanceVars.LobbyStatSettings();
-			array[i].LowWatermark = StatDescriptions[i].LowWatermark;
-			array[i].m_StatType = StatDescriptions[i].m_StatType;
-			array[i].LowerIsBetter = StatDescriptions[i].LowerIsBetter;
+			array[i] = new GameBalanceVars.LobbyStatSettings
+			{
+				LowWatermark = StatDescriptions[i].LowWatermark,
+				m_StatType = StatDescriptions[i].m_StatType,
+				LowerIsBetter = StatDescriptions[i].LowerIsBetter
+			};
 		}
 		return array;
 	}
@@ -172,21 +143,15 @@ public class GameResultBadgeData : MonoBehaviour
 
 	public GameBalanceVars.GameResultBadge GetBadgeInfo(int badgeID)
 	{
-		for (int i = 0; i < GameResultBadges.Length; i++)
+		foreach (GameBalanceVars.GameResultBadge badge in GameResultBadges)
 		{
-			if (GameResultBadges[i].UniqueBadgeID != badgeID)
+			if (badge.UniqueBadgeID == badgeID)
 			{
-				continue;
-			}
-			while (true)
-			{
-				return GameResultBadges[i];
+				return badge;
 			}
 		}
-		while (true)
-		{
-			return null;
-		}
+
+		return null;
 	}
 
 	private void OnValidate()
