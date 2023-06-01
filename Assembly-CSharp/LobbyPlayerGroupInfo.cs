a2b17940f83904f62a544a08e7ff4b5d03f4b94b
zheneq <zheneq@users.noreply.github.com>
2023-06-01 03:35:31 +0300

    Update LobbyPlayerGroupInfo.cs

--- Assembly-CSharp/LobbyPlayerGroupInfo.cs
+++ Assembly-CSharp/LobbyPlayerGroupInfo.cs
@@ -5,17 +5,11 @@ using System.Collections.Generic;
 public class LobbyPlayerGroupInfo
 {
 	public bool InAGroup;
-
 	public bool IsLeader;
-
 	public GameType SelectedQueueType;
-
 	public ushort SubTypeMask;
-
 	private LobbyCharacterInfo m_ChararacterInfo;
-
 	public string MemberDisplayName;
-
 	public List<UpdateGroupMemberData> Members;
 
 	public LobbyCharacterInfo ChararacterInfo => m_ChararacterInfo;
@@ -32,36 +26,18 @@ public class LobbyPlayerGroupInfo
 
 	public void SetCharacterInfo(LobbyCharacterInfo newInfo, bool isFromServerResponse = false)
 	{
-		int num;
-		if (m_ChararacterInfo != null)
-		{
-			num = ((m_ChararacterInfo.CharacterType != newInfo.CharacterType) ? 1 : 0);
-		}
-		else
-		{
-			num = 1;
-		}
-		bool switchedChars = (byte)num != 0;
+		bool switchedChars = m_ChararacterInfo == null
+		                     || m_ChararacterInfo.CharacterType != newInfo.CharacterType;
 		m_ChararacterInfo = newInfo;
 		UIManager.Get().HandleNewSceneStateParameter(new UICharacterScreen.CharacterSelectSceneStateParameters
 		{
 			ClientSelectedVisualInfo = m_ChararacterInfo.CharacterSkin
 		});
-		if (!(AppState.GetCurrent() == AppState_GroupCharacterSelect.Get()))
+		if (AppState.GetCurrent() == AppState_GroupCharacterSelect.Get()
+		    && UICharacterSelectScreenController.Get() != null)
 		{
-			return;
-		}
-		while (true)
-		{
-			if (UICharacterSelectScreenController.Get() != null)
-			{
-				while (true)
-				{
-					UICharacterSelectScreenController.Get().QuickPlayUpdateCharacters(GameManager.Get().GameplayOverrides, switchedChars, isFromServerResponse);
-					return;
-				}
-			}
-			return;
+			UICharacterSelectScreenController.Get().QuickPlayUpdateCharacters(
+				GameManager.Get().GameplayOverrides, switchedChars, isFromServerResponse);
 		}
 	}
 }
