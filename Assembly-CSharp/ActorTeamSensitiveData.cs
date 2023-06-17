06c0cba488a54828769cc19f7e9491683fff7b2e
zheneq <zheneq@users.noreply.github.com>
2023-06-18 02:18:26 +0300

    Revert "Server Adjust dash visibility"
    This reverts commit 779648822896ae807d267b62d2c35a03a9b239bf.
    
    Apparently there was a reason these dashes were not adjusted. Dash animation freezes on enemy clients, it looks very bad sometimes.

--- Assembly-CSharp/ActorTeamSensitiveData.cs
+++ Assembly-CSharp/ActorTeamSensitiveData.cs
@@ -397,13 +397,7 @@ public class ActorTeamSensitiveData : NetworkBehaviour, IGameEventListener
 	{
 		return NetworkServer.active
 		       && m_typeObservingMe == ObservedBy.Hostiles
-		       
-		       // rogues
-		       // && (movementType == ActorData.MovementType.Normal || movementType == ActorData.MovementType.Teleport)
-		       // custom
-		       && movementType != ActorData.MovementType.None
-		       && movementType != ActorData.MovementType.Knockback
-		       
+		       && (movementType == ActorData.MovementType.Normal || movementType == ActorData.MovementType.Teleport)
 		       && (movementType != ActorData.MovementType.Teleport || teleportType == ActorData.TeleportType.Evasion_AdjustToVision)
 		       && path != null
 		       && (ServerEffectManager.Get() == null || !ServerEffectManager.Get().HasEffectRequiringAccuratePositionOnClients(Actor));
