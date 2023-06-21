814d077fc62e1f1a6a54c0e78f125e5124be7a46
zheneq <zheneq@users.noreply.github.com>
2023-06-21 17:11:43 +0300

    Update LobbyServerPlayerInfo.cs

--- Assembly-CSharp/LobbyServerPlayerInfo.cs
+++ Assembly-CSharp/LobbyServerPlayerInfo.cs
@@ -5,37 +5,21 @@ using System.Collections.Generic;
 public class LobbyServerPlayerInfo : LobbyPlayerCommonInfo
 {
 	public int AccountLevel;
-
 	public int TotalLevel;
-
 	public int NumWins;
-
 	public float AccMatchmakingElo;
-
 	public int AccMatchmakingCount;
-
 	public Dictionary<CharacterType, float> CharMatchmakingElo;
-
 	public Dictionary<CharacterType, int> CharMatchmakingCount;
-
 	public float UsedMatchmakingElo;
-
 	public int RankedTier;
-
 	public float RankedPoints;
-
 	public string MatchmakingEloKey;
-
 	public List<int> ProxyPlayerIds = new List<int>();
-
 	public long GroupIdAtStartOfMatch;
-
 	public int GroupSizeAtStartOfMatch;
-
 	public bool GroupLeader;
-
 	public ClientAccessLevel EffectiveClientAccessLevel;
-
 	public int RankedSortKarma;
 
 	public LobbyServerPlayerInfo Clone()
