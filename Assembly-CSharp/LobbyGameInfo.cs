161aaa421036c201a85618ecb9c12f8dc6f61098
zheneq <zheneq@users.noreply.github.com>
2023-12-04 20:31:28 +0400

    Update LobbyGameInfo.cs
    (cherry picked from commit f71437ebb0e2a45e2ac7f65f3f23856bd2c0cc13)

--- Assembly-CSharp/LobbyGameInfo.cs
+++ Assembly-CSharp/LobbyGameInfo.cs
@@ -6,112 +6,48 @@ using System.Collections.Generic;
 public class LobbyGameInfo
 {
 	public string MonitorServerProcessCode;
-
 	public string GameServerProcessCode;
-
 	public string GameServerAddress;
-
 	public string GameServerHost;
-
 	public long CreateTimestamp;
-
 	public long UpdateTimestamp;
-
 	public long SelectionStartTimestamp;
-
 	public long SelectionSubPhaseStartTimestamp;
-
 	public long LoadoutSelectionStartTimestamp;
-
 	public GameStatus GameStatus = GameStatus.Stopped;
-
 	public GameResult GameResult;
-
 	public FreelancerResolutionPhaseSubType SelectionSubPhase = FreelancerResolutionPhaseSubType.WAITING_FOR_ALL_PLAYERS;
-
 	public TimeSpan AcceptTimeout;
-
 	public TimeSpan SelectTimeout;
-
 	public TimeSpan LoadoutSelectTimeout;
-
 	public TimeSpan SelectSubPhaseBan1Timeout;
-
 	public TimeSpan SelectSubPhaseBan2Timeout;
-
 	public TimeSpan SelectSubPhaseFreelancerSelectTimeout;
-
 	public TimeSpan SelectSubPhaseTradeTimeout;
-
 	public bool IsActive;
-
 	public int ActivePlayers;
-
 	public int ActiveHumanPlayers;
-
 	public int ActiveSpectators;
-
 	public int AcceptedPlayers;
-
 	public BotDifficulty SelectedBotSkillTeamA;
-
 	public BotDifficulty SelectedBotSkillTeamB;
-
 	public Dictionary<long, int> ggPackUsedAccountIDs;
-
 	public Dictionary<long, Dictionary<int, int>> AccountIdToOverconIdToCount;
-
 	public LobbyGameConfig GameConfig;
 
 	[JsonIgnore]
-	public string Name
-	{
-		get
-		{
-			if (!GameServerProcessCode.IsNullOrEmpty())
-			{
-				if (GameConfig != null)
-				{
-					if (!GameConfig.HasSelectedSubType)
-					{
-						while (true)
-						{
-							switch (2)
-							{
-							case 0:
-								break;
-							default:
-								return $"{GameServerProcessCode} ({GameServerAddress}) [{GameConfig.Map} {GameConfig.GameType}]";
-							}
-						}
-					}
-					return $"{GameServerProcessCode} ({GameServerAddress}) [{GameConfig.Map} {GameConfig.GameType} {GameConfig.InstanceSubType.GetNameAsPayload().Term}]";
-				}
-			}
-			return "unknown";
-		}
-	}
+	public string Name =>
+		!GameServerProcessCode.IsNullOrEmpty() && GameConfig != null
+			? !GameConfig.HasSelectedSubType
+				? $"{GameServerProcessCode} ({GameServerAddress}) [{GameConfig.Map} {GameConfig.GameType}]"
+				: $"{GameServerProcessCode} ({GameServerAddress}) [{GameConfig.Map} {GameConfig.GameType} {GameConfig.InstanceSubType.GetNameAsPayload().Term}]"
+			: "unknown";
 
 	[JsonIgnore]
 	public bool IsCustomGame => GameConfig != null && GameConfig.GameType == GameType.Custom;
 
 	[JsonIgnore]
-	public bool IsQueuedGame
-	{
-		get
-		{
-			int result;
-			if (GameConfig != null)
-			{
-				result = (GameConfig.GameType.IsQueueable() ? 1 : 0);
-			}
-			else
-			{
-				result = 0;
-			}
-			return (byte)result != 0;
-		}
-	}
+	public bool IsQueuedGame => GameConfig != null && GameConfig.GameType.IsQueueable();
 
 	public LobbyGameInfo()
 	{
