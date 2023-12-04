461101fc584ea62cc73360abb6688aa373cf4720
zheneq <zheneq@users.noreply.github.com>
2023-12-04 20:47:44 +0400

    Update IQueueRequirementSystemInfo.cs
    (cherry picked from commit 7fe4d40fb97df2a225158876bd2834dff7bcf5be)

--- Assembly-CSharp/IQueueRequirementSystemInfo.cs
+++ Assembly-CSharp/IQueueRequirementSystemInfo.cs
@@ -3,25 +3,12 @@ using System.Collections.Generic;
 
 public interface IQueueRequirementSystemInfo
 {
-	IFreelancerSetQueryInterface FreelancerSetQueryInterface
-	{
-		get;
-	}
-
-	List<SeasonTemplate> Seasons
-	{
-		get;
-	}
-
+	IFreelancerSetQueryInterface FreelancerSetQueryInterface { get; }
+	List<SeasonTemplate> Seasons { get; }
 	DateTime GetCurrentUTCTime();
-
 	EnvironmentType GetEnvironmentType();
-
 	IEnumerable<GameType> GetGameTypes();
-
 	GameLeavingPenalty GetGameLeavingPenaltyForGameType(GameType gameType);
-
 	IEnumerable<QueueRequirement> GetQueueRequirements(GameType gameType);
-
 	bool IsCharacterAllowed(CharacterType characterType, GameType gameType, GameSubType gameSubType);
 }
