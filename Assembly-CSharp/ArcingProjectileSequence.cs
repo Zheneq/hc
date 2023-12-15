5c67b9f42d2069932576f24cf3c5a19571c4fe2c
zheneq <zheneq@users.noreply.github.com>
2023-12-15 17:03:04 +0400

    Update ArcingProjectileSequence.cs

--- Assembly-CSharp/ArcingProjectileSequence.cs
+++ Assembly-CSharp/ArcingProjectileSequence.cs
@@ -2,62 +2,37 @@ using UnityEngine;
 
 public class ArcingProjectileSequence : SplineProjectileSequence
 {
-	[Separator("Arc's highest point", true)]
+	[Separator("Arc's highest point")]
 	public float m_maxHeight;
-
-	[Separator("Use hit target's position as destination? (vs target pos passed from ability)", true)]
+	[Separator("Use hit target's position as destination? (vs target pos passed from ability)")]
 	public bool m_useTargetHitPos;
-
 	public bool m_useLastTargetHitPos;
-
 	[Space(10f)]
 	public bool m_reverseDirection;
-
-	[Separator("Start Height Offset", true)]
+	[Separator("Start Height Offset")]
 	public float m_startPosYOffset;
-
-	[Separator("Destination Height", true)]
+	[Separator("Destination Height")]
 	public bool m_destPosUseSameHeightAsStart;
-
 	public float m_yOffset;
-
 	[Header("-- For destination height in case of no targets (yOffset still appiles after adjustment) --")]
 	public bool m_destPosUseSameHeightIfNoTargets;
-
 	public bool m_destPosUseGroundHeightIfNoTargets;
-
 	public bool m_destPosAlwaysUseGroundHeight;
 
 	public override void FinishSetup()
 	{
 		base.FinishSetup();
-		if (m_maxHeight == 0f)
-		{
-			return;
-		}
-		while (true)
+		if (m_maxHeight != 0f)
 		{
 			m_doHitsAsProjectileTravels = false;
-			return;
 		}
 	}
 
 	internal override Vector3 GetSequencePos()
 	{
-		if (m_fx != null)
-		{
-			while (true)
-			{
-				switch (5)
-				{
-				case 0:
-					break;
-				default:
-					return m_fx.transform.position;
-				}
-			}
-		}
-		return Vector3.zero;
+		return m_fx != null
+			? m_fx.transform.position
+			: Vector3.zero;
 	}
 
 	internal virtual Vector3 GetStartPos()
@@ -67,94 +42,79 @@ public class ArcingProjectileSequence : SplineProjectileSequence
 
 	internal override Vector3[] GetSplinePath(int curIndex, int maxIndex)
 	{
-		Vector3 vector = GetStartPos();
+		Vector3 startPos = GetStartPos();
 		if (m_useOverrideStartPos)
 		{
-			vector = m_overrideStartPos;
+			startPos = m_overrideStartPos;
 		}
-		vector.y += m_startPosYOffset;
+		startPos.y += m_startPosYOffset;
 		Vector3[] array = new Vector3[5];
-		Vector3 vector2;
 		if (m_maxHeight == 0f)
 		{
+			Vector3 endPos;
 			if (m_useTargetHitPos)
 			{
 				int targetIndex = 0;
 				if (m_useLastTargetHitPos)
 				{
-					targetIndex = Mathf.Max(0, base.Targets.Length - 1);
+					targetIndex = Mathf.Max(0, Targets.Length - 1);
 				}
-				vector2 = GetTargetHitPosition(targetIndex, m_hitPosJoint);
+				endPos = GetTargetHitPosition(targetIndex, m_hitPosJoint);
 			}
 			else
 			{
-				vector2 = base.TargetPos;
-				vector2.y += m_yOffset;
+				endPos = TargetPos;
+				endPos.y += m_yOffset;
 			}
 			if (m_destPosUseSameHeightAsStart)
 			{
-				vector2.y = vector.y;
+				endPos.y = startPos.y;
 			}
 			if (m_destPosAlwaysUseGroundHeight)
 			{
-				vector2.y = (float)Board.Get().BaselineHeight + m_yOffset;
+				endPos.y = Board.Get().BaselineHeight + m_yOffset;
 			}
-			else
+			else if (Targets == null || Targets.Length == 0)
 			{
-				if (base.Targets != null)
-				{
-					if (base.Targets.Length != 0)
-					{
-						goto IL_0168;
-					}
-				}
 				if (m_destPosUseSameHeightIfNoTargets)
 				{
-					vector2.y = vector.y;
+					endPos.y = startPos.y;
 				}
 				else if (m_destPosUseGroundHeightIfNoTargets)
 				{
-					vector2.y = (float)Board.Get().BaselineHeight + m_yOffset;
+					endPos.y = Board.Get().BaselineHeight + m_yOffset;
 				}
 			}
-			goto IL_0168;
-		}
-		Vector3 vector3;
-		if (m_useTargetHitPos)
-		{
-			vector3 = GetTargetHitPosition(0, m_hitPosJoint);
+			Vector3 moveVector = endPos - startPos;
+			array[0] = startPos - moveVector;
+			array[1] = startPos;
+			array[2] = (startPos + endPos) * 0.5f;
+			array[3] = endPos;
+			array[4] = endPos + moveVector;
 		}
 		else
 		{
-			vector3 = base.TargetPos;
+			Vector3 vector3 = m_useTargetHitPos
+				? GetTargetHitPosition(0, m_hitPosJoint)
+				: TargetPos;
+			if (m_destPosUseSameHeightAsStart)
+			{
+				vector3.y = startPos.y;
+			}
+			array[0] = startPos + Vector3.down * m_maxHeight;
+			array[1] = startPos;
+			array[2] = (startPos + vector3) * 0.5f + Vector3.up * m_maxHeight;
+			array[3] = vector3;
+			array[4] = vector3 + Vector3.down * m_maxHeight;
 		}
-		if (m_destPosUseSameHeightAsStart)
-		{
-			vector3.y = vector.y;
-		}
-		array[0] = vector + Vector3.down * m_maxHeight;
-		array[1] = vector;
-		array[2] = (vector + vector3) * 0.5f + Vector3.up * m_maxHeight;
-		array[3] = vector3;
-		array[4] = vector3 + Vector3.down * m_maxHeight;
-		goto IL_02cb;
-		IL_0168:
-		Vector3 b = vector2 - vector;
-		array[0] = vector - b;
-		array[1] = vector;
-		array[2] = (vector + vector2) * 0.5f;
-		array[3] = vector2;
-		array[4] = vector2 + b;
-		goto IL_02cb;
-		IL_02cb:
 		if (m_reverseDirection)
 		{
-			Vector3 vector4 = array[0];
+			Vector3 tmp = array[0];
 			array[0] = array[4];
-			array[4] = vector4;
-			vector4 = array[1];
+			array[4] = tmp;
+			tmp = array[1];
 			array[1] = array[3];
-			array[3] = vector4;
+			array[3] = tmp;
 		}
 		return array;
 	}
