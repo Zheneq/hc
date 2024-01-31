e5920f0cacfb1523d02ff542b4b0d6e6b3214af8
zheneq <zheneq@users.noreply.github.com>
2024-01-31 04:23:03 +0400

    Update SpoilsData.cs

--- Assembly-CSharp/SpoilsData.cs
+++ Assembly-CSharp/SpoilsData.cs
@@ -4,52 +4,38 @@ using UnityEngine;
 public class SpoilsData : MonoBehaviour
 {
 	public SpoilsManager.SpoilsType m_spoilsType;
-
 	public PowerUp m_overrideSpoils;
 
 	private PowerUp m_spawnedPowerUp;
 
 	public void SpawnSpoilOnDeath(BoardSquare desiredSquare)
 	{
-		if (!(SpoilsManager.Get() != null))
+		if (SpoilsManager.Get() == null || desiredSquare == null)
 		{
 			return;
 		}
-		while (true)
+		ActorData component = GetComponent<ActorData>();
+		if (!GameplayUtils.IsPlayerControlled(component))
 		{
-			if (!(desiredSquare != null))
-			{
-				return;
-			}
-			while (true)
-			{
-				ActorData component = GetComponent<ActorData>();
-				if (!GameplayUtils.IsPlayerControlled(component))
-				{
-					return;
-				}
-				List<BoardSquare> list = SpoilsManager.Get().FindSquaresToSpawnSpoil(desiredSquare, component.GetEnemyTeam(), 1, true, true, 3);
-				if (list.Count > 0)
-				{
-					if (m_overrideSpoils != null)
-					{
-						m_spawnedPowerUp = SpoilsManager.Get().SpawnSpoils(list[0], m_overrideSpoils, component.GetEnemyTeam(), false);
-					}
-					else
-					{
-						m_spawnedPowerUp = SpoilsManager.Get().SpawnSpoils(list[0], m_spoilsType, component.GetEnemyTeam());
-					}
-				}
-				if (!m_spawnedPowerUp)
-				{
-					return;
-				}
-				while (true)
-				{
-					m_spawnedPowerUp.CalculateBoardSquare();
-					return;
-				}
-			}
+			return;
+		}
+		List<BoardSquare> list = SpoilsManager.Get().FindSquaresToSpawnSpoil(
+			desiredSquare,
+			component.GetEnemyTeam(),
+			1,
+			true,
+			true,
+			3);
+		if (list.Count > 0)
+		{
+			m_spawnedPowerUp = m_overrideSpoils != null
+				? SpoilsManager.Get().SpawnSpoils(list[0], m_overrideSpoils, component.GetEnemyTeam(), false)
+				: SpoilsManager.Get().SpawnSpoils(list[0], m_spoilsType, component.GetEnemyTeam());
+		}
+
+		if (m_spawnedPowerUp)
+		{
+			m_spawnedPowerUp.CalculateBoardSquare();
 		}
 	}
 }
