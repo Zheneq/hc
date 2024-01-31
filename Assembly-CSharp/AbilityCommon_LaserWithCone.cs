a79d2b95859754436df36bb06675b8bf14a3dce3
zheneq <zheneq@users.noreply.github.com>
2024-01-31 05:44:04 +0400

    Update AbilityCommon_LaserWithCone.cs

--- Assembly-CSharp/AbilityCommon_LaserWithCone.cs
+++ Assembly-CSharp/AbilityCommon_LaserWithCone.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class AbilityCommon_LaserWithCone
 {
+	private const float Sqrt2 = 1.415f;
+	
 	public static Vector3 GetConeLosCheckPos(Vector3 startPos, Vector3 endPos)
 	{
 		AreaEffectUtils.GetEndPointForValidGameplaySquare(startPos, endPos, out Vector3 adjustedEndPoint);
@@ -10,25 +12,14 @@ public class AbilityCommon_LaserWithCone
 		{
 			Vector3 vector = adjustedEndPoint - startPos;
 			vector.y = 0f;
-			float maxDistance = vector.magnitude + 0.1f;
+			float maxDistance = vector.magnitude + 0.1f; // TODO CLIENT BUG looks like it's supposed to be Board.SquareSizeStatic * Sqrt2 + 0.1f
 			vector.Normalize();
-			Vector3 origin = adjustedEndPoint - Board.SquareSizeStatic * 1.415f * vector;
+			Vector3 origin = adjustedEndPoint - Board.SquareSizeStatic * Sqrt2 * vector;
 			LayerMask mask = 1 << VectorUtils.s_raycastLayerDynamicLineOfSight;
-			if (Physics.Raycast(origin, vector, out RaycastHit hitInfo, maxDistance, mask))
+			if (Physics.Raycast(origin, vector, out RaycastHit hitInfo, maxDistance, mask)
+			    && (hitInfo.collider.gameObject.layer & VectorUtils.s_raycastLayerDynamicLineOfSight) != 0)
 			{
-				if ((hitInfo.collider.gameObject.layer & VectorUtils.s_raycastLayerDynamicLineOfSight) != 0)
-				{
-					while (true)
-					{
-						switch (7)
-						{
-						case 0:
-							break;
-						default:
-							return hitInfo.point - 0.1f * vector;
-						}
-					}
-				}
+				return hitInfo.point - 0.1f * vector;
 			}
 		}
 		return adjustedEndPoint;
