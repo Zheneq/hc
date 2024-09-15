800d22739054eec196f1d4cfecb1d718bbd0367f
zheneq <zheneq@users.noreply.github.com>
2024-09-15 17:05:07 +0400

    Update FreelancerResolutionPhaseSubTypeExtensions.cs

--- Assembly-CSharp/FreelancerResolutionPhaseSubTypeExtensions.cs
+++ Assembly-CSharp/FreelancerResolutionPhaseSubTypeExtensions.cs
@@ -1,21 +1,14 @@
 public static class FreelancerResolutionPhaseSubTypeExtensions
 {
-	public static bool IsPickBanSubPhase(this FreelancerResolutionPhaseSubType subtype)
-	{
-		int result;
-		if (subtype != FreelancerResolutionPhaseSubType.PICK_BANS1)
-		{
-			result = ((subtype == FreelancerResolutionPhaseSubType.PICK_BANS2) ? 1 : 0);
-		}
-		else
-		{
-			result = 1;
-		}
-		return (byte)result != 0;
-	}
+    public static bool IsPickBanSubPhase(this FreelancerResolutionPhaseSubType subtype)
+    {
+        return subtype == FreelancerResolutionPhaseSubType.PICK_BANS1
+               || subtype == FreelancerResolutionPhaseSubType.PICK_BANS2;
+    }
 
-	public static bool IsPickFreelancerSubPhase(this FreelancerResolutionPhaseSubType subtype)
-	{
-		return subtype == FreelancerResolutionPhaseSubType.PICK_FREELANCER1 || subtype == FreelancerResolutionPhaseSubType.PICK_FREELANCER2;
-	}
+    public static bool IsPickFreelancerSubPhase(this FreelancerResolutionPhaseSubType subtype)
+    {
+        return subtype == FreelancerResolutionPhaseSubType.PICK_FREELANCER1
+               || subtype == FreelancerResolutionPhaseSubType.PICK_FREELANCER2;
+    }
 }
