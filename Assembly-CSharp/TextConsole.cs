70d0692df2ca504fa455c30809ce4eee01605041
zheneq <zheneq@users.noreply.github.com>
2024-11-22 00:43:27 +0400

    Update TextConsole.cs

--- Assembly-CSharp/TextConsole.cs
+++ Assembly-CSharp/TextConsole.cs
@@ -1,223 +1,173 @@
-using I2.Loc;
-using LobbyGameClientMessages;
 using System;
 using System.Collections.Generic;
-using System.Threading;
+using I2.Loc;
+using LobbyGameClientMessages;
 
 public class TextConsole
 {
-	public struct Message
-	{
-		public string Text;
+    public struct Message
+    {
+        public string Text;
+        public ConsoleMessageType MessageType;
+        public CharacterType CharacterType;
+        public bool DisplayDevTag;
+        public long SenderAccountId;
+        public string SenderHandle;
+        public Team SenderTeam;
+        public string RecipientHandle;
+        public Team RestrictVisibiltyToTeam;
+    }
 
-		public ConsoleMessageType MessageType;
+    public struct AllowedEmojis
+    {
+        public List<int> emojis;
+    }
 
-		public CharacterType CharacterType;
+    private static TextConsole s_instance;
 
-		public bool DisplayDevTag;
+    public string LastWhisperSenderHandle { get; private set; }
 
-		public long SenderAccountId;
+    public event Action<Message, AllowedEmojis> OnMessage;
 
-		public string SenderHandle;
+    public TextConsole()
+    {
+        OnMessage = delegate { };
+        ClientGameManager.Get().OnChatNotification += HandleChatNotification;
+    }
 
-		public Team SenderTeam;
+    public static TextConsole Get()
+    {
+        return s_instance;
+    }
 
-		public string RecipientHandle;
+    public static void Instantiate()
+    {
+        s_instance = new TextConsole();
+    }
 
-		public Team RestrictVisibiltyToTeam;
-	}
+    public void Write(Message message, List<int> EmojisAllowed = null)
+    {
+        AllowedEmojis allowedEmojis = new AllowedEmojis
+        {
+            emojis = EmojisAllowed
+        };
+        OnMessage(message, allowedEmojis);
+        UITextConsole.StoreMessage(message, allowedEmojis);
+    }
 
-	public struct AllowedEmojis
-	{
-		public List<int> emojis;
-	}
+    public void Write(string text, ConsoleMessageType messageType = ConsoleMessageType.SystemMessage)
+    {
+        Write(
+            new Message
+            {
+                MessageType = messageType,
+                Text = text
+            });
+    }
 
-	private static TextConsole s_instance;
+    public string RemoveRichTextTags(string theString)
+    {
+        string text = theString;
+        if (text.IndexOf('<') != -1 && text.IndexOf('>') != -1)
+        {
+            text = text.Replace("<", "< ");
+        }
 
-	public string LastWhisperSenderHandle
-	{
-		get;
-		private set;
-	}
+        return text;
+    }
 
-	private Action<Message, AllowedEmojis> OnMessageHolder;
-	public event Action<Message, AllowedEmojis> OnMessage
-	{
-		add
-		{
-			Action<Message, AllowedEmojis> action = this.OnMessageHolder;
-			Action<Message, AllowedEmojis> action2;
-			do
-			{
-				action2 = action;
-				action = Interlocked.CompareExchange(ref this.OnMessageHolder, (Action<Message, AllowedEmojis>)Delegate.Combine(action2, value), action);
-			}
-			while ((object)action != action2);
-			while (true)
-			{
-				return;
-			}
-		}
-		remove
-		{
-			Action<Message, AllowedEmojis> action = this.OnMessageHolder;
-			Action<Message, AllowedEmojis> action2;
-			do
-			{
-				action2 = action;
-				action = Interlocked.CompareExchange(ref this.OnMessageHolder, (Action<Message, AllowedEmojis>)Delegate.Remove(action2, value), action);
-			}
-			while ((object)action != action2);
-		}
-	}
+    public void OnInputSubmitted(string input)
+    {
+        if (string.IsNullOrEmpty(input))
+        {
+            return;
+        }
 
-	public TextConsole()
-	{
-		this.OnMessageHolder = delegate
-		{
-		};
-		
-		ClientGameManager.Get().OnChatNotification += HandleChatNotification;
-	}
+        bool isInGame = GameManager.Get() != null
+                        && GameManager.Get().GameInfo != null
+                        && GameManager.Get().GameInfo.GameStatus != GameStatus.Stopped;
 
-	public static TextConsole Get()
-	{
-		return s_instance;
-	}
+        input = ChatEmojiManager.Get().UnlocalizeEmojis(input);
+        input = RemoveRichTextTags(input);
 
-	public static void Instantiate()
-	{
-		s_instance = new TextConsole();
-	}
+        string arguments;
+        string command;
+        if (input[0] == '/')
+        {
+            string[] array = input.Split((string[])null, 2, StringSplitOptions.RemoveEmptyEntries);
+            if (array.Length >= 2)
+            {
+                command = array[0];
+                arguments = array[1];
+            }
+            else
+            {
+                command = input;
+                arguments = string.Empty;
+            }
+        }
+        else
+        {
+            command = isInGame ? "/team" : "/global";
+            arguments = input;
+        }
 
-	public void Write(Message message, List<int> EmojisAllowed = null)
-	{
-		AllowedEmojis allowedEmojis = default(AllowedEmojis);
-		allowedEmojis.emojis = EmojisAllowed;
-		this.OnMessageHolder(message, allowedEmojis);
-		UITextConsole.StoreMessage(message, allowedEmojis);
-	}
+        command = command.Trim();
+        if (!SlashCommands.Get().RunSlashCommand(command, arguments) && DebugCommands.Get() != null)
+        {
+            DebugCommands.Get().RunDebugCommand(command, arguments);
+        }
+    }
 
-	public void Write(string text, ConsoleMessageType messageType = ConsoleMessageType.SystemMessage)
-	{
-		Message message = default(Message);
-		message.MessageType = messageType;
-		message.Text = text;
-		Write(message);
-	}
+    public void HandleSetDevTagResponse(SetDevTagResponse response)
+    {
+        Write(
+            new Message
+            {
+                Text = response.Success ? "Success" : "Failed",
+                MessageType = ConsoleMessageType.SystemMessage
+            });
+    }
 
-	public string RemoveRichTextTags(string theString)
-	{
-		string text = theString;
-		if (text.IndexOf('<') != -1)
-		{
-			if (text.IndexOf('>') != -1)
-			{
-				text = text.Replace("<", "< ");
-			}
-		}
-		return text;
-	}
+    public void HandleChatNotification(ChatNotification notification)
+    {
+        if (Options_UI.Get() != null
+            && Options_UI.Get().GetEnableProfanityFilter()
+            && notification.ConsoleMessageType != ConsoleMessageType.BroadcastMessage)
+        {
+            notification.Text = BannedWords.Get().FilterPhrase(
+                notification.Text,
+                LocalizationManager.CurrentLanguageCode);
+        }
 
-	public void OnInputSubmitted(string input)
-	{
-		if (string.IsNullOrEmpty(input))
-		{
-			return;
-		}
-		bool isInGame = GameManager.Get() != null
-		            && GameManager.Get().GameInfo != null
-		            && GameManager.Get().GameInfo.GameStatus != GameStatus.Stopped;
-		input = ChatEmojiManager.Get().UnlocalizeEmojis(input);
-		input = RemoveRichTextTags(input);
-		string arguments;
-		string command;
-		if (input[0] == '/')
-		{
-			string[] array = input.Split((string[])null, 2, StringSplitOptions.RemoveEmptyEntries);
-			if (array.Length >= 2)
-			{
-				command = array[0];
-				arguments = array[1];
-			}
-			else
-			{
-				command = input;
-				arguments = string.Empty;
-			}
-		}
-		else
-		{
-			command = isInGame ? "/team" : "/global";
-			arguments = input;
-		}
+        Message message = new Message
+        {
+            Text = notification.LocalizedText == null ? notification.Text : notification.LocalizedText.ToString(),
+            MessageType = notification.ConsoleMessageType,
+            SenderAccountId = notification.SenderAccountId,
+            SenderHandle = notification.SenderHandle,
+            SenderTeam = notification.SenderTeam,
+            RecipientHandle = notification.RecipientHandle,
+            CharacterType = notification.CharacterType,
+            DisplayDevTag = notification.DisplayDevTag
+        };
+        Write(message, notification.EmojisAllowed);
 
-		command = command.Trim();
-		if (SlashCommands.Get().RunSlashCommand(command, arguments))
-		{
-			return;
-		}
-		if (DebugCommands.Get() != null)
-		{
-			DebugCommands.Get().RunDebugCommand(command, arguments);
-		}
-	}
+        switch (notification.ConsoleMessageType)
+        {
+            case ConsoleMessageType.WhisperChat:
+            {
+                ClientGameManager clientGameManager = ClientGameManager.Get();
+                if (clientGameManager == null || clientGameManager.Handle != notification.SenderHandle)
+                {
+                    LastWhisperSenderHandle = notification.SenderHandle;
+                }
 
-	public void HandleSetDevTagResponse(SetDevTagResponse response)
-	{
-		string empty = string.Empty;
-		if (response.Success)
-		{
-			empty = "Success";
-		}
-		else
-		{
-			empty = "Failed";
-		}
-		Write(new Message
-		{
-			Text = empty,
-			MessageType = ConsoleMessageType.SystemMessage
-		});
-	}
-
-	public void HandleChatNotification(ChatNotification notification)
-	{
-		if (Options_UI.Get() != null)
-		{
-			if (Options_UI.Get().GetEnableProfanityFilter() && notification.ConsoleMessageType != ConsoleMessageType.BroadcastMessage)
-			{
-				notification.Text = BannedWords.Get().FilterPhrase(notification.Text, LocalizationManager.CurrentLanguageCode);
-			}
-		}
-		Write(new Message
-		{
-			Text = ((notification.LocalizedText == null) ? notification.Text : notification.LocalizedText.ToString()),
-			MessageType = notification.ConsoleMessageType,
-			SenderAccountId = notification.SenderAccountId,
-			SenderHandle = notification.SenderHandle,
-			SenderTeam = notification.SenderTeam,
-			RecipientHandle = notification.RecipientHandle,
-			CharacterType = notification.CharacterType,
-			DisplayDevTag = notification.DisplayDevTag
-		}, notification.EmojisAllowed);
-		if (notification.ConsoleMessageType == ConsoleMessageType.WhisperChat)
-		{
-			ClientGameManager clientGameManager = ClientGameManager.Get();
-			if (!(clientGameManager == null))
-			{
-				if (!(clientGameManager.Handle != notification.SenderHandle))
-				{
-					goto IL_0148;
-				}
-			}
-			LastWhisperSenderHandle = notification.SenderHandle;
-		}
-		goto IL_0148;
-		IL_0148:
-		if (notification.ConsoleMessageType == ConsoleMessageType.BroadcastMessage)
-		{
-			SystemMenuBroadcast.Get().DisplaySystemMessage(notification);
-		}
-	}
+                break;
+            }
+            case ConsoleMessageType.BroadcastMessage:
+                SystemMenuBroadcast.Get().DisplaySystemMessage(notification);
+                break;
+        }
+    }
 }
