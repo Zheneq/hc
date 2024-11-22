fa708f6999c02ed994d66360eb10c350744cc9ce
zheneq <zheneq@users.noreply.github.com>
2024-11-22 23:19:54 +0400

    Update UIGGPackNotification.cs

--- Assembly-CSharp/UIGGPackNotification.cs
+++ Assembly-CSharp/UIGGPackNotification.cs
@@ -4,66 +4,64 @@ using UnityEngine.UI;
 
 public class UIGGPackNotification : MonoBehaviour
 {
-	public Image m_backgroundImage;
+    public Image m_backgroundImage;
+    public Image m_foregroundImage;
+    public Image m_ribbonImage;
+    public TextMeshProUGUI m_UserNameLabel;
+    public TextMeshProUGUI m_TitleLabel;
+    public RectTransform[] m_ggButtonLevelImages;
 
-	public Image m_foregroundImage;
+    public bool DoneAnimating { get; private set; }
 
-	public Image m_ribbonImage;
+    public void AnimationDone()
+    {
+        DoneAnimating = true;
+    }
 
-	public TextMeshProUGUI m_UserNameLabel;
-
-	public TextMeshProUGUI m_TitleLabel;
-
-	public RectTransform[] m_ggButtonLevelImages;
-
-	public bool DoneAnimating
-	{
-		get;
-		private set;
-	}
-
-	public void AnimationDone()
-	{
-		DoneAnimating = true;
-	}
-
-	public void Setup(UISideNotifications.UIGGPackNotificationInfo info)
-	{
-		m_UserNameLabel.text = info.GGPackUserName;
+    public void Setup(UISideNotifications.UIGGPackNotificationInfo info)
+    {
+        m_UserNameLabel.text = info.GGPackUserName;
+        m_TitleLabel.text = GameBalanceVars.Get().GetTitle(
+            info.GGPackUserTitle,
 #if EVOS
-        // Custom titles
-        m_TitleLabel.text = GameBalanceVars.Get().GetTitle(info.GGPackUserTitle, info.GGPackUserName, string.Empty, info.GGPackUserTitleLevel);
-#else
-		m_TitleLabel.text = GameBalanceVars.Get().GetTitle(info.GGPackUserTitle, string.Empty, info.GGPackUserTitleLevel);
+			info.GGPackUserName, // Custom titles
 #endif
-		GameBalanceVars.PlayerBanner banner = GameBalanceVars.Get().GetBanner(info.GGPackUserBannerForeground);
-		GameBalanceVars.PlayerBanner banner2 = GameBalanceVars.Get().GetBanner(info.GGPackUserBannerBackground);
-		GameBalanceVars.PlayerRibbon ribbon = GameBalanceVars.Get().GetRibbon(info.GGPackUserRibbon);
-		for (int i = 0; i < m_ggButtonLevelImages.Length; i++)
-		{
-			UIManager.SetGameObjectActive(m_ggButtonLevelImages[i], i == info.NumGGPacksUsed - 1);
-		}
-		if (banner != null)
-		{
-			Sprite sprite = (Sprite)Resources.Load(banner.m_resourceString, typeof(Sprite));
-			m_foregroundImage.sprite = sprite;
-			UIManager.SetGameObjectActive(m_foregroundImage, sprite != null);
-		}
-		if (banner2 != null)
-		{
-			Sprite sprite2 = (Sprite)Resources.Load(banner2.m_resourceString, typeof(Sprite));
-			m_backgroundImage.sprite = sprite2;
-			UIManager.SetGameObjectActive(m_backgroundImage, sprite2 != null);
-		}
-		if (ribbon != null)
-		{
-			Sprite sprite3 = (Sprite)Resources.Load(ribbon.m_resourceString, typeof(Sprite));
-			m_ribbonImage.sprite = sprite3;
-			UIManager.SetGameObjectActive(m_ribbonImage, sprite3 != null);
-		}
-		else
-		{
-			UIManager.SetGameObjectActive(m_ribbonImage, false);
-		}
-	}
+            string.Empty,
+            info.GGPackUserTitleLevel);
+        GameBalanceVars.PlayerBanner bannerForeground =
+            GameBalanceVars.Get().GetBanner(info.GGPackUserBannerForeground);
+        GameBalanceVars.PlayerBanner bannerBackground =
+            GameBalanceVars.Get().GetBanner(info.GGPackUserBannerBackground);
+        GameBalanceVars.PlayerRibbon ribbon =
+            GameBalanceVars.Get().GetRibbon(info.GGPackUserRibbon);
+        for (int i = 0; i < m_ggButtonLevelImages.Length; i++)
+        {
+            UIManager.SetGameObjectActive(m_ggButtonLevelImages[i], i == info.NumGGPacksUsed - 1);
+        }
+
+        if (bannerForeground != null)
+        {
+            Sprite sprite = (Sprite)Resources.Load(bannerForeground.m_resourceString, typeof(Sprite));
+            m_foregroundImage.sprite = sprite;
+            UIManager.SetGameObjectActive(m_foregroundImage, sprite != null);
+        }
+
+        if (bannerBackground != null)
+        {
+            Sprite sprite = (Sprite)Resources.Load(bannerBackground.m_resourceString, typeof(Sprite));
+            m_backgroundImage.sprite = sprite;
+            UIManager.SetGameObjectActive(m_backgroundImage, sprite != null);
+        }
+
+        if (ribbon != null)
+        {
+            Sprite sprite = (Sprite)Resources.Load(ribbon.m_resourceString, typeof(Sprite));
+            m_ribbonImage.sprite = sprite;
+            UIManager.SetGameObjectActive(m_ribbonImage, sprite != null);
+        }
+        else
+        {
+            UIManager.SetGameObjectActive(m_ribbonImage, false);
+        }
+    }
 }
