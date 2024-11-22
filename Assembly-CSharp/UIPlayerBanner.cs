0b3806c24412c59e4e9f48521204b4c6bd2c7787
zheneq <zheneq@users.noreply.github.com>
2024-11-22 23:56:20 +0400

    Update UIPlayerBanner.cs

--- Assembly-CSharp/UIPlayerBanner.cs
+++ Assembly-CSharp/UIPlayerBanner.cs
@@ -5,171 +5,142 @@ using UnityEngine.UI;
 
 public class UIPlayerBanner : MonoBehaviour
 {
-	public Image m_bannerImage;
+    public Image m_bannerImage;
+    public Image m_profileImage;
+    public Image m_ribbonImage;
+    public TextMeshProUGUI m_playerName;
+    public TextMeshProUGUI m_playerTitle;
+    public TextMeshProUGUI m_playerLevel;
 
-	public Image m_profileImage;
+    public const string standardResourceString = "Banners/Background/02_blue";
+    public const string standardEmblemResourceString = "Banners/Emblems/Chest01";
+    public const string rankedModeEnemyBannerResourceString = "Banners/Background/rankedRedDefault";
 
-	public Image m_ribbonImage;
+    private Action<PersistedAccountData> m_onAccountDataUpdated;
+    private Action<GameBalanceVars.PlayerBanner, GameBalanceVars.PlayerBanner> m_onPlayerBannerChange;
+    private Action<GameBalanceVars.PlayerRibbon> m_onPlayerRibbonChange;
 
-	public TextMeshProUGUI m_playerName;
+    private void Start()
+    {
+        m_onAccountDataUpdated = delegate(PersistedAccountData accountData)
+        {
+            SetBanner(
+                ClientGameManager.Get().GetCurrentBackgroundBanner(),
+                GameBalanceVars.PlayerBanner.BannerType.Background);
+            SetBanner(
+                ClientGameManager.Get().GetCurrentForegroundBanner(),
+                GameBalanceVars.PlayerBanner.BannerType.Foreground);
+            SetRibbon(ClientGameManager.Get().GetCurrentRibbon());
+            if (m_playerName != null)
+            {
+                m_playerName.text = accountData.UserName;
+            }
 
-	public TextMeshProUGUI m_playerTitle;
+            if (m_playerTitle != null)
+            {
+                m_playerTitle.text = GameBalanceVars.Get().GetTitle(accountData.AccountComponent.SelectedTitleID);
+            }
 
-	public TextMeshProUGUI m_playerLevel;
+            if (m_playerLevel != null)
+            {
+                m_playerLevel.text = ClientGameManager.Get().GetDisplayedStatString(accountData);
+            }
+        };
+        ClientGameManager.Get().OnAccountDataUpdated += m_onAccountDataUpdated;
+        if (ClientGameManager.Get().IsPlayerAccountDataAvailable())
+        {
+            m_onAccountDataUpdated(ClientGameManager.Get().GetPlayerAccountData());
+        }
 
-	public const string standardResourceString = "Banners/Background/02_blue";
+        SetBanner(
+            ClientGameManager.Get().GetCurrentBackgroundBanner(),
+            GameBalanceVars.PlayerBanner.BannerType.Background);
+        SetBanner(
+            ClientGameManager.Get().GetCurrentForegroundBanner(),
+            GameBalanceVars.PlayerBanner.BannerType.Foreground);
+        SetRibbon(ClientGameManager.Get().GetCurrentRibbon());
+        m_onPlayerBannerChange = delegate(
+            GameBalanceVars.PlayerBanner foregroundBanner,
+            GameBalanceVars.PlayerBanner backgroundBanner)
+        {
+            SetBanner(backgroundBanner, GameBalanceVars.PlayerBanner.BannerType.Background);
+            SetBanner(foregroundBanner, GameBalanceVars.PlayerBanner.BannerType.Foreground);
+        };
+        ClientGameManager.Get().OnPlayerBannerChange += m_onPlayerBannerChange;
+        m_onPlayerRibbonChange = delegate { SetRibbon(ClientGameManager.Get().GetCurrentRibbon()); };
+        ClientGameManager.Get().OnPlayerRibbonChange += m_onPlayerRibbonChange;
+    }
 
-	public const string standardEmblemResourceString = "Banners/Emblems/Chest01";
+    private void OnDestroy()
+    {
+        ClientGameManager clientGameManager = ClientGameManager.Get();
+        if (clientGameManager == null)
+        {
+            return;
+        }
 
-	public const string rankedModeEnemyBannerResourceString = "Banners/Background/rankedRedDefault";
+        clientGameManager.OnAccountDataUpdated -= m_onAccountDataUpdated;
+        clientGameManager.OnPlayerBannerChange -= m_onPlayerBannerChange;
+        clientGameManager.OnPlayerRibbonChange -= m_onPlayerRibbonChange;
+    }
 
-	private Action<PersistedAccountData> m_onAccountDataUpdated;
+    private void SetBanner(GameBalanceVars.PlayerBanner banner, GameBalanceVars.PlayerBanner.BannerType bannerType)
+    {
+        Sprite sprite;
+        if (banner != null)
+        {
+            sprite = (Sprite)Resources.Load(banner.m_resourceString, typeof(Sprite));
+            if (sprite == null)
+            {
+                Log.Warning(
+                    Log.Category.UI,
+                    $"Could not load banner resource from [{banner.m_resourceString}] as sprite.");
+            }
+        }
+        else
+        {
+            sprite = (Sprite)Resources.Load(standardResourceString, typeof(Sprite));
+            if (sprite == null)
+            {
+                Log.Warning(
+                    Log.Category.UI,
+                    $"Could not load banner resource from [{standardResourceString}] as sprite.");
+            }
+        }
 
-	private Action<GameBalanceVars.PlayerBanner, GameBalanceVars.PlayerBanner> m_onPlayerBannerChange;
+        if (sprite != null)
+        {
+            if (bannerType == GameBalanceVars.PlayerBanner.BannerType.Background)
+            {
+                m_bannerImage.sprite = sprite;
+            }
+            else
+            {
+                m_profileImage.sprite = sprite;
+            }
+        }
+    }
 
-	private Action<GameBalanceVars.PlayerRibbon> m_onPlayerRibbonChange;
+    private void SetRibbon(GameBalanceVars.PlayerRibbon ribbon)
+    {
+        if (m_ribbonImage == null)
+        {
+            return;
+        }
 
-	private void Start()
-	{
-		m_onAccountDataUpdated = delegate(PersistedAccountData accountData)
-		{
-			SetBanner(ClientGameManager.Get().GetCurrentBackgroundBanner(), GameBalanceVars.PlayerBanner.BannerType.Background);
-			SetBanner(ClientGameManager.Get().GetCurrentForegroundBanner(), GameBalanceVars.PlayerBanner.BannerType.Foreground);
-			SetRibbon(ClientGameManager.Get().GetCurrentRibbon());
-			if (m_playerName != null)
-			{
-				m_playerName.text = accountData.UserName;
-			}
-			if (m_playerTitle != null)
-			{
-				m_playerTitle.text = GameBalanceVars.Get().GetTitle(accountData.AccountComponent.SelectedTitleID, string.Empty);
-			}
-			if (m_playerLevel != null)
-			{
-				while (true)
-				{
-					switch (6)
-					{
-					case 0:
-						break;
-					default:
-						m_playerLevel.text = ClientGameManager.Get().GetDisplayedStatString(accountData);
-						return;
-					}
-				}
-			}
-		};
-		ClientGameManager.Get().OnAccountDataUpdated += m_onAccountDataUpdated;
-		if (ClientGameManager.Get().IsPlayerAccountDataAvailable())
-		{
-			m_onAccountDataUpdated(ClientGameManager.Get().GetPlayerAccountData());
-		}
-		SetBanner(ClientGameManager.Get().GetCurrentBackgroundBanner(), GameBalanceVars.PlayerBanner.BannerType.Background);
-		SetBanner(ClientGameManager.Get().GetCurrentForegroundBanner(), GameBalanceVars.PlayerBanner.BannerType.Foreground);
-		SetRibbon(ClientGameManager.Get().GetCurrentRibbon());
-		m_onPlayerBannerChange = delegate(GameBalanceVars.PlayerBanner foregroundBanner, GameBalanceVars.PlayerBanner backgroundBanner)
-		{
-			SetBanner(backgroundBanner, GameBalanceVars.PlayerBanner.BannerType.Background);
-			SetBanner(foregroundBanner, GameBalanceVars.PlayerBanner.BannerType.Foreground);
-		};
-		ClientGameManager.Get().OnPlayerBannerChange += m_onPlayerBannerChange;
-		m_onPlayerRibbonChange = delegate
-		{
-			SetRibbon(ClientGameManager.Get().GetCurrentRibbon());
-		};
-		ClientGameManager.Get().OnPlayerRibbonChange += m_onPlayerRibbonChange;
-	}
+        Sprite sprite = null;
+        if (ribbon != null)
+        {
+            sprite = Resources.Load<Sprite>(ribbon.m_resourceString);
+            if (sprite == null)
+            {
+                Log.Warning(
+                    Log.Category.UI,
+                    $"Could not load ribbon resource from [{ribbon.m_resourceString}] as sprite.");
+            }
+        }
 
-	private void OnDestroy()
-	{
-		ClientGameManager clientGameManager = ClientGameManager.Get();
-		if (clientGameManager == null)
-		{
-			while (true)
-			{
-				switch (5)
-				{
-				case 0:
-					break;
-				default:
-					return;
-				}
-			}
-		}
-		clientGameManager.OnAccountDataUpdated -= m_onAccountDataUpdated;
-		clientGameManager.OnPlayerBannerChange -= m_onPlayerBannerChange;
-		clientGameManager.OnPlayerRibbonChange -= m_onPlayerRibbonChange;
-	}
-
-	private void SetBanner(GameBalanceVars.PlayerBanner banner, GameBalanceVars.PlayerBanner.BannerType bannerType)
-	{
-		Sprite sprite = null;
-		if (banner != null)
-		{
-			sprite = (Sprite)Resources.Load(banner.m_resourceString, typeof(Sprite));
-			if (sprite == null)
-			{
-				Log.Warning(Log.Category.UI, $"Could not load banner resource from [{banner.m_resourceString}] as sprite.");
-			}
-		}
-		else
-		{
-			sprite = (Sprite)Resources.Load("Banners/Background/02_blue", typeof(Sprite));
-			if (sprite == null)
-			{
-				Log.Warning(Log.Category.UI, string.Format("Could not load banner resource from [{0}] as sprite.", "Banners/Background/02_blue"));
-			}
-		}
-		if (!(sprite != null))
-		{
-			return;
-		}
-		while (true)
-		{
-			if (bannerType == GameBalanceVars.PlayerBanner.BannerType.Background)
-			{
-				while (true)
-				{
-					switch (7)
-					{
-					case 0:
-						break;
-					default:
-						m_bannerImage.sprite = sprite;
-						return;
-					}
-				}
-			}
-			m_profileImage.sprite = sprite;
-			return;
-		}
-	}
-
-	private void SetRibbon(GameBalanceVars.PlayerRibbon ribbon)
-	{
-		if (m_ribbonImage == null)
-		{
-			while (true)
-			{
-				switch (7)
-				{
-				case 0:
-					break;
-				default:
-					return;
-				}
-			}
-		}
-		Sprite sprite = null;
-		if (ribbon != null)
-		{
-			sprite = Resources.Load<Sprite>(ribbon.m_resourceString);
-			if (sprite == null)
-			{
-				Log.Warning(Log.Category.UI, $"Could not load ribbon resource from [{ribbon.m_resourceString}] as sprite.");
-			}
-		}
-		UIManager.SetGameObjectActive(m_ribbonImage, sprite != null);
-		m_ribbonImage.sprite = sprite;
-	}
+        UIManager.SetGameObjectActive(m_ribbonImage, sprite != null);
+        m_ribbonImage.sprite = sprite;
+    }
 }
