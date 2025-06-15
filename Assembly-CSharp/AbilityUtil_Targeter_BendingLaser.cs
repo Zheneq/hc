ae68cc52037cd5e837e1c649e4891393b81372f2
zheneq <zheneq@users.noreply.github.com>
2025-06-15 21:57:42 +0400

    Update AbilityUtil_Targeter_BendingLaser.cs

--- Assembly-CSharp/AbilityUtil_Targeter_BendingLaser.cs
+++ Assembly-CSharp/AbilityUtil_Targeter_BendingLaser.cs
@@ -69,46 +69,28 @@ public class AbilityUtil_Targeter_BendingLaser : AbilityUtil_Targeter
 
 	private float GetClampedRangeInSquares(ActorData targetingActor, AbilityTarget currentTarget)
 	{
-		Vector3 travelBoardSquareWorldPositionForLos = targetingActor.GetLoSCheckPos();
-		float magnitude = (currentTarget.FreePos - travelBoardSquareWorldPositionForLos).magnitude;
+		Vector3 loSCheckPos = targetingActor.GetLoSCheckPos();
+		float magnitude = (currentTarget.FreePos - loSCheckPos).magnitude;
 		if (magnitude < m_minDistanceBeforeBend * Board.Get().squareSize)
 		{
 			return m_minDistanceBeforeBend;
 		}
 		if (magnitude > m_maxDistanceBeforeBend * Board.Get().squareSize)
 		{
-			while (true)
-			{
-				switch (2)
-				{
-				case 0:
-					break;
-				default:
-					return m_maxDistanceBeforeBend;
-				}
-			}
+			return m_maxDistanceBeforeBend;
 		}
 		return magnitude / Board.Get().squareSize;
 	}
 
 	private float GetDistanceRemaining(ActorData targetingActor, AbilityTarget previousTarget, out Vector3 bendPos)
 	{
-		Vector3 a = bendPos = targetingActor.GetLoSCheckPos();
+		Vector3 loSCheckPos = bendPos = targetingActor.GetLoSCheckPos();
 		if (m_stoppedShort)
 		{
-			while (true)
-			{
-				switch (6)
-				{
-				case 0:
-					break;
-				default:
-					return 0f;
-				}
-			}
+			return 0f;
 		}
 		float clampedRangeInSquares = GetClampedRangeInSquares(targetingActor, previousTarget);
-		bendPos = a + previousTarget.AimDirection * clampedRangeInSquares * Board.Get().squareSize;
+		bendPos = loSCheckPos + previousTarget.AimDirection * clampedRangeInSquares * Board.Get().squareSize;
 		return m_maxTotalDistance - clampedRangeInSquares;
 	}
 
@@ -134,191 +116,183 @@ public class AbilityUtil_Targeter_BendingLaser : AbilityUtil_Targeter
 
 	public override void UpdateTargetingMultiTargets(AbilityTarget currentTarget, ActorData targetingActor, int currentTargetIndex, List<AbilityTarget> targets)
 	{
-		float num = m_width * Board.Get().squareSize;
+		float widthInWorld = m_width * Board.Get().squareSize;
 		float y = 0.1f - BoardSquare.s_LoSHeightOffset;
 		ClearActorsInRange();
 		m_ordererdHitActors.Clear();
 		List<ActorData> list = new List<ActorData>();
 		m_stoppedShort = false;
 		m_highlightsToFade.Clear();
-		int num2;
-		if (m_showAngleIndicators)
-		{
-			num2 = ((currentTargetIndex == 0) ? 1 : 0);
-		}
-		else
-		{
-			num2 = 0;
-		}
-		bool flag = (byte)num2 != 0;
+		bool showAngleIndicators = m_showAngleIndicators && currentTargetIndex == 0;
 		VectorUtils.LaserCoords laserCoords = default(VectorUtils.LaserCoords);
-		float num3;
-		Vector3 vector;
-		float num4;
+		float rangeInSquares;
+		Vector3 aimDir;
+		float initialOffsetInSquares;
 		if (currentTargetIndex == 0)
 		{
 			laserCoords.start = targetingActor.GetLoSCheckPos();
-			num3 = GetClampedRangeInSquares(targetingActor, currentTarget);
-			vector = currentTarget.AimDirection;
-			num4 = GameWideData.Get().m_laserInitialOffsetInSquares;
+			rangeInSquares = GetClampedRangeInSquares(targetingActor, currentTarget);
+			aimDir = currentTarget.AimDirection;
+			initialOffsetInSquares = GameWideData.Get().m_laserInitialOffsetInSquares;
 		}
 		else
 		{
 			Vector3 aimDirection = targets[currentTargetIndex - 1].AimDirection;
-			num3 = GetDistanceRemaining(targetingActor, targets[0], out laserCoords.start);
+			rangeInSquares = GetDistanceRemaining(targetingActor, targets[0], out laserCoords.start);
+			
 			Vector3 freePos = currentTarget.FreePos;
 			if ((currentTarget.FreePos - targets[currentTargetIndex - 1].FreePos).magnitude < Mathf.Epsilon)
 			{
 				freePos += aimDirection * 10f;
 			}
-			vector = freePos - laserCoords.start;
-			vector.y = 0f;
-			vector.Normalize();
-			num4 = -0.2f;
-			if (m_maxBendAngle > 0f)
+			aimDir = freePos - laserCoords.start;
+			aimDir.y = 0f;
+			aimDir.Normalize();
+			
+			initialOffsetInSquares = -0.2f;
+			if (m_maxBendAngle > 0f && m_maxBendAngle < 360f)
 			{
-				if (m_maxBendAngle < 360f)
-				{
-					vector = Vector3.RotateTowards(aimDirection, vector, (float)Math.PI / 180f * m_maxBendAngle, 0f);
-				}
+				aimDir = Vector3.RotateTowards(aimDirection, aimDir, (float)Math.PI / 180f * m_maxBendAngle, 0f);
 			}
-			laserCoords.start = VectorUtils.GetAdjustedStartPosWithOffset(laserCoords.start, laserCoords.start + vector, num4);
+			laserCoords.start = VectorUtils.GetAdjustedStartPosWithOffset(laserCoords.start, laserCoords.start + aimDir, initialOffsetInSquares);
 		}
 		if (currentTargetIndex > 0)
 		{
-			Vector3 lineEndPoint = VectorUtils.GetLineEndPoint(laserCoords.start, vector, num3 * Board.SquareSizeStatic);
-			num3 = Mathf.Min(VectorUtils.HorizontalPlaneDistInSquares(lineEndPoint, laserCoords.start), num3);
+			Vector3 lineEndPoint = VectorUtils.GetLineEndPoint(laserCoords.start, aimDir, rangeInSquares * Board.SquareSizeStatic);
+			rangeInSquares = Mathf.Min(VectorUtils.HorizontalPlaneDistInSquares(lineEndPoint, laserCoords.start), rangeInSquares);
 		}
-		List<ActorData> actors = AreaEffectUtils.GetActorsInLaser(laserCoords.start, vector, num3, m_width, targetingActor, GetAffectedTeams(), m_penetrateLoS, 0, false, false, out laserCoords.end, null, null, currentTargetIndex > 0);
+		List<ActorData> actors = AreaEffectUtils.GetActorsInLaser(
+			laserCoords.start,
+			aimDir,
+			rangeInSquares,
+			m_width,
+			targetingActor,
+			GetAffectedTeams(),
+			m_penetrateLoS,
+			0,
+			false,
+			false,
+			out laserCoords.end,
+			null,
+			null,
+			currentTargetIndex > 0);
 		TargeterUtils.SortActorsByDistanceToPos(ref actors, laserCoords.start);
-		bool flag2 = false;
-		int num5 = m_maxTargets;
-		if (currentTargetIndex > 0 && m_ability != null)
+		bool isMultiHit = false;
+		int numActorsInRange = m_maxTargets;
+		if (currentTargetIndex > 0
+		    && m_ability != null
+		    && currentTargetIndex < m_ability.Targeters.Count)
 		{
-			if (currentTargetIndex < m_ability.Targeters.Count)
+			AbilityUtil_Targeter abilityUtil_Targeter = m_ability.Targeters[currentTargetIndex - 1];
+			for (int i = actors.Count - 1; i >= 0; i--)
 			{
-				AbilityUtil_Targeter abilityUtil_Targeter = m_ability.Targeters[currentTargetIndex - 1];
-				for (int num6 = actors.Count - 1; num6 >= 0; num6--)
+				if (abilityUtil_Targeter.IsActorInTargetRange(actors[i]))
 				{
-					ActorData actor = actors[num6];
-					if (abilityUtil_Targeter.IsActorInTargetRange(actor))
-					{
-						actors.RemoveAt(num6);
-					}
+					actors.RemoveAt(i);
 				}
-				flag2 = (abilityUtil_Targeter.GetNumActorsInRange() > 0);
-				num5 -= abilityUtil_Targeter.GetNumActorsInRange();
 			}
+			isMultiHit = abilityUtil_Targeter.GetNumActorsInRange() > 0;
+			numActorsInRange -= abilityUtil_Targeter.GetNumActorsInRange();
 		}
 		if (actors.Contains(targetingActor))
 		{
 			actors.Remove(targetingActor);
 		}
-		if (actors.Count > num5)
+		if (actors.Count > numActorsInRange)
 		{
-			actors.RemoveRange(num5, actors.Count - num5);
+			actors.RemoveRange(numActorsInRange, actors.Count - numActorsInRange);
 		}
+		
 		float magnitude = (laserCoords.end - laserCoords.start).magnitude;
+		if (currentTargetIndex == 0 && magnitude < rangeInSquares * Board.Get().squareSize - 0.1f)
+		{
+			m_stoppedShort = true;
+		}
+		magnitude -= initialOffsetInSquares;
+		
 		if (currentTargetIndex == 0)
 		{
-			if (magnitude < num3 * Board.Get().squareSize - 0.1f)
-			{
-				m_stoppedShort = true;
-			}
+			laserCoords.start = VectorUtils.GetAdjustedStartPosWithOffset(laserCoords.start, laserCoords.end, initialOffsetInSquares);
 		}
-		magnitude -= num4;
-		if (currentTargetIndex == 0)
-		{
-			laserCoords.start = VectorUtils.GetAdjustedStartPosWithOffset(laserCoords.start, laserCoords.end, num4);
-		}
-		float num7 = magnitude;
-		float num8 = 0f;
-		float lengthInSquares = m_maxTotalDistance - num3 + 0.5f * num;
+		float lengthInWorld1 = magnitude;
+		float lengthInWorld2 = 0f;
+		float lengthInSquares = m_maxTotalDistance - rangeInSquares + 0.5f * widthInWorld;
 		if (m_highlights.IsNullOrEmpty())
 		{
-			m_highlights = new List<GameObject>(4);
-			m_highlights.Add(HighlightUtils.Get().CreateRectangularCursor(num, num7));
-			m_highlights.Add(HighlightUtils.Get().CreateRectangularCursor(num, num8));
+			m_highlights = new List<GameObject>(4)
+			{
+				HighlightUtils.Get().CreateRectangularCursor(widthInWorld, lengthInWorld1),
+				HighlightUtils.Get().CreateRectangularCursor(widthInWorld, lengthInWorld2),
+				HighlightUtils.Get().CreateDynamicLineSegmentMesh(lengthInSquares, 0.2f, true, Color.cyan),
+				HighlightUtils.Get().CreateDynamicLineSegmentMesh(lengthInSquares, 0.2f, true, Color.cyan)
+			};
 			m_laserStartRect = m_highlights[0].GetComponent<UIRectangleCursor>();
 			m_laserEndRect = m_highlights[1].GetComponent<UIRectangleCursor>();
-			m_highlights.Add(HighlightUtils.Get().CreateDynamicLineSegmentMesh(lengthInSquares, 0.2f, true, Color.cyan));
-			m_highlights.Add(HighlightUtils.Get().CreateDynamicLineSegmentMesh(lengthInSquares, 0.2f, true, Color.cyan));
-			m_highlights[2].SetActive(flag);
-			m_highlights[3].SetActive(flag);
+			m_highlights[2].SetActive(showAngleIndicators);
+			m_highlights[3].SetActive(showAngleIndicators);
 		}
-		int num9;
-		if (num5 > 0)
+
+		bool hitMaxTargets = numActorsInRange > 0 && actors.Count == numActorsInRange;
+		if (hitMaxTargets)
 		{
-			num9 = ((actors.Count == num5) ? 1 : 0);
-		}
-		else
-		{
-			num9 = 0;
-		}
-		bool flag3 = (byte)num9 != 0;
-		if (flag3)
-		{
-			Vector3 travelBoardSquareWorldPosition = actors[actors.Count - 1].GetFreePos();
-			travelBoardSquareWorldPosition.y = laserCoords.start.y;
-			num7 = (travelBoardSquareWorldPosition - laserCoords.start).magnitude;
+			Vector3 lastTargetPos = actors[actors.Count - 1].GetFreePos();
+			lastTargetPos.y = laserCoords.start.y;
+			lengthInWorld1 = (lastTargetPos - laserCoords.start).magnitude;
 			if (m_startFadeAtActorRadius)
 			{
-				num7 -= GameWideData.Get().m_actorTargetingRadiusInSquares * Board.SquareSizeStatic;
+				lengthInWorld1 -= GameWideData.Get().m_actorTargetingRadiusInSquares * Board.SquareSizeStatic;
 			}
-			num8 = magnitude - num7;
+			lengthInWorld2 = magnitude - lengthInWorld1;
 			if (m_laserStartRect != null)
 			{
-				if (num8 < m_laserStartRect.m_lengthPerCorner)
+				if (lengthInWorld2 < m_laserStartRect.m_lengthPerCorner)
 				{
-					flag3 = false;
-					num7 = magnitude;
+					hitMaxTargets = false;
+					lengthInWorld1 = magnitude;
 				}
 				else
 				{
-					num8 += m_laserStartRect.m_distCasterToInterior;
+					lengthInWorld2 += m_laserStartRect.m_distCasterToInterior;
 					if (currentTargetIndex != 0)
 					{
-						num7 += num4 + m_laserStartRect.m_distCasterToStart;
+						lengthInWorld1 += initialOffsetInSquares + m_laserStartRect.m_distCasterToStart;
 					}
-					num7 += m_laserStartRect.m_lengthPerCorner;
+					lengthInWorld1 += m_laserStartRect.m_lengthPerCorner;
 				}
 			}
 		}
-		HighlightUtils.Get().ResizeRectangularCursor(num, num7, m_highlights[0]);
-		m_laserStartRect.SetRectangleEndVisible(!flag3);
-		m_laserEndRect.SetRectangleStartVisible(!flag3);
-		m_highlights[1].SetActive(flag3);
-		if (flag3)
+		HighlightUtils.Get().ResizeRectangularCursor(widthInWorld, lengthInWorld1, m_highlights[0]);
+		m_laserStartRect.SetRectangleEndVisible(!hitMaxTargets);
+		m_laserEndRect.SetRectangleStartVisible(!hitMaxTargets);
+		m_highlights[1].SetActive(hitMaxTargets);
+		if (hitMaxTargets)
 		{
-			HighlightUtils.Get().ResizeRectangularCursor(num, num8, m_highlights[1]);
+			HighlightUtils.Get().ResizeRectangularCursor(widthInWorld, lengthInWorld2, m_highlights[1]);
 			m_highlightsToFade.Add(1);
 		}
 		Vector3 normalized = (laserCoords.end - laserCoords.start).normalized;
 		m_highlights[0].transform.position = laserCoords.start + new Vector3(0f, y, 0f);
 		m_highlights[0].transform.rotation = Quaternion.LookRotation(normalized);
-		if (flag3)
+		if (hitMaxTargets)
 		{
-			m_highlights[1].transform.position = laserCoords.start + normalized * (num7 - m_laserEndRect.m_lengthPerCorner - m_laserEndRect.m_distCasterToInterior) + new Vector3(0f, y, 0f);
+			m_highlights[1].transform.position = laserCoords.start + normalized * (lengthInWorld1 - m_laserEndRect.m_lengthPerCorner - m_laserEndRect.m_distCasterToInterior) + new Vector3(0f, y, 0f);
 			m_highlights[1].transform.rotation = Quaternion.LookRotation(normalized);
 		}
-		if (num5 > 0)
+		if (numActorsInRange > 0)
 		{
-			int num10 = 0;
+			int actorsHit = 0;
 			for (int i = 0; i < actors.Count; i++)
 			{
 				ActorData actorData = actors[i];
-				Vector3 vector2 = laserCoords.start;
-				if (currentTargetIndex > 0)
+				Vector3 start = laserCoords.start;
+				if (currentTargetIndex > 0 && Board.Get().GetSquareFromVec3(start) == actorData.GetCurrentBoardSquare())
 				{
-					if (Board.Get().GetSquareFromVec3(vector2) == actorData.GetCurrentBoardSquare())
-					{
-						vector2 = targetingActor.GetLoSCheckPos();
-					}
+					start = targetingActor.GetLoSCheckPos();
 				}
-				AddActorInRange(actorData, vector2, targetingActor);
-				if (!flag2 && i == 0)
+				AddActorInRange(actorData, start, targetingActor);
+				if (!isMultiHit && i == 0)
 				{
-					AddActorInRange(actorData, vector2, targetingActor, AbilityTooltipSubject.Near, true);
+					AddActorInRange(actorData, start, targetingActor, AbilityTooltipSubject.Near, true);
 				}
 				if (currentTargetIndex > 0)
 				{
@@ -326,7 +300,7 @@ public class AbilityUtil_Targeter_BendingLaser : AbilityUtil_Targeter
 				}
 				list.Add(actorData);
 				m_ordererdHitActors.Add(actorData);
-				num10++;
+				actorsHit++;
 			}
 			if (m_affectsTargetingActor)
 			{
@@ -337,38 +311,35 @@ public class AbilityUtil_Targeter_BendingLaser : AbilityUtil_Targeter
 		{
 			m_highlightsToFade.Add(0);
 		}
-		if (flag)
+		if (showAngleIndicators)
 		{
-			GameObject gameObject = m_highlights[2];
-			GameObject gameObject2 = m_highlights[3];
+			GameObject highlight2 = m_highlights[2];
+			GameObject highlight3 = m_highlights[3];
 			Vector3 aimDirection2 = currentTarget.AimDirection;
 			aimDirection2.y = 0f;
-			if (aimDirection2.magnitude > 0f)
+			if (aimDirection2.magnitude > 0f && !m_stoppedShort)
 			{
-				if (!m_stoppedShort)
-				{
-					gameObject.SetActive(true);
-					gameObject2.SetActive(true);
-					float num11 = VectorUtils.HorizontalAngle_Deg(aimDirection2);
-					float angle = num11 + m_maxBendAngle;
-					float angle2 = num11 - m_maxBendAngle;
-					Vector3 end = laserCoords.end;
-					end -= aimDirection2 * ((num + 0.2f) * 0.5f);
-					end.y = HighlightUtils.GetHighlightHeight();
-					gameObject.transform.position = end;
-					gameObject2.transform.position = end;
-					gameObject.transform.rotation = Quaternion.LookRotation(VectorUtils.AngleDegreesToVector(angle));
-					gameObject2.transform.rotation = Quaternion.LookRotation(VectorUtils.AngleDegreesToVector(angle2));
-					HighlightUtils.Get().AdjustDynamicLineSegmentLength(gameObject, lengthInSquares);
-					HighlightUtils.Get().AdjustDynamicLineSegmentLength(gameObject2, lengthInSquares);
-					goto IL_0a33;
-				}
+				highlight2.SetActive(true);
+				highlight3.SetActive(true);
+				float num11 = VectorUtils.HorizontalAngle_Deg(aimDirection2);
+				float angle = num11 + m_maxBendAngle;
+				float angle2 = num11 - m_maxBendAngle;
+				Vector3 end = laserCoords.end;
+				end -= aimDirection2 * ((widthInWorld + 0.2f) * 0.5f);
+				end.y = HighlightUtils.GetHighlightHeight();
+				highlight2.transform.position = end;
+				highlight3.transform.position = end;
+				highlight2.transform.rotation = Quaternion.LookRotation(VectorUtils.AngleDegreesToVector(angle));
+				highlight3.transform.rotation = Quaternion.LookRotation(VectorUtils.AngleDegreesToVector(angle2));
+				HighlightUtils.Get().AdjustDynamicLineSegmentLength(highlight2, lengthInSquares);
+				HighlightUtils.Get().AdjustDynamicLineSegmentLength(highlight3, lengthInSquares);
 			}
-			gameObject.SetActive(false);
-			gameObject2.SetActive(false);
+			else
+			{
+				highlight2.SetActive(false);
+				highlight3.SetActive(false);
+			}
 		}
-		goto IL_0a33;
-		IL_0a33:
 		DrawInvalidSquareIndicators(currentTarget, targetingActor, laserCoords.start, laserCoords.end);
 	}
 
