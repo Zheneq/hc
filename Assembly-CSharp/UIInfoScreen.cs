1bda5432947524565efbcdfe204f1629e82d2b32
zheneq <zheneq@users.noreply.github.com>
2025-09-06 23:02:32 +0400

    Update UIInfoScreen.cs

--- Assembly-CSharp/UIInfoScreen.cs
+++ Assembly-CSharp/UIInfoScreen.cs
@@ -2,46 +2,26 @@ using UnityEngine;
 
 public class UIInfoScreen : MonoBehaviour
 {
-	private void Update()
-	{
-		if (!(GameFlowData.Get() != null))
-		{
-			return;
-		}
-		while (true)
-		{
-			if (!(GameManager.Get() != null))
-			{
-				return;
-			}
-			while (true)
-			{
-				if (GameManager.Get().GameConfig.GameType == GameType.Tutorial)
-				{
-					return;
-				}
-				while (true)
-				{
-					if (GameFlowData.Get().gameState != GameState.BothTeams_Decision)
-					{
-						if (GameFlowData.Get().gameState != GameState.BothTeams_Resolve)
-						{
-							return;
-						}
-					}
-					if (InputManager.Get().IsKeyBindingNewlyHeld(KeyPreference.ToggleInfo))
-					{
-						while (true)
-						{
-							Team teamViewing = GameFlowData.Get().LocalPlayerData.GetTeamViewing();
-							UIGameOverScreen.SetupTeamMemberList(GameplayUtils.GenerateStatsFromGame(teamViewing, GameFlowData.Get().LocalPlayerData.playerControllerId));
-							UIGameStatsWindow.Get().ToggleStatsWindow();
-							return;
-						}
-					}
-					return;
-				}
-			}
-		}
-	}
+    private void Update()
+    {
+        if (GameFlowData.Get() == null
+            || GameManager.Get() == null
+            || GameManager.Get().GameConfig.GameType == GameType.Tutorial)
+        {
+            return;
+        }
+
+        bool isDecisionOrResolve = GameFlowData.Get().gameState == GameState.BothTeams_Decision
+                                   || GameFlowData.Get().gameState == GameState.BothTeams_Resolve;
+        
+        if (isDecisionOrResolve && InputManager.Get().IsKeyBindingNewlyHeld(KeyPreference.ToggleInfo))
+        {
+            Team teamViewing = GameFlowData.Get().LocalPlayerData.GetTeamViewing();
+            UIGameOverScreen.SetupTeamMemberList(
+                GameplayUtils.GenerateStatsFromGame(
+                    teamViewing,
+                    GameFlowData.Get().LocalPlayerData.playerControllerId));
+            UIGameStatsWindow.Get().ToggleStatsWindow();
+        }
+    }
 }
