a5b45e411d5c0e1061da325d39c8bf54132fbe47
zheneq <zheneq@users.noreply.github.com>
2025-11-14 19:56:39 +0400

    Update InterfaceManager.cs

--- Assembly-CSharp/InterfaceManager.cs
+++ Assembly-CSharp/InterfaceManager.cs
@@ -4,230 +4,212 @@ using UnityEngine.Networking;
 
 public class InterfaceManager : NetworkBehaviour
 {
-	public class DisplayAlertMessage : MessageBase
-	{
-		public string alertText;
+    public class DisplayAlertMessage : MessageBase
+    {
+        public string alertText;
+        public float r;
+        public float g;
+        public float b;
 
-		public float r;
+        public override void Serialize(NetworkWriter writer)
+        {
+            writer.Write(alertText);
+            writer.Write(r);
+            writer.Write(g);
+            writer.Write(b);
+        }
 
-		public float g;
+        public override void Deserialize(NetworkReader reader)
+        {
+            alertText = reader.ReadString();
+            r = reader.ReadSingle();
+            g = reader.ReadSingle();
+            b = reader.ReadSingle();
+        }
+    }
 
-		public float b;
+    public Texture2D m_backgroundTexture;
+    public float m_combatTextLifetime = 2f;
+    public float m_combatTextScrollSpeed = 0.5f;
+    public int m_combatTextSize = 16;
 
-		public override void Serialize(NetworkWriter writer)
-		{
-			writer.Write(alertText);
-			writer.Write(r);
-			writer.Write(g);
-			writer.Write(b);
-		}
+    private const float LOW_TIME_MIN = 5.4f;
+    public static Color s_techPointsTextColor = new Color(0.5f, 0.4f, 1f);
+    public static Color s_coolDownTextColor = new Color(1f, 0.5f, 0.1f);
 
-		public override void Deserialize(NetworkReader reader)
-		{
-			alertText = reader.ReadString();
-			r = reader.ReadSingle();
-			g = reader.ReadSingle();
-			b = reader.ReadSingle();
-		}
-	}
+    private static InterfaceManager s_instance;
 
-	public Texture2D m_backgroundTexture;
+    public static InterfaceManager Get()
+    {
+        return s_instance;
+    }
 
-	public float m_combatTextLifetime = 2f;
+    private void Awake()
+    {
+        s_instance = this;
+    }
 
-	public float m_combatTextScrollSpeed = 0.5f;
+    public override void OnStartClient()
+    {
+        ClientGameManager.Get().Client.RegisterHandler((short)MyMsgType.DisplayAlert, MsgDisplayAlert);
+    }
 
-	public int m_combatTextSize = 16;
+    private void OnDestroy()
+    {
+        if (ClientGameManager.Get() != null)
+        {
+            NetworkClient client = ClientGameManager.Get().Client;
+            if (client != null)
+            {
+                client.UnregisterHandler((short)MyMsgType.DisplayAlert);
+            }
+        }
 
-	private const float LOW_TIME_MIN = 5.4f;
+        s_instance = null;
+    }
 
-	public static Color s_techPointsTextColor = new Color(0.5f, 0.4f, 1f);
+    private void Update()
+    {
+        ActorData activeOwnedActorData = GameFlowData.Get().activeOwnedActorData;
+        ActorData actorData = null;
+        bool isSwitchFreelancerKeyPressed = false;
+        if (InputManager.Get().IsKeyBindingNewlyHeld(KeyPreference.SwitchFreelancer)
+            || Application.isEditor && Input.GetKeyDown(KeyCode.F11))
+        {
+            actorData = GameFlowData.Get().nextOwnedActorData;
+            isSwitchFreelancerKeyPressed = true;
+        }
 
-	public static Color s_coolDownTextColor = new Color(1f, 0.5f, 0.1f);
+        if (activeOwnedActorData != null)
+        {
+            List<ActorData> ownedActorDatas = GameFlowData.Get().m_ownedActorDatas;
+            Team team = activeOwnedActorData.GetTeam();
+            for (int i = 0; i < ownedActorDatas.Count; i++)
+            {
+                if (ownedActorDatas[i].GetTeam() != team)
+                {
+                    continue;
+                }
 
-	private static InterfaceManager s_instance;
+                KeyPreference keyPreference = KeyPreference.Freelancer1 + i;
+                if (InputManager.Get().IsKeyBindingNewlyHeld(keyPreference))
+                {
+                    if (actorData != ownedActorDatas[i])
+                    {
+                        actorData = ownedActorDatas[i];
+                        isSwitchFreelancerKeyPressed = true;
+                    }
 
-	public static InterfaceManager Get()
-	{
-		return s_instance;
-	}
+                    break;
+                }
+            }
+        }
 
-	private void Awake()
-	{
-		s_instance = this;
-	}
+        if (actorData != null)
+        {
+            CameraManager.Get().SetTargetObject(
+                actorData.gameObject,
+                CameraManager.CameraTargetReason.UserFocusingOnActor);
+            if (GameFlowData.Get().IsActorDataOwned(actorData) && actorData != GameFlowData.Get().activeOwnedActorData)
+            {
+                GameFlowData.Get().activeOwnedActorData = actorData;
+            }
 
-	public override void OnStartClient()
-	{
-		ClientGameManager.Get().Client.RegisterHandler(49, MsgDisplayAlert);
-	}
+            if (isSwitchFreelancerKeyPressed)
+            {
+                UIFrontEnd.PlaySound(FrontEndButtonSounds.HudLockIn);
+            }
+        }
+    }
 
-	private void OnDestroy()
-	{
-		if (ClientGameManager.Get() != null)
-		{
-			NetworkClient client = ClientGameManager.Get().Client;
-			if (client != null)
-			{
-				client.UnregisterHandler(49);
-			}
-		}
-		s_instance = null;
-	}
+    public void OnTurnTick()
+    {
+        UISounds.GetUISounds().Play("ui_notification_turn_start");
+        UILoadingScreenPanel.Get().SetVisible(false);
+        if (UIFrontendLoadingScreen.Get().gameObject.activeSelf)
+        {
+            UIFrontendLoadingScreen.Get().StartDisplayFadeOut();
+        }
+    }
 
-	private void Update()
-	{
-		ActorData activeOwnedActorData = GameFlowData.Get().activeOwnedActorData;
-		ActorData actorData = null;
-		bool flag = false;
-		if (InputManager.Get().IsKeyBindingNewlyHeld(KeyPreference.SwitchFreelancer))
-		{
-			goto IL_005a;
-		}
-		if (Application.isEditor)
-		{
-			if (Input.GetKeyDown(KeyCode.F11))
-			{
-				goto IL_005a;
-			}
-		}
-		goto IL_0069;
-		IL_0069:
-		if (activeOwnedActorData != null)
-		{
-			List<ActorData> ownedActorDatas = GameFlowData.Get().m_ownedActorDatas;
-			Team team = activeOwnedActorData.GetTeam();
-			for (int i = 0; i < ownedActorDatas.Count; i++)
-			{
-				if (ownedActorDatas[i].GetTeam() != team)
-				{
-					continue;
-				}
-				KeyPreference keyPreference = KeyPreference.Freelancer1;
-				keyPreference += i;
-				if (!InputManager.Get().IsKeyBindingNewlyHeld(keyPreference))
-				{
-					continue;
-				}
-				if (actorData != ownedActorDatas[i])
-				{
-					actorData = ownedActorDatas[i];
-					flag = true;
-				}
-				break;
-			}
-		}
-		if (!(actorData != null))
-		{
-			return;
-		}
-		while (true)
-		{
-			CameraManager.Get().SetTargetObject(actorData.gameObject, CameraManager.CameraTargetReason.UserFocusingOnActor);
-			if (GameFlowData.Get().IsActorDataOwned(actorData) && actorData != GameFlowData.Get().activeOwnedActorData)
-			{
-				GameFlowData.Get().activeOwnedActorData = actorData;
-			}
-			if (flag)
-			{
-				while (true)
-				{
-					UIFrontEnd.PlaySound(FrontEndButtonSounds.HudLockIn);
-					return;
-				}
-			}
-			return;
-		}
-		IL_005a:
-		actorData = GameFlowData.Get().nextOwnedActorData;
-		flag = true;
-		goto IL_0069;
-	}
+    public bool ShouldHandleMouseClick()
+    {
+        return !UIUtils.IsMouseOnGUI() && GUIUtility.hotControl == 0;
+    }
 
-	public void OnTurnTick()
-	{
-		UISounds.GetUISounds().Play("ui_notification_turn_start");
-		UILoadingScreenPanel.Get().SetVisible(false);
-		if (!UIFrontendLoadingScreen.Get().gameObject.activeSelf)
-		{
-			return;
-		}
-		while (true)
-		{
-			UIFrontendLoadingScreen.Get().StartDisplayFadeOut();
-			return;
-		}
-	}
+    [Server]
+    public void SendDisplayAlert(string alertText, Color alertColor, NetworkConnection destinationConnection)
+    {
+        if (!NetworkServer.active)
+        {
+            Debug.LogWarning(
+                "[Server] function 'System.Void InterfaceManager::SendDisplayAlert(System.String,UnityEngine.Color,UnityEngine.Networking.NetworkConnection)' called on client");
+            return;
+        }
 
-	public bool ShouldHandleMouseClick()
-	{
-		bool flag = !UIUtils.IsMouseOnGUI();
-		bool flag2 = GUIUtility.hotControl == 0;
-		return flag && flag2;
-	}
+        DisplayAlertMessage displayAlertMessage = new DisplayAlertMessage
+        {
+            alertText = alertText,
+            r = alertColor.r,
+            g = alertColor.g,
+            b = alertColor.b
+        };
+        NetworkServer.SendToClient(
+            destinationConnection.connectionId,
+            (short)MyMsgType.DisplayAlert,
+            displayAlertMessage);
+    }
 
-	[Server]
-	public void SendDisplayAlert(string alertText, Color alertColor, NetworkConnection destinationConnection)
-	{
-		if (!NetworkServer.active)
-		{
-			Debug.LogWarning("[Server] function 'System.Void InterfaceManager::SendDisplayAlert(System.String,UnityEngine.Color,UnityEngine.Networking.NetworkConnection)' called on client");
-			return;
-		}
-		DisplayAlertMessage displayAlertMessage = new DisplayAlertMessage();
-		displayAlertMessage.alertText = alertText;
-		displayAlertMessage.r = alertColor.r;
-		displayAlertMessage.g = alertColor.g;
-		displayAlertMessage.b = alertColor.b;
-		NetworkServer.SendToClient(destinationConnection.connectionId, 49, displayAlertMessage);
-	}
+    [Client]
+    protected void MsgDisplayAlert(NetworkMessage netMsg)
+    {
+        if (!NetworkClient.active)
+        {
+            Debug.LogWarning(
+                "[Client] function 'System.Void InterfaceManager::MsgDisplayAlert(UnityEngine.Networking.NetworkMessage)' called on server");
+            return;
+        }
 
-	[Client]
-	protected void MsgDisplayAlert(NetworkMessage netMsg)
-	{
-		if (!NetworkClient.active)
-		{
-			while (true)
-			{
-				switch (2)
-				{
-				case 0:
-					break;
-				default:
-					Debug.LogWarning("[Client] function 'System.Void InterfaceManager::MsgDisplayAlert(UnityEngine.Networking.NetworkMessage)' called on server");
-					return;
-				}
-			}
-		}
-		DisplayAlertMessage displayAlertMessage = netMsg.ReadMessage<DisplayAlertMessage>();
-		DisplayAlert(displayAlertMessage.alertText, new Color(displayAlertMessage.r, displayAlertMessage.g, displayAlertMessage.b));
-	}
+        DisplayAlertMessage displayAlertMessage = netMsg.ReadMessage<DisplayAlertMessage>();
+        DisplayAlert(
+            displayAlertMessage.alertText,
+            new Color(displayAlertMessage.r, displayAlertMessage.g, displayAlertMessage.b));
+    }
 
-	internal void DisplayAlert(string alertText, Color color, float messageTimeSeconds = 2f, bool showBackground = false, int displayIndex = 0)
-	{
-		HUD_UI.Get().m_mainScreenPanel.m_alertDisplay.DisplayAlert(alertText, color, messageTimeSeconds, showBackground, displayIndex);
-	}
+    internal void DisplayAlert(
+        string alertText,
+        Color color,
+        float messageTimeSeconds = 2f,
+        bool showBackground = false,
+        int displayIndex = 0)
+    {
+        HUD_UI.Get().m_mainScreenPanel.m_alertDisplay.DisplayAlert(
+            alertText,
+            color,
+            messageTimeSeconds,
+            showBackground,
+            displayIndex);
+    }
 
-	internal void CancelAlert(string alertText)
-	{
+    internal void CancelAlert(string alertText)
+    {
 #if SERVER
 		HUD_UI.Get()?.m_mainScreenPanel?.m_alertDisplay?.CancelAlert(alertText);  // TODO LOW NULL custom checks bc can be null on server
 #else
-		HUD_UI.Get().m_mainScreenPanel.m_alertDisplay.CancelAlert(alertText);
+        HUD_UI.Get().m_mainScreenPanel.m_alertDisplay.CancelAlert(alertText);
 #endif
-	}
+    }
 
-	private void UNetVersion()
-	{
-	}
+    private void UNetVersion()
+    {
+    }
 
-	public override bool OnSerialize(NetworkWriter writer, bool forceAll)
-	{
-		bool result = default(bool);
-		return result;
-	}
+    public override bool OnSerialize(NetworkWriter writer, bool forceAll)
+    {
+        return false;
+    }
 
-	public override void OnDeserialize(NetworkReader reader, bool initialState)
-	{
-	}
+    public override void OnDeserialize(NetworkReader reader, bool initialState)
+    {
+    }
 }
