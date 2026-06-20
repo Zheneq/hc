a38e1946b58bf27a637775b2e1b48c44042eabbd
zheneq <zheneq@users.noreply.github.com>
2026-06-20 14:57:07 +0400

    Update CrashReportArchiveNameRequest.cs

--- Assembly-CSharp/LobbyGameClientMessages/CrashReportArchiveNameRequest.cs
+++ Assembly-CSharp/LobbyGameClientMessages/CrashReportArchiveNameRequest.cs
@@ -2,11 +2,10 @@ using System;
 
 namespace LobbyGameClientMessages
 {
-	[Serializable]
-	public class CrashReportArchiveNameRequest : WebSocketMessage
-	{
-		public int NumArchiveBytes;
-
-		public bool DevBuild;
-	}
+    [Serializable]
+    public class CrashReportArchiveNameRequest : WebSocketMessage
+    {
+        public int NumArchiveBytes;
+        public bool DevBuild;
+    }
 }
