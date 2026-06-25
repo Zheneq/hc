a75dbcbd9d73b07ad352f2d5f391b5211a1147fb
zheneq <zheneq@users.noreply.github.com>
2026-06-25 19:35:03 +0400

    Update FriendList.cs

--- Assembly-CSharp/FriendList.cs
+++ Assembly-CSharp/FriendList.cs
@@ -4,9 +4,7 @@ using System.Collections.Generic;
 [Serializable]
 public class FriendList
 {
-	public Dictionary<long, FriendInfo> Friends = new Dictionary<long, FriendInfo>();
-
-	public bool IsDelta;
-
-	public bool IsError;
+    public Dictionary<long, FriendInfo> Friends = new Dictionary<long, FriendInfo>();
+    public bool IsDelta;
+    public bool IsError;
 }
