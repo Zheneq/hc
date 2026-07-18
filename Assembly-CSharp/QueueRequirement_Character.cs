9eed4517259baac56cfe1d8375c479732c46e46a
zheneq <zheneq@users.noreply.github.com>
2026-07-18 13:53:09 +0400

    Update QueueRequirement_Character.cs

--- Assembly-CSharp/QueueRequirement_Character.cs
+++ Assembly-CSharp/QueueRequirement_Character.cs
@@ -4,127 +4,110 @@ using System;
 [Serializable]
 public class QueueRequirement_Character : QueueRequirement
 {
-	private CharacterType CharacterType;
+    private CharacterType CharacterType;
+    private RequirementType m_requirementType = RequirementType.HasUnlockedCharacter;
+    private bool m_anyGroupMember;
 
-	private RequirementType m_requirementType = RequirementType.HasUnlockedCharacter;
+    public override RequirementType Requirement => m_requirementType;
+    public override bool AnyGroupMember => m_anyGroupMember;
 
-	private bool m_anyGroupMember;
+    public override bool DoesApplicantPass(
+        IQueueRequirementSystemInfo systemInfo,
+        IQueueRequirementApplicant applicant,
+        GameType gameType,
+        GameSubType gameSubType)
+    {
+        bool result = false;
+        if (m_requirementType != RequirementType.HasUnlockedCharacter)
+        {
+            throw new Exception($"Unknown QueueRequirement_Character requirement: {Requirement}");
+        }
 
-	public override RequirementType Requirement => m_requirementType;
+        for (int i = 0; i < (int)CharacterType.Last; i++)
+        {
+            CharacterType characterType = (CharacterType)i;
+            if (applicant.IsCharacterTypeAvailable(characterType) && CharacterType == characterType)
+            {
+                result = true;
+                break;
+            }
+        }
 
-	public override bool AnyGroupMember => m_anyGroupMember;
+        return result;
+    }
 
-	public override bool DoesApplicantPass(IQueueRequirementSystemInfo systemInfo, IQueueRequirementApplicant applicant, GameType gameType, GameSubType gameSubType)
-	{
-		bool result = false;
-		RequirementType requirementType = m_requirementType;
-		if (requirementType != RequirementType.HasUnlockedCharacter)
-		{
-			while (true)
-			{
-				switch (1)
-				{
-				case 0:
-					break;
-				default:
-					throw new Exception($"Unknown QueueRequirement_Character requirement: {Requirement}");
-				}
-			}
-		}
-		int num = 0;
-		while (true)
-		{
-			if (num < 40)
-			{
-				CharacterType characterType = (CharacterType)num;
-				if (applicant.IsCharacterTypeAvailable(characterType))
-				{
-					if (CharacterType == characterType)
-					{
-						result = true;
-						break;
-					}
-				}
-				num++;
-				continue;
-			}
-			break;
-		}
-		return result;
-	}
+    public override LocalizationPayload GenerateFailure(
+        IQueueRequirementSystemInfo systemInfo,
+        IQueueRequirementApplicant applicant,
+        RequirementMessageContext context)
+    {
+        return GenerateFailure(systemInfo, applicant, context, out _);
+    }
 
-	public override LocalizationPayload GenerateFailure(IQueueRequirementSystemInfo systemInfo, IQueueRequirementApplicant applicant, RequirementMessageContext context)
-	{
-		QueueBlockOutReasonDetails Details;
-		return GenerateFailure(systemInfo, applicant, context, out Details);
-	}
+    public override LocalizationPayload GenerateFailure(
+        IQueueRequirementSystemInfo systemInfo,
+        IQueueRequirementApplicant applicant,
+        RequirementMessageContext context,
+        out QueueBlockOutReasonDetails Details)
+    {
+        Details = new QueueBlockOutReasonDetails();
+        if (Requirement != RequirementType.HasUnlockedCharacter)
+        {
+            throw new Exception($"Unknown requirement is failed: {Requirement}");
+        }
 
-	public override LocalizationPayload GenerateFailure(IQueueRequirementSystemInfo systemInfo, IQueueRequirementApplicant applicant, RequirementMessageContext context, out QueueBlockOutReasonDetails Details)
-	{
-		Details = new QueueBlockOutReasonDetails();
-		RequirementType requirement = Requirement;
-		if (requirement != RequirementType.HasUnlockedCharacter)
-		{
-			while (true)
-			{
-				switch (1)
-				{
-				case 0:
-					break;
-				default:
-					throw new Exception($"Unknown requirement is failed: {Requirement}");
-				}
-			}
-		}
-		LocalizationArg_Freelancer localizationArg_Freelancer = LocalizationArg_Freelancer.Create(CharacterType);
-		if (context == RequirementMessageContext.GroupQueueing)
-		{
-			while (true)
-			{
-				switch (5)
-				{
-				case 0:
-					break;
-				default:
-					return LocalizationPayload.Create("NoGroupMemberHasNotUnlockedFreelancerX", "Matchmaking", localizationArg_Freelancer);
-				}
-			}
-		}
-		return LocalizationPayload.Create("UserXHasNotUnlockedFreelancerX", "Matchmaking", applicant.LocalizedHandle, localizationArg_Freelancer);
-	}
+        LocalizationArg_Freelancer localizationArgFreelancer = LocalizationArg_Freelancer.Create(CharacterType);
+        switch (context)
+        {
+            case RequirementMessageContext.GroupQueueing:
+            {
+                return LocalizationPayload.Create(
+                    "NoGroupMemberHasNotUnlockedFreelancerX",
+                    "Matchmaking",
+                    localizationArgFreelancer);
+            }
+            default:
+            {
+                return LocalizationPayload.Create(
+                    "UserXHasNotUnlockedFreelancerX",
+                    "Matchmaking",
+                    applicant.LocalizedHandle,
+                    localizationArgFreelancer);
+            }
+        }
+    }
 
-	public override void WriteToJson(JsonWriter writer)
-	{
-		writer.WritePropertyName("Character");
-		writer.WriteValue(CharacterType.ToString());
-		writer.WritePropertyName("AnyGroupMember");
-		writer.WriteValue(AnyGroupMember.ToString());
-	}
+    public override void WriteToJson(JsonWriter writer)
+    {
+        writer.WritePropertyName("Character");
+        writer.WriteValue(CharacterType.ToString());
+        writer.WritePropertyName("AnyGroupMember");
+        writer.WriteValue(AnyGroupMember.ToString());
+    }
 
-	public static QueueRequirement Create(RequirementType reqType, JsonReader reader)
-	{
-		QueueRequirement_Character queueRequirement_Character = new QueueRequirement_Character();
-		queueRequirement_Character.m_requirementType = reqType;
-		reader.Read();
-		string value = reader.Value.ToString();
-		queueRequirement_Character.CharacterType = (CharacterType)Enum.Parse(typeof(CharacterType), value, true);
-		reader.Read();
-		if (reader.TokenType == JsonToken.PropertyName)
-		{
-			if (reader.Value != null)
-			{
-				if (reader.Value.ToString() == "AnyGroupMember")
-				{
-					reader.Read();
-					queueRequirement_Character.m_anyGroupMember = bool.Parse(reader.Value.ToString());
-					reader.Read();
-					goto IL_00da;
-				}
-			}
-		}
-		queueRequirement_Character.m_anyGroupMember = false;
-		goto IL_00da;
-		IL_00da:
-		return queueRequirement_Character;
-	}
+    public static QueueRequirement Create(RequirementType reqType, JsonReader reader)
+    {
+        QueueRequirement_Character queueRequirementCharacter = new QueueRequirement_Character
+        {
+            m_requirementType = reqType
+        };
+        reader.Read();
+        string value = reader.Value.ToString();
+        queueRequirementCharacter.CharacterType = (CharacterType)Enum.Parse(typeof(CharacterType), value, true);
+        reader.Read();
+        if (reader.TokenType == JsonToken.PropertyName
+            && reader.Value != null
+            && reader.Value.ToString() == "AnyGroupMember")
+        {
+            reader.Read();
+            queueRequirementCharacter.m_anyGroupMember = bool.Parse(reader.Value.ToString());
+            reader.Read();
+        }
+        else
+        {
+            queueRequirementCharacter.m_anyGroupMember = false;
+        }
+
+        return queueRequirementCharacter;
+    }
 }
