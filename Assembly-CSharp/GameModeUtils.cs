99203086f0cafceeed2852b52d21540764f08b0a
zheneq <zheneq@users.noreply.github.com>
2026-07-21 18:04:01 +0400

    Update GameModeUtils.cs

--- Assembly-CSharp/GameModeUtils.cs
+++ Assembly-CSharp/GameModeUtils.cs
@@ -1,94 +1,40 @@
 public static class GameModeUtils
 {
-	public static bool IsCtfGameModeEvent(ClientGameModeEvent gameModeEvent)
-	{
-		if (gameModeEvent == null)
-		{
-			return false;
-		}
-		return IsCtfGameModeEventType(gameModeEvent.m_eventType);
-	}
+    public static bool IsCtfGameModeEvent(ClientGameModeEvent gameModeEvent)
+    {
+        return gameModeEvent != null && IsCtfGameModeEventType(gameModeEvent.m_eventType);
+    }
 
-	public static bool IsCtcGameModeEvent(ClientGameModeEvent gameModeEvent)
-	{
-		if (gameModeEvent == null)
-		{
-			return false;
-		}
-		return IsCtcGameModeEventType(gameModeEvent.m_eventType);
-	}
+    public static bool IsCtcGameModeEvent(ClientGameModeEvent gameModeEvent)
+    {
+        return gameModeEvent != null && IsCtcGameModeEventType(gameModeEvent.m_eventType);
+    }
 
-	public static bool IsCtfGameModeEventType(GameModeEventType gameModeEventType)
-	{
-		switch (gameModeEventType)
-		{
-		case GameModeEventType.Ctf_FlagPickedUp:
-			return true;
-		case GameModeEventType.Ctf_FlagDropped:
-			return true;
-		case GameModeEventType.Ctf_FlagTurnedIn:
-			return true;
-		case GameModeEventType.Ctf_FlagSentToSpawn:
-			return true;
-		default:
-			return false;
-		}
-	}
+    public static bool IsCtfGameModeEventType(GameModeEventType gameModeEventType)
+    {
+        switch (gameModeEventType)
+        {
+            case GameModeEventType.Ctf_FlagPickedUp:
+            case GameModeEventType.Ctf_FlagDropped:
+            case GameModeEventType.Ctf_FlagTurnedIn:
+            case GameModeEventType.Ctf_FlagSentToSpawn:
+                return true;
+            default:
+                return false;
+        }
+    }
 
-	public static bool IsCtcGameModeEventType(GameModeEventType gameModeEventType)
-	{
-		if (gameModeEventType == GameModeEventType.Ctc_CoinPickedUp)
-		{
-			while (true)
-			{
-				switch (4)
-				{
-				case 0:
-					break;
-				default:
-					return true;
-				}
-			}
-		}
-		if (gameModeEventType == GameModeEventType.Ctc_CoinsDropped)
-		{
-			while (true)
-			{
-				switch (7)
-				{
-				case 0:
-					break;
-				default:
-					return true;
-				}
-			}
-		}
-		if (gameModeEventType == GameModeEventType.Ctc_NonCoinPowerupTouched)
-		{
-			while (true)
-			{
-				switch (4)
-				{
-				case 0:
-					break;
-				default:
-					return true;
-				}
-			}
-		}
-		if (gameModeEventType == GameModeEventType.Ctc_CoinPowerupTouched)
-		{
-			while (true)
-			{
-				switch (7)
-				{
-				case 0:
-					break;
-				default:
-					return true;
-				}
-			}
-		}
-		return false;
-	}
+    public static bool IsCtcGameModeEventType(GameModeEventType gameModeEventType)
+    {
+        switch (gameModeEventType)
+        {
+            case GameModeEventType.Ctc_CoinPickedUp:
+            case GameModeEventType.Ctc_CoinsDropped:
+            case GameModeEventType.Ctc_NonCoinPowerupTouched:
+            case GameModeEventType.Ctc_CoinPowerupTouched:
+                return true;
+            default:
+                return false;
+        }
+    }
 }
