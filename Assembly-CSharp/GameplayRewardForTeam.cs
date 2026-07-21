ceba5705c6df4365180ae006392ce90225808644
zheneq <zheneq@users.noreply.github.com>
2026-07-21 20:47:48 +0400

    Update GameplayRewardForTeam.cs

--- Assembly-CSharp/GameplayRewardForTeam.cs
+++ Assembly-CSharp/GameplayRewardForTeam.cs
@@ -3,40 +3,26 @@ using System;
 [Serializable]
 public class GameplayRewardForTeam
 {
-	public string m_name = "Gameplay Reward";
+    public string m_name = "Gameplay Reward";
+    public int m_objectivePointAdjust;
+    public StandardEffectInfo m_effectOnMembers;
+    public int m_creditsToMembers;
+    public int m_healingToMembers;
+    public int m_techPointsToMembers;
+    public AbilityStatMod[] m_permanentStatMods;
+    public StatusType[] m_permanentStatusChanges;
 
-	public int m_objectivePointAdjust;
+    public void ClientApplyRewardTo(Team team)
+    {
+        if (m_objectivePointAdjust == 0)
+        {
+            return;
+        }
 
-	public StandardEffectInfo m_effectOnMembers;
-
-	public int m_creditsToMembers;
-
-	public int m_healingToMembers;
-
-	public int m_techPointsToMembers;
-
-	public AbilityStatMod[] m_permanentStatMods;
-
-	public StatusType[] m_permanentStatusChanges;
-
-	public void ClientApplyRewardTo(Team team)
-	{
-		if (m_objectivePointAdjust == 0)
-		{
-			return;
-		}
-		while (true)
-		{
-			ObjectivePoints objectivePoints = ObjectivePoints.Get();
-			if (objectivePoints != null)
-			{
-				while (true)
-				{
-					objectivePoints.AdjustUnresolvedPoints(m_objectivePointAdjust, team);
-					return;
-				}
-			}
-			return;
-		}
-	}
+        ObjectivePoints objectivePoints = ObjectivePoints.Get();
+        if (objectivePoints != null)
+        {
+            objectivePoints.AdjustUnresolvedPoints(m_objectivePointAdjust, team);
+        }
+    }
 }
